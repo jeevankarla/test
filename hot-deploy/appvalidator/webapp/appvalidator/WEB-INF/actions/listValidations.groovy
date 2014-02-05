cc2891f27625b1baae660ee4cb6d403851f674a7
charan <charan@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-02-05 08:54:28 +0000

    App Validators
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@17 40d85018-ee8e-4105-aa2c-066e80ea2389

--- hot-deploy/appvalidator/webapp/appvalidator/WEB-INF/actions/listValidations.groovy
+++ hot-deploy/appvalidator/webapp/appvalidator/WEB-INF/actions/listValidations.groovy
@@ -1,13 +1,15 @@
 
 validationList = [];
 validationList.add(["component":"byproducts", "category":"Party", "id":"validateBoothParty", "name":"Party Validation", 
-	"description" : "Check each active booth/party has valid name, description, owner and classification"]);
+	"description" : "Check each active booth/party has valid name, description and owner"]);
 validationList.add(["component":"byproducts", "category":"Product", "id":"validateProduct", "name":"Product Validation",
 	"description" : "Check each active byproducts product has valid name, brandName, description and uom"]);
 validationList.add(["component":"byproducts", "category":"Product", "id":"validateProductPrice", "name":"Product Price Validation",
-	"description" : "Check each active byproducts product has valid price for all classifications"]);
+	"description" : "Check each active products has valid price"]);
 validationList.add(["component":"byproducts", "category":"Product", "id":"validatePartyProductPrice", "name":"Product Party Price Validation",
 	"description" : "Check each active party has valid byproducts product price"]);
 validationList.add(["component":"byproducts", "category":"Product", "id":"validateProductCategory", "name":"Product Category Validation",
 	"description" : "Check each byproducts product is associated with required categories"]);
+validationList.add(["component":"byproducts", "category":"Party", "id":"validateDealerRoute", "name":"Dealer Route Validation",
+	"description" : "Check each Dealer (Agents, Parlour & Shopee) is associated with Route and every route required a transporter"]);
 context.validationList = validationList;
--- hot-deploy/appvalidator/webapp/appvalidator/WEB-INF/actions/validators/byproducts/validateDealerRoute.groovy
+++ hot-deploy/appvalidator/webapp/appvalidator/WEB-INF/actions/validators/byproducts/validateDealerRoute.groovy
@@ -13,6 +13,11 @@ import org.ofbiz.base.util.*;
 import javolution.util.FastMap;
 import in.vasista.vbiz.byproducts.ByProductServices;
 import in.vasista.vbiz.byproducts.ByProductReportServices;
+import org.ofbiz.network.NetworkServices;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.entity.util.EntityUtil;
 
 def validationResult = new StringBuffer();
 numOks = 0;
@@ -30,33 +35,38 @@ boothList.each{ booth ->
 	message = "";
 	ownerName = "";
 	classification = "";
-	if (UtilValidate.isEmpty(booth.facilityName))
-	{
-		hasError = true;
-		message ="facilityName is empty;";
-	}
-	if (UtilValidate.isEmpty(booth.description))
-	{
-		hasWarn = true;
-		message += "description is empty;";
-	}
 	// check for valid owner party
-	ownerResult = ByProductServices.getBoothOwnerContactInfo(dctx, UtilMisc.toMap("facilityId", booth.facilityId, "userLogin", userLogin));
-	if (UtilValidate.isEmpty(ownerResult) || UtilValidate.isEmpty(ownerResult.contactInfo))
+	boothRoutes = delegator.findList("FacilityGroupMember", EntityCondition.makeCondition("facilityId", EntityOperator.EQUALS, booth.facilityId), UtilMisc.toSet("facilityGroupId"), null, null, false);
+	//ownerResult = NetworkServices.getBoothRoute(dctx, UtilMisc.toMap("boothId", booth.facilityId, "userLogin", userLogin));
+	boothRoutes = EntityUtil.getFieldListFromEntityList(boothRoutes, "facilityGroupId", true);
+	routeId = "";
+	tempHasError = false;
+	tempMessage = "";
+	if (UtilValidate.isEmpty(boothRoutes))
 	{
 		hasError = true;
-		message += "owner missing;";
+		message += "route missing;";
 	}
 	else {
-		ownerContactInfo = ownerResult.contactInfo;
-		ownerName = ownerContactInfo.name;
-		if (UtilValidate.isEmpty(ownerName))
-		{
-			hasWarn = true;
-			message += "owner name is empty;";
+		boothRoutes.each{ eachRoute ->
+			route = delegator.findOne("Facility", UtilMisc.toMap("facilityId", eachRoute), false);
+			if (UtilValidate.isEmpty(route))
+			{
+				tempHasError = true;
+				tempMessage += "Route ["+ route.facilityId+"]Missing";
+			}
+			else{
+				if(UtilValidate.isEmpty(route.ownerPartyId)){
+					tempHasError = true;
+					tempMessage += "Transporter for route ["+ route.facilityId +"] missing";
+				}
+			}
 		}
 	}
-	if (hasError) {
+	if(tempHasError){
+		message += tempMessage;
+	}
+	if (hasError || tempHasError) {
 		status = "ERROR"
 		numErrors++;
 	}
@@ -67,8 +77,7 @@ boothList.each{ booth ->
 	else {
 		numOks++;
 	}
-	validationResult.append( " [" + status + "] {" + booth.facilityId + "} {" + booth.facilityName + "}" +
-		" {" + booth.description + "} {" +  ownerName + "}" + message + "\n");
+	validationResult.append( " [" + status + "] {" + booth.facilityId + "} {" + booth.facilityName + "}" + message + "\n");
 }
 
 
