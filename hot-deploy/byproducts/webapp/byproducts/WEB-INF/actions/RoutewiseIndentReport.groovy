1ac0caa007a8c29816ca6897d704f9e200132839
raja <raja@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-02-18 13:40:54 +0000

    bugfix in cleanUp
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@91 40d85018-ee8e-4105-aa2c-066e80ea2389

--- hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/RoutewiseIndentReport.groovy
+++ hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/RoutewiseIndentReport.groovy
@@ -38,7 +38,7 @@
 	import java.math.BigDecimal;
 	import java.math.MathContext;
 	import org.ofbiz.base.util.UtilNumber;
-	import org.ofbiz.network.NetworkServices;
+
 	import in.vasista.vbiz.byproducts.ByProductServices;
 	
 	reportTypeFlag = context.reportTypeFlag;
