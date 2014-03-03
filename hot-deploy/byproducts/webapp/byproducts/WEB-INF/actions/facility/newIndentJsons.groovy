f7a47be8776fd2bed78a755725ad425174775b82
raja <raja@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-03-03 10:28:10 +0000

    only for catageryProducts and BrandName for autoComplete in indent
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@168 40d85018-ee8e-4105-aa2c-066e80ea2389

--- hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/facility/newIndentJsons.groovy
+++ hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/facility/newIndentJsons.groovy
@@ -14,6 +14,7 @@ import java.text.ParseException;
 import org.ofbiz.service.ServiceUtil;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
+import in.vasista.vbiz.byproducts.ByProductNetworkServices;
 
 
 effectiveDate = null;
@@ -29,13 +30,14 @@ effDateDayEnd = UtilDateTime.getDayEnd(effectiveDate);
 dctx = dispatcher.getDispatchContext();
 
 condList = [];
-condList.add(EntityCondition.makeCondition("productId", EntityOperator.NOT_EQUAL, "_NA_"));
+/*condList.add(EntityCondition.makeCondition("productId", EntityOperator.NOT_EQUAL, "_NA_"));
 condList.add(EntityCondition.makeCondition("isVirtual", EntityOperator.NOT_EQUAL, "Y"));
 condList.add(EntityCondition.makeCondition(EntityCondition.makeCondition("salesDiscontinuationDate", EntityOperator.EQUALS, null), EntityOperator.OR, EntityCondition.makeCondition("salesDiscontinuationDate", EntityOperator.GREATER_THAN_EQUAL_TO, effDateDayBegin)));
 cond = EntityCondition.makeCondition(condList, EntityOperator.AND);
 prodList = delegator.findList("Product", cond, UtilMisc.toSet("productId", "description","quantityIncluded"), UtilMisc.toList("sequenceNum"), null, false);
+*/
 
-
+prodList =ByProductNetworkServices.getByProductProducts(dispatcher.getDispatchContext(), UtilMisc.toMap("salesDate",effDateDayBegin));
 /*crateProducts = delegator.findList("ProductCategoryMember", EntityCondition.makeCondition("productCategoryId", EntityOperator.EQUALS, "CRATE_INDENT"), UtilMisc.toSet("productId"), null, null, false);
 crateProdIds = EntityUtil.getFieldListFromEntityList(crateProducts, "productId", true);
 
@@ -70,10 +72,10 @@ prodList.each{eachItem ->
 	
 	JSONObject newObj = new JSONObject();
 	newObj.put("value",eachItem.productId);
-	newObj.put("label", eachItem.description);
+	newObj.put("label", eachItem.brandName);
 	productItemsJSON.add(newObj);
 	productIdLabelJSON.put(eachItem.productId,  eachItem.description);
-	productLabelIdJSON.putAt(eachItem.description, eachItem.productId);
+	productLabelIdJSON.putAt(eachItem.brandName, eachItem.productId);
 	productQtyIncJSON.putAt(eachItem.productId, eachItem.quantityIncluded);
 }
 context.productItemsJSON = productItemsJSON;
