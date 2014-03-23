50076ff57b1c7deb7e809c06197c0726203b0342
raja <raja@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-03-23 12:59:55 +0000

    auto complete js prepared for routes and booths
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@340 40d85018-ee8e-4105-aa2c-066e80ea2389

---
+++ hot-deploy/byproducts/webapp/byproducts/images/autoCompleteFacility.js
@@ -0,0 +1,87 @@
+
+$(function() {
+       $("#boothId").autocomplete({ source: boothsList }).keydown(function(e){
+    	   $.ui.autocomplete.filter = function (array, term) { 
+               var matcher = new RegExp($.ui.autocomplete.escapeRegex(term), "i");
+               return $.grep(array, function (value) {
+                   return matcher.test(value.label || value.value || value);
+               });
+           };
+
+			if (e.keyCode === 13){
+		      	 $('#boothId').autocomplete('close');
+		      	 if( typeof $("#boothId").val() != "undefined"   && $("#boothId").val() != ""){ 
+		      	 }
+				return false;
+			}
+		});
+       // booth auto Complete
+     $('#boothId').keypress(function (e) {
+			$("#boothId").autocomplete({ source: boothsList ,filterOpt : ""});	
+			$.ui.autocomplete.filter = function (array, term) { 
+                var matcher = new RegExp($.ui.autocomplete.escapeRegex(term), "i");
+                return $.grep(array, function (value) {
+                    return matcher.test(value.label || value.value || value);
+                });
+            };
+	 });
+     
+  
+     // route auto Complete
+     $('#routeId').blur(function (e) {    	 	
+			//$("#routeId").autocomplete({ source: routesList , onblur: function( event, ui ) {
+		var rt = $("#routeId").val();		
+ 	 $('span#routeTooltip').html('<label>'+rt+'</label>');
+ 	 
+			//} });	
+	 });
+  // route auto Complete
+  $('#routeId').keypress(function (e) {    	 	
+			$("#routeId").autocomplete({ source: routesList , select: function( event, ui ) {
+				$('span#routeTooltip').html('<label>'+ui.item.label+'</label>');
+			} });	
+	 });
+  
+     $('#routeId').focus(function (e) {    	 	
+			$("#routeId").autocomplete({ source: routesList });	
+	 }); 
+     
+     changeRowColor();
+});
+
+//we have to use color scheme for vehcileStatus List
+function changeRowColor(){
+	   /* var cell;
+	    var result = $('tr').find("td:contains('VEHICLE_RETURNED')");
+	    alert("==text="+$(result).text()+"=afterTrim="+$.trim($(result).text()));
+	    var compText=$.trim($(result).text());
+	    if(compText=="VEHICLE_RETURNED"){
+	    	var resParent = $(result).parent();
+	    	$(resParent).css("background-color", "#D76871");
+	   // $(this).css("background-color", "#FFCC88");
+        }else{
+        	$(resParent).css("background-color", "green");
+        }
+	    */
+	    
+	
+	var tableObj = $('#_col table tr');
+	$(tableObj).each( function( index, element ){
+	    var result = $(this).find("td:contains('VEHICLE_RETURNED')");
+	    var compText=$.trim($(result).text());
+	    if (compText==("VEHICLE_RETURNED")){
+	    	 $(this).css("background-color", "#77BA72");
+	    }else{
+	    	$(this).css("background-color", "#D76871");
+	    }
+	});
+	
+   // 
+}
+
+
+
+
+
+
+
