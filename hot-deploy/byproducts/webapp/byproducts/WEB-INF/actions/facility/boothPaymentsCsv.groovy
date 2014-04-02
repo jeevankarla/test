101eabf96ddf016f5d214459eec968d41b9f7690
raja <raja@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-04-02 15:10:27 +0000

    csv added
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@455 40d85018-ee8e-4105-aa2c-066e80ea2389

---
+++ hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/facility/boothPaymentsCsv.groovy
@@ -0,0 +1,45 @@
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javolution.util.FastList;
+import in.vasista.vbiz.byproducts.ByProductNetworkServices;
+import org.ofbiz.base.util.*;
+
+if(parameters.statusId){
+	statusId = parameters.statusId;
+}else{
+	statusId = context.statusId;
+}
+
+dctx = dispatcher.getDispatchContext();
+boothRouteIdsMap=context.boothRouteIdsMap;
+boothPaymentsCsvList =FastList.newInstance();
+
+	boothPaymentsList.each { boothPayment ->
+	boothPaymentCsvMap =[:];
+	boothDetails = (ByProductNetworkServices.getBoothDetails(dctx , UtilMisc.toMap("boothId",boothPayment.get("facilityId")))).get("boothDetails");
+	
+	boothPaymentCsvMap["supplyDate"] = UtilDateTime.toDateString(paymentTimestamp,"MM/dd/yy");
+	boothPaymentCsvMap["zoneId"] = boothDetails.get("zoneId");
+	boothPaymentCsvMap["routeId"] = boothRouteIdsMap.get(boothPayment.get("facilityId"));
+	boothPaymentCsvMap["boothId"] = boothDetails.get("boothId");
+	boothPaymentCsvMap["boothName"] = boothDetails.get("boothName");
+	boothPaymentCsvMap["vendorName"] = boothDetails.get("vendorName");	
+	if(statusId == "PAID"){
+		boothPaymentCsvMap["paymentMethodTypeId"] = boothPayment.get("paymentMethodTypeId");
+		boothPaymentCsvMap["amount"] = (new BigDecimal(boothPayment.get("amount")));
+		boothPaymentCsvMap["userId"] =  boothPayment.get("userId");;
+	}else{
+		boothPaymentCsvMap["paymentMethodTypeId"] = null;
+		boothPaymentCsvMap["amount"] = (new BigDecimal(boothPayment.get("grandTotal")));
+		boothPaymentCsvMap["userId"] = null;
+	}
+	
+	
+	boothPaymentsCsvList.add(boothPaymentCsvMap);
+}
+
+context.boothPaymentsCsvList = boothPaymentsCsvList;
+
+
