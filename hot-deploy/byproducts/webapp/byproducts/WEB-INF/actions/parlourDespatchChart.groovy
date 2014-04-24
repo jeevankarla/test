e6d3ae68df64cb3bb99f3b8495a3be024ce32b7f
nagababu <nagababu@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-04-24 20:08:52 +0000

    category helper method fix
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@633 40d85018-ee8e-4105-aa2c-066e80ea2389

--- hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/parlourDespatchChart.groovy
+++ hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/parlourDespatchChart.groovy
@@ -168,7 +168,7 @@ productRevenueMap = [:];
 productCatRevenueMap = [:];
 regionWiseSalesMap = [:];
 
-productCatMap = ByProductNetworkServices.getProductCategoryMap(dctx, UtilMisc.toMap("productCategoryId","BYPROD_CAT" ,"salesDate",thruDate));
+productCatMap = ByProductNetworkServices.getProductCategoryMap(dctx, UtilMisc.toMap("productCategoryId","BYPROD_CAT" ,"salesDate",fromDate));
 if(shipmentReceipts){
 	shipmentReceipts.each{eachItem ->
 		boothRegionMap = boothsRegionMap.get((eachItem.getAt("facilityId")));
