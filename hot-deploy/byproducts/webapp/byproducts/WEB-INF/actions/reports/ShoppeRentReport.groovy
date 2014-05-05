81aaf9d289a94520effa5f63d4fe9bb11d413f5c
supriya <supriya@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-05-05 07:58:19 +0000

    Shoppe Rent Report
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@735 40d85018-ee8e-4105-aa2c-066e80ea2389

---
+++ hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/reports/ShoppeRentReport.groovy
@@ -0,0 +1,71 @@
+import org.ofbiz.base.util.*;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.util.EntityUtil;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.lang.*;
+import java.math.BigDecimal;
+import java.text.ParseException;
+import org.ofbiz.entity.*;
+import org.ofbiz.entity.condition.*;
+import javolution.util.FastList;
+import javolution.util.FastMap;
+import org.ofbiz.base.util.UtilDateTime;
+import org.ofbiz.network.LmsServices;
+import org.ofbiz.entity.util.EntityFindOptions;
+import in.vasista.vbiz.byproducts.ByProductServices;
+import in.vasista.vbiz.byproducts.ByProductNetworkServices;
+
+customTimePeriod=delegator.findList("CustomTimePeriod", EntityCondition.makeCondition("customTimePeriodId", parameters.customTimePeriodId), null,null, null, false);
+//Debug.log("customTimePeriod================="+customTimePeriod);
+dayStartfromDate=UtilDateTime.toTimestamp(customTimePeriod[0].fromDate);
+dayStartThruDate=UtilDateTime.toTimestamp(customTimePeriod[0].thruDate);
+context.put("dayStartfromDate", dayStartfromDate);
+context.put("dayStartThruDate", dayStartThruDate);
+
+decimals = UtilNumber.getBigDecimalScale("ledger.decimals");
+rounding = UtilNumber.getBigDecimalRoundingMode("ledger.rounding");
+
+
+finalList =[];
+facilityIdsList=ByProductNetworkServices.getAllBooths(delegator,null).get("boothsList");
+facilityIdsList. each {facilityId ->
+	facilityDetails = delegator.findOne("Facility", UtilMisc.toMap("facilityId", facilityId), false);
+	boothFacilityId = facilityDetails.get("facilityId");
+	facilityName = facilityDetails.get("description");
+	Map inputRateAmt = UtilMisc.toMap("userLogin", userLogin);
+	inputRateAmt.put("rateCurrencyUomId", "INR");
+	inputRateAmt.put("facilityId", boothFacilityId);
+	inputRateAmt.put("fromDate",dayStartThruDate);
+	inputRateAmt.put("rateTypeId", "SHOPEE_RENT");
+	
+	Map<String, Object> facilityRateResult = dispatcher.runSync("getFacilityRateAmount", inputRateAmt);
+	BigDecimal rateAmount=(BigDecimal)facilityRateResult.get("rateAmount");
+	
+	BigDecimal TaxrateAmount = rateAmount.divide(new BigDecimal(12.36), rounding);
+	
+	
+	tempMap =[:];
+	tempMap.put("boothId", boothFacilityId);
+	tempMap.put("facilityName", facilityName);
+	tempMap.put("rateAmount", rateAmount);
+	tempMap.put("TaxrateAmount", TaxrateAmount);
+	
+	finalList.addAll(tempMap);
+
+	
+	/*invoiceId = parameters.get("invoiceId");
+	invoice = delegator.findByPrimaryKey("Invoice", [invoiceId : invoiceId]);
+	resMap = dispatcher.runSync("createInvoiceItem", UtilMisc.toMap("invoiceId", invoiceId, "invoiceItemTypeId", "SHOPEE_RENT",
+				   "amount", rateAmount, "userLogin", userLogin));
+			   Debug.log("resMap========="+resMap);*/
+}
+context.putAt("finalList", finalList);
+
+
+
+
+
+
+
+
