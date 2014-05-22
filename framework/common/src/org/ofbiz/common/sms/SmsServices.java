95c2108b37b7e44037e10997caf5d670cfbf3403
kvarma <kvarma@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-05-22 09:01:45 +0000

    allow sending of test/demo SMSes to listed numbers
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@879 40d85018-ee8e-4105-aa2c-066e80ea2389

--- framework/common/src/org/ofbiz/common/sms/SmsServices.java
+++ framework/common/src/org/ofbiz/common/sms/SmsServices.java
@@ -10,18 +10,31 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Map;
+import java.util.List;
+import java.util.Set;
 
+import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilProperties;
 import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.service.DispatchContext;
 import org.ofbiz.service.ServiceUtil;
 
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.entity.util.EntityUtil;
+import org.ofbiz.entity.condition.EntityConditionList;
+import org.ofbiz.entity.util.EntityFindOptions;
+import org.ofbiz.entity.util.EntityListIterator;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+
 public class SmsServices {
     public final static String module = SmsServices.class.getName();
 
 	   /**
-     * Basic JavaMail Service
+     * Basic SMS Service
      *@param ctx The DispatchContext that this service is operating in
      *@param context Map containing the input parameters
      *@return Map with the result of the service, the output parameters
@@ -29,6 +42,7 @@ public class SmsServices {
 	 * @throws UnsupportedEncodingException 
      */
     public static Map<String, Object> sendSms(DispatchContext ctx, Map<String, ? extends Object> context) {
+        Delegator delegator = ctx.getDelegator();    	
 		String postData="";
 		String retval = "";
         String communicationEventId = (String) context.get("communicationEventId");
@@ -49,6 +63,43 @@ public class SmsServices {
 		String mtype = UtilProperties.getPropertyValue("general.properties", "sms.mtype");
 		String DR = UtilProperties.getPropertyValue("general.properties", "sms.DR");
 		String gateway = UtilProperties.getPropertyValue("general.properties", "sms.gateway.url");		
+		String allowedtestlist = UtilProperties.getPropertyValue("general.properties", "sms.allowedtestlist");	
+		if (UtilValidate.isNotEmpty(allowedtestlist)) {
+            boolean allowSms = false;
+			try {	
+	            List<EntityCondition> conditionList = UtilMisc.toList(
+	                    EntityCondition.makeCondition("contactListId", EntityOperator.EQUALS, allowedtestlist),
+	                    EntityCondition.makeCondition("statusId", EntityOperator.EQUALS, "CLPT_ACCEPTED"),
+	                    EntityCondition.makeCondition("preferredContactMechId", EntityOperator.NOT_EQUAL, null),
+	                    EntityUtil.getFilterByDateExpr(), EntityUtil.getFilterByDateExpr("contactFromDate", "contactThruDate"));
+	
+	            EntityConditionList<EntityCondition> conditions = EntityCondition.makeCondition(conditionList, EntityOperator.AND);
+	            Set<String> fieldsToSelect = UtilMisc.toSet("partyId", "preferredContactMechId", "fromDate", "contactNumber", "countryCode");
+	            EntityListIterator eli = null;
+	            eli = delegator.find("ContactListPartyAndContactMechAndTelecomNumber", conditions, null, fieldsToSelect, null,
+	            		new EntityFindOptions(true, EntityFindOptions.TYPE_SCROLL_INSENSITIVE, EntityFindOptions.CONCUR_READ_ONLY, true));
+	            for (GenericValue contactListPartyAndContactMech; (contactListPartyAndContactMech = eli.next()) != null;) {
+	Debug.logInfo("Contact info: " + contactListPartyAndContactMech, module);
+	                String contactListItemNo = contactListPartyAndContactMech.getString("countryCode") + 
+	                	contactListPartyAndContactMech.getString("contactNumber");
+	                if (UtilValidate.isNotEmpty(contactListItemNo) && contactListItemNo.equals(mobileNumber)) {
+	                    allowSms = true;
+	                    break;
+	                }
+	            }
+			}
+			catch (GenericEntityException gee) {
+				// nothing more to do here, sms will not be sent anyways
+				Debug.logError(gee, module);				
+			}
+            if (allowSms == false) {
+            	// don't send sms, just return
+	            String errMsg = "Not sending sms to " + "[" + mobileNumber + "]. Test sms not enabled for this number. ";
+				Debug.logError(errMsg, module);
+	            return ServiceUtil.returnError(errMsg);	            	
+            }
+		}
+
         Debug.logInfo("sendSms: contactNumberTo=" + mobileNumber, module);
 
 		
