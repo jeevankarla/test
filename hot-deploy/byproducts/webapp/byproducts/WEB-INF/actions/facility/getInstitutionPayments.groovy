e2e4a1cf448080ea0bfc23e6faac610bfd073818
charan <charan@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-06-17 08:06:07 +0000

    institution payments
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@1157 40d85018-ee8e-4105-aa2c-066e80ea2389

---
+++ hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/facility/getInstitutionPayments.groovy
@@ -0,0 +1,182 @@
+import org.ofbiz.base.util.UtilDateTime;
+
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import net.sf.json.JSONObject;
+import javolution.util.FastList;
+import org.ofbiz.base.util.*;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONArray;
+import in.vasista.vbiz.byproducts.ByProductNetworkServices;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.entity.util.EntityUtil;
+import org.ofbiz.entity.util.EntityFindOptions;
+
+dctx = dispatcher.getDispatchContext();
+Map boothsPaymentsDetail = [:];
+paymentDate ="";
+paymentIds = FastList.newInstance();
+boothRouteIdsMap=[:];
+onlyCurrentDues = Boolean.TRUE;
+duesByParty = Boolean.TRUE;
+bankName = "";
+if(parameters.paymentDate){
+	paymentDate = parameters.paymentDate;
+}else{
+	paymentDate = context.paymentDate;
+}
+
+if(parameters.statusId){
+	statusId = parameters.statusId;
+}else{
+	statusId = context.statusId;
+}
+facilityId = "";
+if(parameters.facilityId){
+	facilityId = parameters.facilityId;
+}else{
+	facilityId = context.facilityId;
+}
+condList = [];
+if(facilityId != null){
+	facilityDetail = delegator.findOne("Facility", UtilMisc.toMap("facilityId", facilityId), false);
+	if(facilityDetail && facilityDetail.categoryTypeEnum != "CR_INST"){
+		Debug.logInfo(facilityId+" is not a credit institution '","");
+		context.errorMessage = facilityId+" is not a credit institution '";
+		return;
+	}
+	ownerPartyId = facilityDetail.ownerPartyId;
+	partyProfileDetail = delegator.findList("PartyProfileDefault", EntityCondition.makeCondition("partyId", EntityOperator.EQUALS, ownerPartyId), null, null, null, false);
+	partyProfileDetail = EntityUtil.filterByDate(partyProfileDetail, UtilDateTime.nowTimestamp());
+	if(partyProfileDetail){
+		partyPayType = EntityUtil.getFirst(partyProfileDetail);
+		partyPayMeth = partyPayType.defaultPayMeth;
+		if(partyPayMeth){
+			condList.add(EntityCondition.makeCondition("ownerPartyId", EntityOperator.EQUALS, ownerPartyId));
+			condList.add(EntityCondition.makeCondition("finAccountTypeId", EntityOperator.EQUALS, "BANK_ACCOUNT"));
+			condList.add(EntityCondition.makeCondition("statusId", EntityOperator.EQUALS, "FNACT_ACTIVE"));
+			accCond = EntityCondition.makeCondition(condList, EntityOperator.AND);
+			finAccounts = delegator.findList("FinAccount", accCond, null, null, null ,false);
+			if(finAccounts){
+				finAccount = EntityUtil.getFirst(finAccounts);
+				context.chequeBankName = finAccount.finAccountName;
+			}
+		}
+	}
+}
+hideSearch = "Y";
+isCreditParty = false;
+facilityIdList = [];
+condList = [];
+if(facilityId){
+	condList.add(EntityCondition.makeCondition("facilityId", EntityOperator.EQUALS, facilityId));
+}
+cond = EntityCondition.makeCondition(condList, EntityOperator.AND);
+facilityCustomBill = delegator.findList("FacilityCustomBilling", cond, null, null, null, false);
+facilityBills = EntityUtil.filterByDate(facilityCustomBill, UtilDateTime.nowTimestamp());
+facilityIdList = EntityUtil.getFieldListFromEntityList(facilityBills, "facilityId", true);
+
+Timestamp paymentTimestamp = UtilDateTime.nowTimestamp();
+
+SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+try {
+	paymentTimestamp = UtilDateTime.toTimestamp(dateFormat.parse(paymentDate));
+} catch (ParseException e) {
+	Debug.logError(e, "Cannot parse date string: " + paymentDate, "");
+   
+}
+if(parameters.hideSearch){
+	hideSearch = parameters.hideSearch;
+}
+if(parameters.onlyCurrentDues){
+	onlyCurrentDues = Boolean.parseBoolean(parameters.onlyCurrentDues);
+}
+Debug.log("only current dues #########################3"+onlyCurrentDues);
+if(statusId =="PAID"){
+	invoiceStatusId = "INVOICE_PAID";	
+}else{
+	invoiceStatusId = null;
+}
+if(parameters.paymentIds){
+	paymentIds = parameters.paymentIds;	
+}
+
+boothPaymentsList=[];
+
+if(hideSearch == "N"){
+	if (statusId == "PAID") {
+		boothsPaymentsDetail = ByProductNetworkServices.getBoothPaidPayments( dctx , [paymentDate:paymentDate , facilityId:facilityId, paymentIds : paymentIds, isByParty: duesByParty]);
+		boothTempPaymentsList = boothsPaymentsDetail["paymentsList"];
+		boothRouteIdsMap=boothsPaymentsDetail["boothRouteIdsMap"];
+	}
+	else {
+		boothsPaymentsDetail = ByProductNetworkServices.getBoothPayments( delegator ,dispatcher, userLogin ,paymentDate , invoiceStatusId,facilityId ,paymentMethodTypeId , onlyCurrentDues,duesByParty, Boolean.TRUE);
+		boothTempPaymentsList = boothsPaymentsDetail["boothPaymentsList"];
+	}
+	if(statusId != "PAID" && boothTempPaymentsList.size()== 0){
+		tempMap = [:];
+		tempMap.facilityId = eachRetailer;
+		tempMap.routeId = "";
+		tempMap.paymentMethodTypeDesc = "";
+		tempMap.grandTotal = 0;
+		tempMap.totalDue = 0;
+		tempMap.supplyDate = UtilDateTime.getDayStart(UtilDateTime.nowTimestamp());
+		boothTempPaymentsList.add(tempMap);
+	}
+	Debug.log("boothTempPaymentsList AASSSDDD#####################################"+boothTempPaymentsList);
+	boothPaymentsInnerList = [];
+	boothPaymentsList = [];
+	invoicesTotalAmount =0;
+	invoicesTotalDueAmount = 0;
+	exclList = [];
+	if (statusId != "PAID" ) {
+		
+		boothTempPaymentsList.each{boothPay ->
+			facilityId = boothPay.get("facilityId");
+			boothPaymentsInnerList.add(boothPay);
+			invoicesTotalAmount = invoicesTotalAmount+boothPay.get("grandTotal");
+			invoicesTotalDueAmount = invoicesTotalDueAmount+boothPay.get("totalDue");
+		}
+		
+		boothPaymentsList.addAll(boothPaymentsInnerList);
+		boothsPaymentsDetail["invoicesTotalAmount"] =invoicesTotalAmount;
+		boothsPaymentsDetail["invoicesTotalDueAmount"] =invoicesTotalDueAmount;
+	}
+	
+	if(statusId == "PAID"){
+	    boothPaymentsInnerList = boothTempPaymentsList;
+	}
+	context.boothPaymentsList = boothPaymentsInnerList;	
+	context.paymentDate= paymentDate;
+	context.paymentTimestamp= paymentTimestamp;
+	if(boothsPaymentsDetail["invoicesTotalDueAmount"] != null){
+		context.invoicesTotalDueAmount = UtilFormatOut.formatCurrency(boothsPaymentsDetail["invoicesTotalDueAmount"], context.get("currencyUomId"), locale);
+	}
+	// now get the past dues breakup
+		
+}
+condList.clear();
+condList.add(EntityCondition.makeCondition("attrName", EntityOperator.EQUALS, "COLLECTION_ACCOUNT"));
+condList.add(EntityCondition.makeCondition("attrValue", EntityOperator.EQUALS, "Y"));
+cond = EntityCondition.makeCondition(condList, EntityOperator.AND);
+finAccountAttr = delegator.findList("FinAccountAttribute", cond, UtilMisc.toSet("finAccountId"), null, null, false);
+collectionAccIds = EntityUtil.getFieldListFromEntityList(finAccountAttr, "finAccountId", true);
+//Debug.log("collectionAccIds ########################"+collectionAccIds);
+condList.clear();
+if(collectionAccIds){
+	condList.add(EntityCondition.makeCondition("finAccountId", EntityOperator.NOT_IN, collectionAccIds));
+}
+condList.add(EntityCondition.makeCondition("ownerPartyId", EntityOperator.EQUALS, "Company"));
+condList.add(EntityCondition.makeCondition("statusId", EntityOperator.EQUALS, "FNACT_ACTIVE"));
+condList.add(EntityCondition.makeCondition("finAccountTypeId", EntityOperator.EQUALS, "BANK_ACCOUNT"));
+finAccCond = EntityCondition.makeCondition(condList, EntityOperator.AND);
+fundTransferAccounts = delegator.findList("FinAccount", finAccCond, null, null, null, false);
+//Debug.log("fundTransferAccounts #########################################"+fundTransferAccounts);
+context.fundTransferAccounts = fundTransferAccounts;
+context.statusId = statusId;
+context.boothRouteIdsMap=boothRouteIdsMap;
+
+
---
+++ hot-deploy/byproducts/webapp/byproducts/reports/printInstitutionPayments.ftl
@@ -0,0 +1,127 @@
+<#--
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+-->
+<script type="text/javascript">
+//<![CDATA[	
+    function togglePaymentId(master) {
+        var facilities = jQuery("#listBooths :checkbox[name='paymentIds']");
+
+        jQuery.each(facilities, function() {
+            this.checked = master.checked;
+        });
+        //getBoothRunningTotal();
+    }
+    
+    function setVoidPaymentParameters(currentPayment){
+    	jQuery(currentPayment).attr( "disabled", "disabled");
+    	var currentEle = jQuery(currentPayment);
+    	
+    	formName=document.forms['cancelPayment'];
+    	var domObj = $(currentEle).parent().parent();
+        var rowObj = $(domObj).html();
+        var method = $(domObj).find("#paymentMethodTypeId");
+        var payment = $(domObj).find("#paymentId");
+        var tabItemObj = $(domObj).find("#tabItem");
+        var methodValue = $(method).val();
+        var payId = $(payment).val(); 
+        var tabValue = $(tabItemObj).val(); 
+        var appendStr = "<input type=hidden name=paymentMethodTypeId value="+methodValue+" />";  
+        $("#cancelPayment").append(appendStr);  
+        appendStr = "<input type=hidden name=subTabItem value="+tabValue+" />";
+        $("#cancelPayment").append(appendStr);  	    	
+    	appendStr = "<input type=hidden name=paymentId value="+payId+" />";    		
+	    $("#cancelPayment").append(appendStr); 
+	     $("#cancelPayment").submit();	
+	   
+    
+    }
+    function setPrintPaymentParameters(currentPayment){
+    	jQuery(currentPayment).attr( "disabled", "disabled");
+    	var currentEle = jQuery(currentPayment);
+    	formName=document.forms['listBooths'];	
+		var hiddenpaymentId = document.createElement("input");
+	    hiddenpaymentId.setAttribute("type", "hidden");
+	    hiddenpaymentId.setAttribute("name", "paymentId");	   
+	    hiddenpaymentId.setAttribute("value", currentEle.parent().siblings(":nth-child(1)").children(":first-child").val());
+	    formName.appendChild(hiddenpaymentId);
+	    formName.submit();		  
+    
+    }
+    
+//]]>
+</script>
+
+<#if boothPaymentsList?has_content>
+  <form name="cancelPayment" id="cancelPayment"  method="post" action="cancelInstPayment">
+  </form>
+  <form name="listBooths" id="listBooths"  method="post" action="cashReceiptReport.pdf" target="_blank">
+    <div align="right">
+      <#--<input id="submitButton" type="button"  onclick="javascript:jQuery('#listBooths').submit();" value="Print Receipt"/>-->
+    </div>
+
+    <table class="basic-table hover-bar" cellspacing="0">
+      <thead>
+        <tr class="header-row-2">
+          <td/>
+          <td>Institution Code</td>
+          <td>Name</td>
+          <td>Payment Date</td>
+          <td>${uiLabelMap.paymentLocation}</td>
+          <td>${uiLabelMap.Amount}</td>
+          <td>PaymentMethod Type</td>
+          <td>Cancel</td>          
+          <#--<td align="right">${uiLabelMap.CommonSelectAll} <input type="checkbox" id="checkAllFacilities" name="checkAllFacilities" onchange="javascript:togglePaymentId(this);" checked="true"/></td>-->
+        </tr>
+      </thead>
+      <tbody>
+        <#assign alt_row = false>
+        <#list boothPaymentsList as payment>
+        	       
+            <tr valign="middle"<#if alt_row> class="alternate-row"</#if>>
+            	<input type="hidden" name="paymentMethodTypeId" id="paymentMethodTypeId" value="${payment.paymentMethodTypeId?if_exists}">   
+        		<input type="hidden" name="tabItem" id="tabItem" value="${parameters.subTabItem?if_exists}"> 
+            	<td><input id="paymentId" type="hidden"  value="${payment.paymentId?if_exists}"/></td>            
+              <td>${(payment.facilityId)?if_exists}</td>
+              <#assign facility = delegator.findOne("Facility", {"facilityId" : payment.facilityId}, false) />
+              <td>${(facility.facilityName)?if_exists}</td>
+              <td><#if payment.paymentDate?has_content>${Static["org.ofbiz.base.util.UtilDateTime"].toDateString(payment.paymentDate ,"dd/MM/yyyy HH:mm:ss")}</#if></td> 
+               <td>               
+                ${(payment.paymentLocation)?if_exists}
+              </td>
+              <td><@ofbizCurrency amount=payment.amount isoCode=defaultOrganizationPartyCurrencyUomId/></td>
+              <td>  
+              	<#if (payment.paymentMethodTypeId)?has_content>
+              		<#assign paymentMethodType = delegator.findOne("PaymentMethodType", {"paymentMethodTypeId" : payment.paymentMethodTypeId}, true) />
+              		 ${(paymentMethodType.description)?if_exists}
+              	</#if>
+              </td>
+              <td><#if hasPaymentCancelPermission && (payment.paymentMethodTypeId!="AXISHTOH_PAYIN")>
+              			 <input id="submitButton" type="button"  onclick="javascript:setVoidPaymentParameters(this);" value="Cancel"/> 
+              	  </#if>
+              </td>                     
+              <#--<td><input type="checkbox" id="paymentId_${payment_index}" name="paymentIds" value="${payment.paymentId?if_exists}"  checked="true"/></td>-->
+            </tr>
+            <#-- toggle the row color -->
+            <#assign alt_row = !alt_row>
+        </#list>
+      </tbody>
+    </table>
+  </form>
+<#else>
+  <h3>No Payments Found</h3>
+</#if>
