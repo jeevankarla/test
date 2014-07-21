fd4e20eca77b45d1b1695555e8e6d0c3962059af
prathyusha <prathyusha@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-07-21 11:12:37 +0000

    New or terminated report for facility
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@1516 40d85018-ee8e-4105-aa2c-066e80ea2389

---
+++ hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/NewOrTerminateRtlReport.groovy
@@ -0,0 +1,72 @@
+import org.ofbiz.base.util.*;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.util.EntityUtil;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.service.LocalDispatcher;
+import java.text.ParseException;
+import javolution.util.FastList;
+import javolution.util.FastMap;
+import org.ofbiz.party.party.PartyHelper;
+import org.ofbiz.base.util.UtilMisc;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Map;
+import java.text.SimpleDateFormat;
+import javax.swing.text.html.parser.Entity;
+import org.ofbiz.entity.util.EntityFindOptions;
+import org.ofbiz.service.ServiceUtil;
+import org.ofbiz.network.LmsServices;
+dctx = dispatcher.getDispatchContext();
+
+customTimePeriod=delegator.findOne("CustomTimePeriod",[customTimePeriodId : parameters.customTimePeriodId], false);
+fromDateTime=UtilDateTime.toTimestamp(customTimePeriod.getDate("fromDate"));
+thruDateTime=UtilDateTime.toTimestamp(customTimePeriod.getDate("thruDate"));
+context.put("fromDateTime", fromDateTime);
+context.put("thruDateTime", thruDateTime);
+
+monthBegin = UtilDateTime.getDayStart(fromDateTime, timeZone, locale);
+monthEnd = UtilDateTime.getDayEnd(thruDateTime, timeZone, locale);
+printDate = UtilDateTime.toDateString(UtilDateTime.nowTimestamp(), "dd/MM/yyyy");
+context.printDate = printDate;
+conditionList=[];
+conditionList1=[];
+
+conditionList.add(EntityCondition.makeCondition("openedDate", EntityOperator.GREATER_THAN_EQUAL_TO ,monthBegin));
+conditionList.add(EntityCondition.makeCondition("openedDate", EntityOperator.LESS_THAN_EQUAL_TO ,monthEnd));
+conditionList.add(EntityCondition.makeCondition("facilityTypeId",  EntityOperator.EQUALS,"BOOTH"));
+condition = EntityCondition.makeCondition(conditionList,EntityOperator.AND);
+List<GenericValue> facilityList = delegator.findList("Facility", condition, null, null, null, false);
+facilitySaleMap = [:];
+Map SCTMap=FastMap.newInstance();
+if(UtilValidate.isNotEmpty(facilityList)){
+	for(i=0;i<facilityList.size();i++){
+		categoryType=facilityList.get(i).get("categoryTypeEnum");
+		facilityId=facilityList.get(i).get("facilityId");
+		if(UtilValidate.isNotEmpty(categoryType)){
+			if(UtilValidate.isEmpty(facilitySaleMap[categoryType])){
+				tempMap=[:];
+				Map finalMap=FastMap.newInstance();
+				tempMap.putAt("name",facilityList.get(i).get("facilityName"));
+				tempMap.putAt("date",facilityList.get(i).get("openedDate"));
+				if(UtilValidate.isEmpty(finalMap[facilityId])){
+					finalMap.put(facilityId,tempMap);
+					facilitySaleMap.put(categoryType,finalMap);
+				}
+				
+				
+			 }else{
+				 tempMap = [:];
+				 Map finalMap=FastMap.newInstance();
+				 finalMap.putAll(facilitySaleMap.get(categoryType));
+				 tempMap.putAt("name",facilityList.get(i).get("facilityName"));
+				 tempMap.putAt("date",facilityList.get(i).get("openedDate"));
+				 finalMap.put(facilityId,tempMap);
+				 facilitySaleMap[categoryType] = finalMap;
+			 }
+		   }
+	}
+}
+context.facilitySaleMap = facilitySaleMap;
