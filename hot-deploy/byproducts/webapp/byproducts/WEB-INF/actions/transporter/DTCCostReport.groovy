70e2494dd830309575e5a7a4c4013edb6b20c04f
vijay <vijay@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-07-23 10:57:07 +0000

    Dtc cost report for the facility rate and facility size in particular period
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@1550 40d85018-ee8e-4105-aa2c-066e80ea2389

--- hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/transporter/DTCCostReport.groovy
+++ hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/transporter/DTCCostReport.groovy
@@ -27,19 +27,6 @@ import in.vasista.vbiz.byproducts.TransporterServices;
 
 dctx = dispatcher.getDispatchContext();
 
-/*customTimePeriod=delegator.findOne("CustomTimePeriod",[customTimePeriodId : parameters.customTimePeriodId], false);
-fromDateTime=UtilDateTime.toTimestamp(customTimePeriod.getDate("fromDate"));
-thruDateTime=UtilDateTime.toTimestamp(customTimePeriod.getDate("thruDate"));
-
-context.put("fromDateTime", fromDateTime);
-context.put("thruDateTime", thruDateTime);
-
-monthBegin = UtilDateTime.getDayStart(fromDateTime, timeZone, locale);
-monthEnd = UtilDateTime.getDayEnd(thruDateTime, timeZone, locale);
-totalDays=UtilDateTime.getIntervalInDays(monthBegin,monthEnd);
-context.put("totalDays", totalDays+1);
-*/
-
 month = parameters.month;
 if(UtilValidate.isEmpty(month)){
 	Debug.logError("Month Cannot Be Empty","");
@@ -65,138 +52,149 @@ totalDays=UtilDateTime.getIntervalInDays(monthBegin,monthEnd);
 context.put("totalDays", totalDays+1);
 
 routeIdsList = [];
-routeWiseMap =[:];
-
-/*periodBillingId = null;
-conditionList=[];
-conditionList.add(EntityCondition.makeCondition("customTimePeriodId", EntityOperator.EQUALS, parameters.customTimePeriodId));
-conditionList.add(EntityCondition.makeCondition("billingTypeId", EntityOperator.EQUALS, "PB_LMS_TRSPT_MRGN"));
-condition = EntityCondition.makeCondition(conditionList,EntityOperator.AND);
-periodBillingList = delegator.findList("PeriodBilling", condition, null, null, null, false);
-if(UtilValidate.isNotEmpty(periodBillingList)){
-	for (int i = 0; i < periodBillingList.size(); ++i) {
-		periodBillingDetails = periodBillingList.get(i);
-		periodBillingId = periodBillingDetails.periodBillingId;
-	}
-}
-conditionList.clear();
-conditionList.add(EntityCondition.makeCondition("periodBillingId", EntityOperator.EQUALS , periodBillingId));
-conditionList.add(EntityCondition.makeCondition("commissionDate", EntityOperator.GREATER_THAN_EQUAL_TO,monthBegin));
-conditionList.add(EntityCondition.makeCondition("commissionDate", EntityOperator.LESS_THAN_EQUAL_TO,monthEnd));
-condition = EntityCondition.makeCondition(conditionList,EntityOperator.AND);
-EntityFindOptions findOptions = new EntityFindOptions();
-routesList = delegator.findList("FacilityAndCommission",condition,["facilityId"]as Set, UtilMisc.toList("parentFacilityId","facilityId"),findOptions,false);
-routeIdsList = EntityUtil.getFieldListFromEntityList(routesList, "facilityId", false);*/
+finalMap = [:];
 routeIdsList = ByProductNetworkServices.getRoutes(dctx,context).get("routesList");
 if(UtilValidate.isNotEmpty(routeIdsList)){
 	routeIdsList.each{ routeId ->
-		totalSaleQty=0;
-		conditionList=[];
-		conditionList.add(EntityCondition.makeCondition("statusId", EntityOperator.EQUALS , "GENERATED"));
-		conditionList.add(EntityCondition.makeCondition("routeId", EntityOperator.EQUALS , routeId));
-		conditionList.add(EntityCondition.makeCondition("estimatedShipDate", EntityOperator.GREATER_THAN_EQUAL_TO,monthBegin));
-		conditionList.add(EntityCondition.makeCondition("estimatedShipDate", EntityOperator.LESS_THAN_EQUAL_TO,monthEnd));
-		condition = EntityCondition.makeCondition(conditionList,EntityOperator.AND);
-		shipments = delegator.findList("Shipment", condition, null, ["routeId"], null, false);
-		shipmentIds=EntityUtil.getFieldListFromEntityList(shipments, "shipmentId", false);
-		if(UtilValidate.isNotEmpty(shipmentIds)){
-			routeTotals = ByProductNetworkServices.getPeriodTotals(dispatcher.getDispatchContext(), [shipmentIds:shipmentIds,fromDate:monthBegin, thruDate:monthEnd,includeReturnOrders:true]);
-			// Populating sales for Milk and Curd products
-			if(UtilValidate.isNotEmpty(routeTotals)){
-				routeProdTotals = routeTotals.get("productTotals");
-				if(UtilValidate.isNotEmpty(routeProdTotals)){
-					routeProdTotals.each{ productValue ->
-						if(UtilValidate.isNotEmpty(productValue)){
-							productId = productValue.getKey();
-							product = delegator.findOne("Product", [productId : productId], false);
-							if("Milk".equals(product.primaryProductCategoryId) || "Curd".equals(product.primaryProductCategoryId)){
-								totalSaleQty = totalSaleQty+productValue.getValue().get("total");
+		routeWiseMap =[:];
+		customTimePeriodId = null;
+		conditionList = [];
+		conditionList.add(EntityCondition.makeCondition("isClosed", EntityOperator.EQUALS, "N"));
+		conditionList.add(EntityCondition.makeCondition("periodTypeId", EntityOperator.EQUALS, "DTC_FORTNIGHT_BILL"));
+		conditionList.add(EntityCondition.makeCondition("fromDate", EntityOperator.GREATER_THAN_EQUAL_TO, UtilDateTime.toSqlDate(monthBegin)));
+		conditionList.add(EntityCondition.makeCondition("thruDate", EntityOperator.LESS_THAN_EQUAL_TO ,UtilDateTime.toSqlDate(monthEnd)));
+		condition = EntityCondition.makeCondition(conditionList, EntityOperator.AND);
+		customTimePeriodList = delegator.findList("CustomTimePeriod", condition, null, null, null, true);
+		if (UtilValidate.isNotEmpty(customTimePeriodList)) {
+			customTimePeriodList.each { customTimePeriod ->
+				if (UtilValidate.isNotEmpty(customTimePeriod)) {
+					customTimePeriodId = customTimePeriod.customTimePeriodId;
+					fromDate = UtilDateTime.toTimestamp(customTimePeriod.getDate("fromDate"));
+					thruDate = UtilDateTime.toTimestamp(customTimePeriod.getDate("thruDate"));
+					
+					dayBegin = UtilDateTime.getDayStart(fromDate, timeZone, locale);
+					dayEnd = UtilDateTime.getDayEnd(thruDate, timeZone, locale);
+					
+					totalSaleQty=0;
+					averageDays = 0;
+					conditionList.clear();
+					conditionList.add(EntityCondition.makeCondition("statusId", EntityOperator.EQUALS , "GENERATED"));
+					conditionList.add(EntityCondition.makeCondition("routeId", EntityOperator.EQUALS , routeId));
+					conditionList.add(EntityCondition.makeCondition("estimatedShipDate", EntityOperator.GREATER_THAN_EQUAL_TO,dayBegin));
+					conditionList.add(EntityCondition.makeCondition("estimatedShipDate", EntityOperator.LESS_THAN_EQUAL_TO,dayEnd));
+					condition = EntityCondition.makeCondition(conditionList,EntityOperator.AND);
+					shipments = delegator.findList("Shipment", condition, null, ["routeId"], null, false);
+					shipmentIds=EntityUtil.getFieldListFromEntityList(shipments, "shipmentId", false);
+					averageDays = shipmentIds.size();
+					if(UtilValidate.isNotEmpty(shipmentIds)){
+						routeTotals = ByProductNetworkServices.getPeriodTotals(dispatcher.getDispatchContext(), [shipmentIds:shipmentIds,fromDate:dayBegin, thruDate:dayEnd,includeReturnOrders:true]);
+						// Populating sales for Milk and Curd products
+						if(UtilValidate.isNotEmpty(routeTotals)){
+							routeProdTotals = routeTotals.get("productTotals");
+							if(UtilValidate.isNotEmpty(routeProdTotals)){
+								routeProdTotals.each{ productValue ->
+									if(UtilValidate.isNotEmpty(productValue)){
+										productId = productValue.getKey();
+										product = delegator.findOne("Product", [productId : productId], false);
+										if("Milk".equals(product.primaryProductCategoryId) || "Curd".equals(product.primaryProductCategoryId)){
+											totalSaleQty = totalSaleQty+productValue.getValue().get("total");
+										}
+									}
+								}
 							}
+						}
+					}
+					//populating Facility Rate and Facility Size
+					/*List<GenericValue> facilities = delegator.findList("FacilityPersonAndFinAccount", EntityCondition.makeCondition("facilityId", EntityOperator.EQUALS, routeId), null, null, null, false);
+					facility = EntityUtil.getFirst(facilities);*/
+					Map inputRateAmt =  UtilMisc.toMap("userLogin", userLogin);
+					inputRateAmt.put("rateCurrencyUomId", "INR");
+					inputRateAmt.put("facilityId", routeId);
+					inputRateAmt.put("fromDate",dayBegin);
+					inputRateAmt.put("rateTypeId", "TRANSPORTER_MRGN");
+					facilityRateResult = dispatcher.runSync("getFacilityRateAmount", inputRateAmt);
+					
+					Map inputFacilitySize =  UtilMisc.toMap("userLogin", userLogin);
+					inputFacilitySize.put("rateCurrencyUomId", "LEN_km");
+					inputFacilitySize.put("facilityId", routeId);
+					inputFacilitySize.put("fromDate",dayBegin);
+					inputFacilitySize.put("rateTypeId", "FACILITY_SIZE");
+					facilitySizeResult = dispatcher.runSync("getRouteDistance", inputFacilitySize);
+					
+					if(UtilValidate.isEmpty(routeWiseMap[customTimePeriodId])){
+						tempMap = [:];
+						tempMap["saleQty"] = new BigDecimal(totalSaleQty).setScale(2,BigDecimal.ROUND_HALF_UP);
+						tempMap["facilityRate"] = (BigDecimal) facilityRateResult.get("rateAmount");
+						tempMap["facilitySize"] = (BigDecimal) facilitySizeResult.get("facilitySize");
+						tempMap["averageDays"] = averageDays;
+						if(totalSaleQty != 0){
+							routeWiseMap[customTimePeriodId] = tempMap;
+						}
+					}else{
+						Map tempMap = FastMap.newInstance();
+						tempMap.putAll(routeWiseMap.get(customTimePeriodId));
+						totalQty = 0;
+						totalQty = new BigDecimal(totalSaleQty).setScale(2,BigDecimal.ROUND_HALF_UP);
+						if(UtilValidate.isNotEmpty(totalQty) && totalQty!=0){
+							tempMap["saleQty"] += totalQty;
+						}
+						if(totalQty != 0){
+							routeWiseMap[customTimePeriodId] = tempMap;
 						}
 					}
 				}
 			}
 		}
-		//populating Facility Rate and Facility Size
-		/*List<GenericValue> facilities = delegator.findList("FacilityPersonAndFinAccount", EntityCondition.makeCondition("facilityId", EntityOperator.EQUALS, routeId), null, null, null, false);
-		facility = EntityUtil.getFirst(facilities);*/
-		Map inputRateAmt =  UtilMisc.toMap("userLogin", userLogin);
-		inputRateAmt.put("rateCurrencyUomId", "INR");
-		inputRateAmt.put("facilityId", routeId);
-		inputRateAmt.put("fromDate",monthBegin );
-		inputRateAmt.put("rateTypeId", "TRANSPORTER_MRGN");
-		facilityRateResult = dispatcher.runSync("getFacilityRateAmount", inputRateAmt);
-		
-		Map inputFacilitySize =  UtilMisc.toMap("userLogin", userLogin);
-		inputFacilitySize.put("rateCurrencyUomId", "LEN_km");
-		inputFacilitySize.put("facilityId", routeId);
-		inputFacilitySize.put("fromDate",monthBegin );
-		inputFacilitySize.put("rateTypeId", "FACILITY_SIZE");
-		facilitySizeResult = dispatcher.runSync("getRouteDistance", inputFacilitySize);
-		
-		if(UtilValidate.isEmpty(routeWiseMap[routeId])){
-			tempMap = [:];
-			tempMap["saleQty"] = new BigDecimal(totalSaleQty).setScale(2,BigDecimal.ROUND_HALF_UP);
-			tempMap["facilityRate"] = (BigDecimal) facilityRateResult.get("rateAmount");
-			tempMap["facilitySize"] = (BigDecimal) facilitySizeResult.get("facilitySize");
-			if(totalSaleQty != 0){
-				routeWiseMap[routeId] = tempMap;
-			}
-		}else{
-			Map tempMap = FastMap.newInstance();
-			tempMap.putAll(routeWiseMap.get(routeId));
-			totalQty = 0;
-			totalQty = new BigDecimal(totalSaleQty).setScale(2,BigDecimal.ROUND_HALF_UP);
-			if(UtilValidate.isNotEmpty(totalQty) && totalQty!=0){
-				tempMap["saleQty"] += totalQty;
-			}
-			if(totalQty != 0){
-				routeWiseMap[routeId] = tempMap;
-			}
+		if(UtilValidate.isNotEmpty(routeWiseMap)){
+			finalMap.put(routeId,routeWiseMap);
 		}
 	}
 }
-context.put("dTCCostMap",routeWiseMap);
+context.put("dTCCostMap",finalMap);
 // for DTC Cost Report CSV
 dtcCostReportCsvList = [];
-if(UtilValidate.isNotEmpty(routeWiseMap)){
-	routeWiseMap.each { route->
-		totalDaysCsv = totalDays + 1;
-		dtcCostCsvMap = [:];
-		payment = 0;
-		averageSaleQty= 0;
-		dtcCostCsvMap["route"] = route.getKey();
-		dtcCostCsvMap["routeSaleQty"] = route.getValue().get("saleQty");
-		dtcCostCsvMap["averageSaleQty"] = ((route.getValue().get("saleQty"))/(totalDaysCsv));
-		dtcCostCsvMap["routeFacilitySize"] = route.getValue().get("facilitySize");
-		dtcCostCsvMap["routeFacilityRate"] = route.getValue().get("facilityRate");
-		if(UtilValidate.isNotEmpty(route.getValue().get("facilitySize"))){
-			payment = (route.getValue().get("facilitySize")*route.getValue().get("facilityRate"));
-			dtcCostCsvMap["payment"] = payment;
-		}else{
-			payment = route.getValue().get("facilityRate");
-			dtcCostCsvMap["payment"] = payment;
+if(UtilValidate.isNotEmpty(finalMap)){
+	Iterator mapIter = finalMap.entrySet().iterator();
+	while (mapIter.hasNext()) {
+		Map.Entry entry = mapIter.next();
+		routeId = entry.getKey();
+		routeValues = entry.getValue();
+		Iterator routeMapIter = routeValues.entrySet().iterator();
+		while (routeMapIter.hasNext()) {
+			Map.Entry routeEntry = routeMapIter.next();
+			periodId = routeEntry.getKey();
+			payment = 0;
+			averageSaleQty= 0;
+			dtcCostCsvMap = [:];
+			dtcCostCsvMap["route"] = routeId;
+			customTimePeriod=delegator.findOne("CustomTimePeriod",[customTimePeriodId : periodId], false);
+			fromDateTime=UtilDateTime.toTimestamp(customTimePeriod.getDate("fromDate"));
+			thruDateTime=UtilDateTime.toTimestamp(customTimePeriod.getDate("thruDate"));
+			fromDateStr = UtilDateTime.toDateString(fromDateTime,"MMM(dd-");
+			thruDateStr = UtilDateTime.toDateString(thruDateTime,"dd)");
+			dtcCostCsvMap["periodId"] = fromDateStr+thruDateStr;
+			dtcCostCsvMap["routeSaleQty"] = routeEntry.getValue().get("saleQty");
+			averageDays = routeEntry.getValue().get("averageDays");
+			if(UtilValidate.isNotEmpty(averageDays)){
+				dtcCostCsvMap["averageSaleQty"] = (routeEntry.getValue().get("saleQty")/averageDays);
+			}
+			dtcCostCsvMap["routeFacilitySize"] = routeEntry.getValue().get("facilitySize");
+			dtcCostCsvMap["routeFacilityRate"] = routeEntry.getValue().get("facilityRate");
+			if(UtilValidate.isNotEmpty(routeEntry.getValue().get("facilitySize"))){
+				payment = (routeEntry.getValue().get("facilitySize")*routeEntry.getValue().get("facilityRate"));
+				dtcCostCsvMap["payment"] = payment;
+			}else{
+				payment = routeEntry.getValue().get("facilityRate");
+				dtcCostCsvMap["payment"] = payment;
+			}
+			averageSaleQty = (routeEntry.getValue().get("saleQty")/averageDays);
+			if(averageSaleQty!=0){
+				dtcCostCsvMap["cost"] = ((payment/averageSaleQty).setScale(2,BigDecimal.ROUND_HALF_UP));
+			}
+			tempMap = [:];
+			tempMap.putAll(dtcCostCsvMap);
+			dtcCostReportCsvList.add(tempMap);
 		}
-		averageSaleQty = ((route.getValue().get("saleQty"))/(totalDaysCsv));
-		if(averageSaleQty!=0){
-			dtcCostCsvMap["cost"] = ((payment/averageSaleQty).setScale(2,BigDecimal.ROUND_HALF_UP));
-		}
-		tempMap = [:];
-		tempMap.putAll(dtcCostCsvMap);
-		dtcCostReportCsvList.add(tempMap);
 	}
 }
 context.put("dtcCostReportCsvList",dtcCostReportCsvList);
-
-
-
-
-
-
-
-
-
-
-
-
-
--- hot-deploy/byproducts/webapp/byproducts/transporter/DTCCostReport.fo.ftl
+++ hot-deploy/byproducts/webapp/byproducts/transporter/DTCCostReport.fo.ftl
@@ -22,8 +22,8 @@ under the License.
 
 <#-- do not display columns associated with values specified in the request, ie constraint values -->
 <fo:layout-master-set>
-	<fo:simple-page-master master-name="main" page-height="12in" page-width="8in"  margin-left=".3in" margin-right=".3in" margin-bottom=".3in" margin-top=".5in">
-        <fo:region-body margin-top="0.3in"/>
+	<fo:simple-page-master master-name="main" page-height="12in" page-width="10in"  margin-left=".3in" margin-right=".3in" margin-bottom=".3in" margin-top=".3in">
+        <fo:region-body margin-top="0.2in"/>
         <fo:region-before extent="1in"/>
         <fo:region-after extent="1in"/>        
     </fo:simple-page-master>   
@@ -41,57 +41,68 @@ ${setRequestAttribute("OUTPUT_FILENAME", "DtcCostReport.pdf")}
 					<fo:block  keep-together="always" text-align="center" font-family="Courier,monospace" white-space-collapse="false" font-weight="bold">&#160;      ${uiLabelMap.KMFDairySubHeader}</fo:block>
 					<fo:block  keep-together="always" text-align="center" font-family="Courier,monospace" white-space-collapse="false" font-weight="bold" font-size="11pt">DISTRIBUTION TRANSPORTATION COST PER LTR OF MILK AND CURD SOLD </fo:block>
               		<fo:block  keep-together="always" text-align="center" font-family="Courier,monospace" white-space-collapse="false" font-weight="bold" font-size="11pt">BETWEEN ${Static["org.ofbiz.base.util.UtilDateTime"].toDateString(monthBegin, "dd/MMMM/yyyy")} to ${Static["org.ofbiz.base.util.UtilDateTime"].toDateString(monthEnd, "dd/MMMM/yyyy")}</fo:block>
-              		<fo:block font-size="8pt">------------------------------------------------------------------------------------------------------------</fo:block>
-              		<fo:block font-size="9pt" font-weight="bold">Route No    								&#160;&#160;Total Sales*  					&#160;&#160;&#160;&#160;Avg Sales  			&#160;&#160;Distance Per Day  			&#160;&#160;&#160;&#160;&#160;Rate  			&#160;&#160;&#160;&#160;&#160;Cost Per Day     &#160;&#160;&#160; Cost/Ltr</fo:block>
-            		<fo:block font-size="8pt">------------------------------------------------------------------------------------------------------------</fo:block>
+              		<fo:block font-size="8pt">----------------------------------------------------------------------------------------------------------------------------------------------</fo:block>
+              		<fo:block font-size="9pt" font-weight="bold">Route No    		&#160;&#160;&#160;&#160;&#160;&#160;Period						&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Total Sales*  			&#160;&#160;&#160;&#160;Avg Sales  				&#160;&#160;&#160;&#160;&#160;Distance Per Day  			&#160;&#160;&#160;&#160;&#160;&#160;Rate  			&#160;&#160;&#160;&#160;Cost Per Day     &#160;&#160;&#160;Cost/Ltr</fo:block>
+            		<fo:block font-size="8pt">----------------------------------------------------------------------------------------------------------------------------------------------</fo:block>
             	<fo:block>
                  	<fo:table>
                     <fo:table-column column-width="60pt"/>
-                    <fo:table-column column-width="60pt"/>
-                    <fo:table-column column-width="80pt"/> 
-               	    <fo:table-column column-width="70pt"/>
+                    <fo:table-column column-width="80pt"/>
+                    <fo:table-column column-width="90pt"/> 
+               	    <fo:table-column column-width="90pt"/>
             		<fo:table-column column-width="95pt"/> 		
-            		<fo:table-column column-width="80pt"/>
-            		<fo:table-column column-width="70pt"/>
+            		<fo:table-column column-width="90pt"/>
+            		<fo:table-column column-width="75pt"/>
+            		<fo:table-column column-width="65pt"/>
                     <fo:table-body>
                     
                     <#assign totalAverageSaleQty =0 >
                     <#assign totalPayment =0 >
                     
-                    <#assign routeDetails = dTCCostMap.entrySet()>
-                    <#list routeDetails as eachRoute>
-                    <#assign saleQty = 0>
-                    <#assign averageSaleQty = 0>
-                    <#assign facilitySize = 0>
-                    <#assign facilityRate = 0>
-                    <#assign payment = 0>
-                    <#assign costPerLtr = 0>
-                    
-                    <#assign saleQty = eachRoute.getValue().get("saleQty")?if_exists>
-                    <#if (totalDays!=0)>
-                    	<#assign averageSaleQty = (saleQty/totalDays)>
-                    </#if>
-                    <#assign facilitySize = eachRoute.getValue().get("facilitySize")?if_exists>
-                    <#assign facilityRate = eachRoute.getValue().get("facilityRate")?if_exists>
-                    <#if facilitySize?has_content && facilitySize!=0>
-                    	<#assign payment = (facilitySize?if_exists*facilityRate?if_exists)>
-                    <#else>
-                    	<#assign payment = facilityRate?if_exists>
-                    </#if>
-                     <#if averageSaleQty?has_content && (averageSaleQty != 0)>
-                    	<#assign costPerLtr = (payment/averageSaleQty)>
-                    </#if>
-                    <#assign totalAverageSaleQty = totalAverageSaleQty+averageSaleQty>
-                    <#assign totalPayment = totalPayment+payment>	
-						<fo:table-row>
+                    <#assign periodDetails = dTCCostMap.entrySet()>
+                    <#list periodDetails as eachPeriod>
+                    	  <#assign routeDetails = eachPeriod.getValue().entrySet()>
+                    	   <#list routeDetails as eachRoute>
+                    	   	<#assign saleQty = 0>
+		                    <#assign averageSaleQty = 0>
+		                    <#assign facilitySize = 0>
+		                    <#assign facilityRate = 0>
+		                    <#assign payment = 0>
+		                    <#assign costPerLtr = 0>
+		                    
+		                    <#assign saleQty = eachRoute.getValue().get("saleQty")?if_exists>
+		                    <#assign averageDays = eachRoute.getValue().get("averageDays")?if_exists>
+		                    <#if (averageDays!=0)>
+		                    	<#assign averageSaleQty = (saleQty/averageDays)>
+		                    </#if>
+		                    <#assign facilitySize = eachRoute.getValue().get("facilitySize")?if_exists>
+		                    <#assign facilityRate = eachRoute.getValue().get("facilityRate")?if_exists>
+		                    <#if facilitySize?has_content && facilitySize!=0>
+		                    	<#assign payment = (facilitySize?if_exists*facilityRate?if_exists)>
+		                    <#else>
+		                    	<#assign payment = facilityRate?if_exists>
+		                    </#if>
+		                     <#if averageSaleQty?has_content && (averageSaleQty != 0)>
+		                    	<#assign costPerLtr = (payment/averageSaleQty)>
+		                    </#if>
+		                    <#assign totalAverageSaleQty = totalAverageSaleQty+averageSaleQty>
+		                    <#assign totalPayment = totalPayment+payment>
+                    	   
+                    	   	<#assign customTimePeriod = delegator.findOne("CustomTimePeriod", {"customTimePeriodId" : eachRoute.getKey()?if_exists}, true)>
+                    	   	<#assign fromDate = Static["org.ofbiz.base.util.UtilDateTime"].toDateString(customTimePeriod.fromDate, "MMM(dd")/>
+                			<#assign thruDate = Static["org.ofbiz.base.util.UtilDateTime"].toDateString(customTimePeriod.thruDate, "dd)")/>	
+                    	   	<fo:table-row>
                 				<fo:table-cell>
-                            		<fo:block  keep-together="always" text-align="left"  white-space-collapse="false">${eachRoute.getKey()?if_exists}</fo:block>  
+                            		<fo:block  keep-together="always" text-align="left"  white-space-collapse="false">${eachPeriod.getKey()?if_exists}</fo:block>  
+                       			</fo:table-cell>
+                				<fo:table-cell>
+                            		<fo:block  keep-together="always" text-align="left"  white-space-collapse="false">${fromDate?if_exists}-${thruDate?if_exists}</fo:block>  
                        			</fo:table-cell>
                        			<fo:table-cell>
                             		<fo:block  text-align="right"  white-space-collapse="false">${(saleQty?if_exists)?string("#0")}.00</fo:block>  
                        			</fo:table-cell>
                        			<fo:table-cell>
-                            		<fo:block  text-align="right"  white-space-collapse="false"><#if (totalDays!=0)>${(saleQty/totalDays)?if_exists?string("#0.00")}</#if></fo:block>  
+                            		<fo:block  text-align="right"  white-space-collapse="false"><#if (averageDays!=0)>${(saleQty/averageDays)?if_exists?string("#0.00")}</#if></fo:block>  
                        			</fo:table-cell>
                        			 <#if facilitySize?has_content>
                        			<fo:table-cell>
@@ -112,15 +123,19 @@ ${setRequestAttribute("OUTPUT_FILENAME", "DtcCostReport.pdf")}
                             		<fo:block  text-align="right" white-space-collapse="false">${costPerLtr?if_exists?string("#0.00")}</fo:block>  
                        			</fo:table-cell>
             				</fo:table-row>
-                	</#list>
-            				<fo:table-row>
+            				</#list>
+                   </#list>
+                			<fo:table-row>
                     			<fo:table-cell>
-                        			<fo:block font-size="8pt">------------------------------------------------------------------------------------------------------------</fo:block>
+                        			<fo:block font-size="8pt">----------------------------------------------------------------------------------------------------------------------------------------------</fo:block>
                     			</fo:table-cell>
 	                        </fo:table-row>
                 			<fo:table-row font-weight="bold">
                 				<fo:table-cell>
                             		<fo:block  keep-together="always" text-align="left"  white-space-collapse="false">Total Cost/Litre</fo:block>  
+                       			</fo:table-cell>
+                       			<fo:table-cell>
+                            		<fo:block  text-align="right"  white-space-collapse="false"></fo:block>  
                        			</fo:table-cell>
                        			<fo:table-cell>
                             		<fo:block  text-align="right"  white-space-collapse="false"></fo:block>  
@@ -143,7 +158,7 @@ ${setRequestAttribute("OUTPUT_FILENAME", "DtcCostReport.pdf")}
             				</fo:table-row>
             				<fo:table-row>
                         			<fo:table-cell>
-                            			<fo:block font-size="8pt">------------------------------------------------------------------------------------------------------------</fo:block>
+                            			<fo:block font-size="8pt">----------------------------------------------------------------------------------------------------------------------------------------------</fo:block>
                         			</fo:table-cell>
 		                     </fo:table-row>
 		                      <fo:table-row>
@@ -173,7 +188,7 @@ ${setRequestAttribute("OUTPUT_FILENAME", "DtcCostReport.pdf")}
 						  </fo:table-row>
 		                  <fo:table-row>
 			       			<fo:table-cell>
-			            		<fo:block  keep-together="always" font-weight="bold">&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Authorised Signatory</fo:block>  
+			            		<fo:block  keep-together="always" font-weight="bold">&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Authorised Signatory</fo:block>  
 			       			</fo:table-cell>
 						</fo:table-row>
                     </fo:table-body>
