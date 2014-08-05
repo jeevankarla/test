283de9da4cd28e7ddfacf7bfac64ab3914a526e9
naresh <naresh@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-08-05 11:20:50 +0000

    Daily Bus Timings done
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@1687 40d85018-ee8e-4105-aa2c-066e80ea2389

---
+++ applications/party/webapp/partymgr/static/inTime.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function () {
+		jQuery('#DailyBusTimings_inTime').timepicker({ 
+		showSecond: true,	
+		timeFormat: 'hh:mm:ss',
+		showOn: 'button',
+	        buttonImage: '/vasista/images/cal.gif',
+		buttonImageOnly: false
+	       
+
+	 });
+
+});
+
+
