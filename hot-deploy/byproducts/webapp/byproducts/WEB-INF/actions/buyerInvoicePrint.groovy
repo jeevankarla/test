4f9204362f9c1d30d733ab0fb2e5f9f8f94eaf3e
charan <charan@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-08-12 14:13:00 +0000

    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@1834 40d85018-ee8e-4105-aa2c-066e80ea2389

--- hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/buyerInvoicePrint.groovy
+++ hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/buyerInvoicePrint.groovy
@@ -173,7 +173,7 @@ invoiceIds.each { invoiceId ->
 			List prodDetails = EntityUtil.filterByCondition(products, EntityCondition.makeCondition("productId", EntityOperator.EQUALS, eachItem.productId));
 			prodDetail = EntityUtil.getFirst(prodDetails);
 			tempMap.put("productId", eachItem.productId);
-			tempMap.put("itemDescription", prodDetails.description);
+			tempMap.put("itemDescription", prodDetail.description);
 			tempMap.put("quantityLtr", (eachItem.quantity)*prodDetail.quantityIncluded);
 			tempMap.put("quantity", eachItem.quantity);
 			tempMap.put("mrpPrice", mrpPrice);
