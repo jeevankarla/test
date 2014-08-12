bb120d368b0bb84ee0d19343fc2892c9ec00e722
supriya <supriya@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-08-12 14:12:57 +0000

    Payroll Exception Report
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@1833 40d85018-ee8e-4105-aa2c-066e80ea2389

---
+++ applications/humanres/webapp/humanres/WEB-INF/actions/humanres/PayrollExceptionReport.groovy
@@ -0,0 +1,59 @@
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.entity.condition.*;
+import org.ofbiz.base.util.*;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.util.EntityUtil;
+import java.util.*;
+import java.lang.*;
+import org.ofbiz.entity.*;
+import org.ofbiz.entity.condition.*;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import java.sql.*;
+import java.util.Calendar;
+import javolution.util.FastList;
+import javolution.util.FastMap;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import org.ofbiz.service.ServiceUtil;
+import org.ofbiz.base.util.UtilNumber;
+import in.vasista.vbiz.humanres.HumanresService;
+import in.vasista.vbiz.humanres.PayrollService;
+import org.ofbiz.party.party.PartyHelper;
+timePeriod = null;
+if (parameters.customTimePeriodId == null) {
+	return;
+}
+dctx = dispatcher.getDispatchContext();
+GenericValue customTimePeriod = delegator.findOne("CustomTimePeriod", [customTimePeriodId : parameters.customTimePeriodId], false);
+timePeriodStart=UtilDateTime.toTimestamp(customTimePeriod.getDate("fromDate"));
+timePeriodEnd=UtilDateTime.toTimestamp(customTimePeriod.getDate("thruDate"));
+
+resultMap = PayrollService.getPayrollAttedancePeriod(dctx, [timePeriodStart:timePeriodStart, timePeriodEnd: timePeriodEnd, timePeriodId: parameters.customTimePeriodId, userLogin : userLogin]);
+if(UtilValidate.isNotEmpty(resultMap.get("lastCloseAttedancePeriod"))){
+	lastCloseAttedancePeriod=resultMap.get("lastCloseAttedancePeriod");
+	timePeriod=lastCloseAttedancePeriod.get("periodName");
+}
+payrollDetailsMap=[:];
+if(UtilValidate.isNotEmpty(timePeriod)){
+	conditionList=[];
+	conditionList.add(EntityCondition.makeCondition("customTimePeriodId", EntityOperator.EQUALS ,timePeriod));
+	condition = EntityCondition.makeCondition(conditionList,EntityOperator.AND);
+	payrollDetailsList = delegator.findList("PayrollAttendance", condition, null, null, null, false);
+	if(UtilValidate.isNotEmpty(payrollDetailsList)){
+		payrollDetailsList.each { payrollDetails ->
+		noOfPayableDays=payrollDetails.get("noOfPayableDays");
+		if(UtilValidate.isEmpty(noOfPayableDays)){
+			payrollDaysMap= [:];
+			partyId=payrollDetails.get("partyId");
+				payableDays = 0;
+				String partyName = PartyHelper.getPartyName(delegator, partyId, false);
+				payrollDaysMap.put(partyName,payableDays);
+				payrollDetailsMap.put(partyId,payrollDaysMap);
+			}
+		}
+	}
+}
+context.put("payrollDetailsMap",payrollDetailsMap);
