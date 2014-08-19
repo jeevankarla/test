76a2bf2a6f4c2280df2c79920bf48acde9cd8c6d
naresh <naresh@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-08-19 13:26:15 +0000

    modified
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@1922 40d85018-ee8e-4105-aa2c-066e80ea2389

--- applications/humanres/webapp/humanres/WEB-INF/actions/reports/MonthlyAttendenceChecklist.groovy
+++ applications/humanres/webapp/humanres/WEB-INF/actions/reports/MonthlyAttendenceChecklist.groovy
@@ -9,6 +9,7 @@ import org.ofbiz.base.util.UtilDateTime;
 import in.vasista.vbiz.humanres.EmplLeaveService;
 import in.vasista.vbiz.humanres.PayrollService;
 import in.vasista.vbiz.humanres.HumanresApiService;
+import in.vasista.vbiz.humanres.HumanresService;
 
 dctx = dispatcher.getDispatchContext();
 
@@ -32,104 +33,103 @@ try {
 context.fromDate=UtilDateTime.toDateString(fromDate, "MMM dd, yyyy");
 context.thruDate=UtilDateTime.toDateString(thruDate, "MMM dd, yyyy");
 
-employeeList = [];
+emplList = [];
 
-activeEmpMap=HumanresApiService.getActiveEmployees(dctx,[userLogin:userLogin]);
-if(UtilValidate.isNotEmpty(activeEmpMap)){
-	Iterator empIter = activeEmpMap.entrySet().iterator();
-	while(empIter.hasNext()){
-		Map.Entry empEntry = empIter.next();
-		employeesMap=empEntry.getValue();
-		if(UtilValidate.isNotEmpty(employeesMap)){
-			Iterator employeeIter=employeesMap.entrySet().iterator();
-			while(employeeIter.hasNext()){
-				Map.Entry employeeEntry=employeeIter.next();
-				allEmployees=employeeEntry.getValue();
-				allEmployees.each { employee ->
-					employeeMap=[:];
-						employeeMap.put("employeeId",employee.employeeId);
-						employeeMap.put("name",employee.name);
-						empLeaveMap=EmplLeaveService.fetchLeaveDaysForPeriod(dctx,[partyId:employee.employeeId,timePeriodStart:fromDate, timePeriodEnd: thruDate,userLogin : userLogin]);
-						leaveDetailmap=empLeaveMap.get("leaveDetailmap");
-						if(UtilValidate.isNotEmpty(leaveDetailmap)){
-							Iterator leaveIter=leaveDetailmap.entrySet().iterator();
-								while(leaveIter.hasNext()){
-								 Map.Entry leaveEntry=leaveIter.next();
-								 employeeMap.put(leaveEntry.getKey(), leaveEntry.getValue());
-							}
-						}
-						employeePayrollAttedance=PayrollService.getEmployeePayrollAttedance(dctx,[employeeId:employee.employeeId,timePeriodStart:fromDate, timePeriodEnd: thruDate,userLogin : userLogin]);
-						Debug.log("employeePayrollAttedance======================"+employeePayrollAttedance);
-						shiftDetails=employeePayrollAttedance.get("shiftDetailMap");
-						if(UtilValidate.isNotEmpty(shiftDetails)){
-							Iterator shiftIter=shiftDetails.entrySet().iterator();
-								while(shiftIter.hasNext()){
-								 Map.Entry shiftEntry=shiftIter.next();
-								 employeeMap.put(shiftEntry.getKey(),shiftEntry.getValue());
-							}
-						}
-						employeeMap.put("workedHolidays",employeePayrollAttedance.get("noOfAttendedHoliDays"));
-						employeeMap.put("workedSsDays",employeePayrollAttedance.get("noOfAttendedSsDays"));
-						employeeMap.put("payableDays",employeePayrollAttedance.get("noOfPayableDays"));
-						employeeMap.put("arrearDays",employeePayrollAttedance.get("noOfArrearDays"));
-						employeeMap.put("lateMin", employeePayrollAttedance.get("lateMin"));
-						employeeMap.put("extraMin", employeePayrollAttedance.get("extraMin"));
-						leaveBalances = delegator.findByAnd("EmplLeaveBalanceStatus",[partyId:employee.employeeId],["leaveTypeId"]);
-						leaveBalances.each{ leaveType ->
-							if(leaveType.leaveTypeId=="CL")
-							employeeMap.put("BCL", leaveType.openingBalance);
-							if(leaveType.leaveTypeId=="EL")
-							employeeMap.put("BEL", leaveType.openingBalance);
-							if(leaveType.leaveTypeId=="HPL")
-							employeeMap.put("BHP", leaveType.openingBalance);
-						}
-						nowDate=UtilDateTime.nowTimestamp();
-						nowfromDate = UtilDateTime.getMonthStart(nowDate);
-						nowthruDate = UtilDateTime.getMonthEnd(nowDate,timeZone,locale);
-						resultMap=dispatcher.runSync("getCustomTimePeriodId", [periodTypeId:"HR_MONTH",fromDate:nowfromDate,thruDate:nowthruDate,userLogin:userLogin]);
-							if(UtilValidate.isNotEmpty(resultMap)){
-							cldAmount=0;
-							cldDays=0;
-							cldAmountList=casteIds=delegator.findByAnd("PartyBenefit", [partyIdTo:employee.employeeId,benefitTypeId:"PAYROL_BEN_COLDALLOW"],["benefitTypeId"]);
-							if(UtilValidate.isNotEmpty(cldAmountList)){
-								cldAmountIds=cldAmountList.get(0).benefitTypeId;
-								cldAmountMap=PayrollService.getPayHeadAmount(dctx,[userLogin:userLogin,payHeadTypeId:cldAmountIds,employeeId:employee.employeeId,customTimePeriodId:resultMap.get("customTimePeriodId"),locale:locale]);
-								cldAmount=cldAmountMap.get("amount");
-								if(UtilValidate.isNotEmpty(cldAmount) && cldAmount!=null)
-									cldDays=employeePayrollAttedance.get("noOfPayableDays");
-							}
-							employeeMap.put("cldDays",cldDays);
-							caAmount=0;
-							caDays=0;
-							caAmountList=casteIds=delegator.findByAnd("PartyBenefit", [partyIdTo:employee.employeeId,benefitTypeId:"PAYROL_BEN_CASH"],["benefitTypeId"]);
-							if(UtilValidate.isNotEmpty(caAmountList)){
-								caAmountIds=caAmountList.get(0).benefitTypeId;
-								caAmountMap=PayrollService.getPayHeadAmount(dctx,[userLogin:userLogin,payHeadTypeId:caAmountIds,employeeId:employee.employeeId,customTimePeriodId:resultMap.get("customTimePeriodId"),locale:locale]);
-								caAmount=caAmountMap.get("amount");
-								if(UtilValidate.isNotEmpty(caAmount) && caAmount!=null)
-								caDays=employeePayrollAttedance.get("noOfPayableDays");
-							}
-							employeeMap.put("caDays",caDays);
-							heatAmount=0;
-							heatDays=0;
-							heatAmountList=casteIds=delegator.findByAnd("PartyBenefit", [partyIdTo:employee.employeeId,benefitTypeId:"PAYROL_BEN_HEATALLOW"],["benefitTypeId"]);
-							
-							if(UtilValidate.isNotEmpty(heatAmountList)){
-								heatAmountIds=heatAmountList.get(0).benfitTypeId;
-								heatAmountMap=PayrollService.getPayHeadAmount(dctx,[userLogin:userLogin,payHeadTypeId:heatAmountIds,employeeId:employee.employeeId,customTimePeriodId:resultMap.get("customTimePeriodId"),locale:locale]);
-								heatAmount=heatAmountMap.get("amount");
-								if(UtilValidate.isNotEmpty(heatAmount) && heatAmount!=null)
-								heatDays=employeePayrollAttedance.get("noOfPayableDays");
-							}
-							employeeMap.put("heatDays",heatDays);
-						}
-					employeeList.add(employeeMap);
-					}
-				
+
+def populateChildren(org, employeeList) {
+	EmploymentsMap=HumanresService.getActiveEmployements(dctx,[userLogin:userLogin,orgPartyId:"Company"]);
+	employments=EmploymentsMap.get("employementList");
+	employments.each { employee ->
+			employeeMap=[:];
+			employeeMap.put("employeeId",employee.partyId);
+			String lastName="";
+			if(employee.lastName!=null){
+				lastName=employee.lastName;
+			}
+			name=employee.firstName + " " + lastName
+			employeeMap.put("name",name);
+			empLeaveMap=EmplLeaveService.fetchLeaveDaysForPeriod(dctx,[partyId:employee.partyId,timePeriodStart:fromDate, timePeriodEnd: thruDate,userLogin : userLogin]);
+			leaveDetailmap=empLeaveMap.get("leaveDetailmap");
+			if(UtilValidate.isNotEmpty(leaveDetailmap)){
+				Iterator leaveIter=leaveDetailmap.entrySet().iterator();
+					while(leaveIter.hasNext()){
+					 Map.Entry leaveEntry=leaveIter.next();
+					 employeeMap.put(leaveEntry.getKey(), leaveEntry.getValue());
 				}
 			}
+			employeePayrollAttedance=PayrollService.getEmployeePayrollAttedance(dctx,[employeeId:employee.partyId,timePeriodStart:fromDate, timePeriodEnd: thruDate,userLogin : userLogin]);
+			shiftDetails=employeePayrollAttedance.get("shiftDetailMap");
+			if(UtilValidate.isNotEmpty(shiftDetails)){
+				Iterator shiftIter=shiftDetails.entrySet().iterator();
+					while(shiftIter.hasNext()){
+					 Map.Entry shiftEntry=shiftIter.next();
+					 employeeMap.put(shiftEntry.getKey(),shiftEntry.getValue());
+				}
+			}
+			employeeMap.put("workedHolidays",employeePayrollAttedance.get("noOfAttendedHoliDays"));
+			employeeMap.put("workedSsDays",employeePayrollAttedance.get("noOfAttendedSsDays"));
+			employeeMap.put("payableDays",employeePayrollAttedance.get("noOfPayableDays"));
+			employeeMap.put("arrearDays",employeePayrollAttedance.get("noOfArrearDays"));
+			employeeMap.put("lateMin", employeePayrollAttedance.get("lateMin"));
+			employeeMap.put("extraMin", employeePayrollAttedance.get("extraMin"));
+			leaveBalances = delegator.findByAnd("EmplLeaveBalanceStatus",[partyId:employee.partyId],["leaveTypeId"]);
+			leaveBalances.each{ leaveType ->
+				if(leaveType.leaveTypeId=="CL")
+				employeeMap.put("BCL", leaveType.openingBalance);
+				if(leaveType.leaveTypeId=="EL")
+				employeeMap.put("BEL", leaveType.openingBalance);
+				if(leaveType.leaveTypeId=="HPL")
+				employeeMap.put("BHP", leaveType.openingBalance);
+			}
+			nowDate=UtilDateTime.nowTimestamp();
+			nowfromDate = UtilDateTime.getMonthStart(nowDate);
+			nowthruDate = UtilDateTime.getMonthEnd(nowDate,timeZone,locale);
+			resultMap=dispatcher.runSync("getCustomTimePeriodId", [periodTypeId:"HR_MONTH",fromDate:nowfromDate,thruDate:nowthruDate,userLogin:userLogin]);
+				if(UtilValidate.isNotEmpty(resultMap)){
+				cldAmount=0;
+				cldDays=0;
+				cldAmountList=casteIds=delegator.findByAnd("PartyBenefit", [partyIdTo:employee.employeeId,benefitTypeId:"PAYROL_BEN_COLDALLOW"],["benefitTypeId"]);
+				if(UtilValidate.isNotEmpty(cldAmountList)){
+					cldAmountIds=cldAmountList.get(0).benefitTypeId;
+					cldAmountMap=PayrollService.getPayHeadAmount(dctx,[userLogin:userLogin,payHeadTypeId:cldAmountIds,employeeId:employee.partyId,customTimePeriodId:resultMap.get("customTimePeriodId"),locale:locale]);
+					cldAmount=cldAmountMap.get("amount");
+					if(UtilValidate.isNotEmpty(cldAmount) && cldAmount!=null)
+						cldDays=employeePayrollAttedance.get("noOfPayableDays");
+				}
+				employeeMap.put("cldDays",cldDays);
+				caAmount=0;
+				caDays=0;
+				caAmountList=casteIds=delegator.findByAnd("PartyBenefit", [partyIdTo:employee.partyId,benefitTypeId:"PAYROL_BEN_CASH"],["benefitTypeId"]);
+				if(UtilValidate.isNotEmpty(caAmountList)){
+					caAmountIds=caAmountList.get(0).benefitTypeId;
+					caAmountMap=PayrollService.getPayHeadAmount(dctx,[userLogin:userLogin,payHeadTypeId:caAmountIds,employeeId:employee.partyId,customTimePeriodId:resultMap.get("customTimePeriodId"),locale:locale]);
+					caAmount=caAmountMap.get("amount");
+					if(UtilValidate.isNotEmpty(caAmount) && caAmount!=null)
+					caDays=employeePayrollAttedance.get("noOfPayableDays");
+				}
+				employeeMap.put("caDays",caDays);
+				heatAmount=0;
+				heatDays=0;
+				heatAmountList=casteIds=delegator.findByAnd("PartyBenefit", [partyIdTo:employee.partyId,benefitTypeId:"PAYROL_BEN_HEATALLOW"],["benefitTypeId"]);
+				if(UtilValidate.isNotEmpty(heatAmountList)){
+					heatAmountIds=heatAmountList.get(0).benfitTypeId;
+					heatAmountMap=PayrollService.getPayHeadAmount(dctx,[userLogin:userLogin,payHeadTypeId:heatAmountIds,employeeId:employee.partyId,customTimePeriodId:resultMap.get("customTimePeriodId"),locale:locale]);
+					heatAmount=heatAmountMap.get("amount");
+					if(UtilValidate.isNotEmpty(heatAmount) && heatAmount!=null)
+					heatDays=employeePayrollAttedance.get("noOfPayableDays");
+				}
+				employeeMap.put("heatDays",heatDays);
+			}
+		emplList.add(employeeMap);
 	}
 }
+
+employeeList = [];
+internalOrgs=[];
+context.internalOrgs=internalOrgs;
+context.employeeList=employeeList;
+company = delegator.findByPrimaryKey("PartyAndGroup", [partyId : "Company"]);
+populateChildren(company, employeeList);
 	
-context.put("employeeList",employeeList);		
+context.put("employeeList",emplList);		
 	
