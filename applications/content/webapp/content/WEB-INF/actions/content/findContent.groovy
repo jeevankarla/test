8277ad75a3921786aa89c392f1946ca7b7265058
kvarma <kvarma@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-08-26 14:28:46 +0000

    filter out non OMS content
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@2030 40d85018-ee8e-4105-aa2c-066e80ea2389

--- applications/content/webapp/content/WEB-INF/actions/content/findContent.groovy
+++ applications/content/webapp/content/WEB-INF/actions/content/findContent.groovy
@@ -7,3 +7,11 @@ if(userLogin){
 		parameters.isPublic = '';
 	}
 }
+if(UtilValidate.isEmpty(parameters.dataCategoryId)){
+	dataCategoryIdList = delegator.findList("DataCategory", EntityCondition.makeCondition("parentCategoryId", EntityOperator.EQUALS , "OMS"), null, null, null, false);
+	
+	if(!UtilValidate.isEmpty(dataCategoryIdList)){
+		parameters.dataCategoryId = EntityUtil.getFieldListFromEntityList(dataCategoryIdList,"dataCategoryId",true);
+		parameters.dataCategoryId_op = "in";			
+	}
+}
