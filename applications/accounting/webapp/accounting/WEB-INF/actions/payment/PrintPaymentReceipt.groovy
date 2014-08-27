2e0f4b6110ce1ed1f966dfd2f07bbeedf0d98459
vikram <vikram@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-08-27 13:23:49 +0000

    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@2049 40d85018-ee8e-4105-aa2c-066e80ea2389

---
+++ applications/accounting/webapp/accounting/WEB-INF/actions/payment/PrintPaymentReceipt.groovy
@@ -0,0 +1,83 @@
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javolution.util.FastList;
+import org.ofbiz.base.util.*;
+import org.ofbiz.entity.*;
+import org.ofbiz.entity.condition.*;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.party.party.PartyHelper;
+import org.ofbiz.base.util.UtilNumber;
+
+//${Static["org.ofbiz.base.util.UtilNumber"].formatRuleBasedAmount(Static["java.lang.Double"].parseDouble(totalAmount?string("#0")), "%rupees-and-paise", locale).toUpperCase()}
+
+paymentIds=FastList.newInstance();
+tempPaymentIds=FastList.newInstance();
+conditionList=[];
+if(parameters.paymentId){
+	paymentId=parameters.paymentId;
+	tempPaymentIds.add(paymentId);
+	parameters.paymentIds = tempPaymentIds;
+}
+paymentMethodTypeId = "";
+paymentDescription = "";
+printPaymentsList = FastList.newInstance();
+if(parameters.paymentIds){
+	
+	paymentIds.addAll(parameters.paymentIds);
+	tempprintPaymentsList = delegator.findList("Payment",EntityCondition.makeCondition("paymentId", EntityOperator.IN , paymentIds)  , null, null, null, false );
+	tempprintPaymentsList.each{paymentRecipt->
+		tempprintPaymentMap=[:];
+		tempprintPaymentMap.putAll(paymentRecipt);
+		totalAmount=paymentRecipt.amount;
+		paymentMethodTypeId = paymentRecipt.paymentMethodTypeId;
+		context.put("paymentMethodTypeId",paymentMethodTypeId);
+		paymentMethodTypeDetails = delegator.findOne("PaymentMethodType", [paymentMethodTypeId : paymentMethodTypeId], false);
+		if(UtilValidate.isNotEmpty(paymentMethodTypeDetails)){
+			paymentDescription = paymentMethodTypeDetails.description;
+			context.put("paymentDescription",paymentDescription);
+		}
+	
+		amountwords=UtilNumber.formatRuleBasedAmount(totalAmount,"%rupees-and-paise", locale).toUpperCase();
+		tempprintPaymentMap.put("amountWords",amountwords);
+		printPaymentsList.add(tempprintPaymentMap);
+	}
+}
+
+paymentMethodType = delegator.findList("PaymentMethodType", EntityCondition.makeCondition(["paymentMethodTypeId" : printPaymentsList[0].get("paymentMethodTypeId")]), null, null, null, false);
+paymentMethodTypeDesc = paymentMethodType[0].get("description");
+Debug.log("paymentMethodTypeDesc ============="+paymentMethodTypeDesc);
+context.paymentMethodTypeDesc = paymentMethodTypeDesc;
+
+paymentType = delegator.findList("PaymentType", EntityCondition.makeCondition(["paymentTypeId" : printPaymentsList[0].get("paymentTypeId")]), null, null, null, false);
+parentTypeId = paymentType[0].get("parentTypeId");
+Debug.log("parentTypeId ============="+parentTypeId);
+
+reportType = null;
+
+if( (printPaymentsList[0].get("paymentTypeId")).contains("PAYIN")){
+	reportType = "Receipt";
+}
+else{
+	reportType = "Payment";
+}
+Debug.log("reportType ============="+reportType);
+
+//if(parentTypeId == "RECEIPT"){
+//	reportType = "RECEIPT";
+//}
+//else{
+//	reportType = "PAYMENT";
+//}
+
+Debug.log("printPaymentsList ============="+printPaymentsList);
+Debug.log("payTypeDesc ============="+paymentType[0].get("description"));
+
+context.paymentTypeDescription = paymentType[0].get("description");
+context.reportType = reportType;
+context.put("printPaymentsList",printPaymentsList);
+
+
