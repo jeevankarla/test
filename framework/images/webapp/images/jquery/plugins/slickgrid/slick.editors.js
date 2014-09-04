3e6f612bd62346c001fefcb063a1202f684fb5bd
charan <charan@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-09-04 05:51:22 +0000

    auto complete not with start
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@2156 40d85018-ee8e-4105-aa2c-066e80ea2389

--- framework/images/webapp/images/jquery/plugins/slickgrid/slick.editors.js
+++ framework/images/webapp/images/jquery/plugins/slickgrid/slick.editors.js
@@ -93,7 +93,11 @@
               // Overrides the default autocomplete filter function to search only from the beginning of the string
     
               $.ui.autocomplete.filter = function (array, term) { 
-                  var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(term), "i");
+            	  var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(term), "i");
+            	  if(args.column.regexMatcher == "contains"){
+            		 matcher = new RegExp($.ui.autocomplete.escapeRegex(term), "i");
+            	  }
+                  //var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(term), "i");
                   return $.grep(array, function (value) {
                       return matcher.test(value.label || value.value || value);
                   });
