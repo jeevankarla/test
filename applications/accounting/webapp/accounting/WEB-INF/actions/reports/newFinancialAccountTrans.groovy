ffd4c525798f7e7bc5ad00a249bf258f9492a5e8
vijay <vijay@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-10-18 05:32:24 +0000

    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@2622 40d85018-ee8e-4105-aa2c-066e80ea2389

--- applications/accounting/webapp/accounting/WEB-INF/actions/reports/newFinancialAccountTrans.groovy
+++ applications/accounting/webapp/accounting/WEB-INF/actions/reports/newFinancialAccountTrans.groovy
@@ -423,7 +423,7 @@ financialAcctgTransList.each{ dayFinAccount ->
 			tempDayTotalsMap.putAll(dayFinAccountMap);
 			dayFinAccountTransList.add(tempDayTotalsMap);
 		}
-		dayFinAccountTransList = UtilMisc.sortMaps(dayFinAccountTransList, UtilMisc.toList("paymentId"));
+		//dayFinAccountTransList = UtilMisc.sortMaps(dayFinAccountTransList, UtilMisc.toList("paymentId"));
 		context.dayFinAccountTransList = dayFinAccountTransList;
 }
 
