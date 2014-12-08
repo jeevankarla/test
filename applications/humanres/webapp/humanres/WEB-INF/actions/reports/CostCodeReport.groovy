f5df8069fa3d6cc18e1eb5061e9bef099fea085c
supriya <supriya@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-12-08 10:22:51 +0000

    In CostCode report added costCode related data.
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@3157 40d85018-ee8e-4105-aa2c-066e80ea2389

--- applications/humanres/webapp/humanres/WEB-INF/actions/reports/CostCodeReport.groovy
+++ applications/humanres/webapp/humanres/WEB-INF/actions/reports/CostCodeReport.groovy
@@ -57,7 +57,6 @@ if(UtilValidate.isNotEmpty(benefitTypeList)){
 	}
 }
 benefitTypeIds = EntityUtil.getFieldListFromEntityList(benefitTypeList, "benefitTypeId", true);
-context.benefitTypeIds=benefitTypeIds;
 //getting deductions
 
 deductionTypeList = delegator.findList("DeductionType", null, null, sortBy, null, false);
@@ -70,98 +69,139 @@ if(UtilValidate.isNotEmpty(deductionTypeList)){
 	}
 }
 dedTypeIds = EntityUtil.getFieldListFromEntityList(deductionTypeList, "deductionTypeId", true);
-context.dedTypeIds=dedTypeIds;
+Map unitIdMap=FastMap.newInstance();
 
 Map emplInputMap = FastMap.newInstance();
 emplInputMap.put("orgPartyId", parameters.partyIdFrom);
 emplInputMap.put("userLogin", userLogin);
 emplInputMap.put("fromDate", timePeriodStart);
 emplInputMap.put("thruDate", timePeriodEnd);
+
 Map EmploymentsMap = HumanresService.getActiveEmployements(dctx,emplInputMap);
 List<GenericValue> employementList = (List<GenericValue>)EmploymentsMap.get("employementList");
 employementList = EntityUtil.orderBy(employementList, UtilMisc.toList("partyIdTo"));
 employementIds = EntityUtil.getFieldListFromEntityList(employementList, "partyIdTo", true);
-locationGeoIds = EntityUtil.getFieldListFromEntityList(employementList, "locationGeoId", true);
-
-Map unitIdMap=FastMap.newInstance();
-Map grandTotalMap=FastMap.newInstance();
-Map finalMap=FastMap.newInstance();
-employementList.each{ employement->
-	unitIdMap.put(employement.partyIdTo,employement.locationGeoId);
-}
-
-List stautsList = UtilMisc.toList("GENERATED","APPROVED");
-conditionList=[];
-conditionList.add(EntityCondition.makeCondition("billingTypeId", EntityOperator.EQUALS , "PAYROLL_BILL"));
-conditionList.add(EntityCondition.makeCondition("statusId", EntityOperator.IN  ,stautsList));
-conditionList.add(EntityCondition.makeCondition("customTimePeriodId", EntityOperator.EQUALS ,parameters.customTimePeriodId));
-if(UtilValidate.isNotEmpty(parameters.partyIdFrom) && (!"Company".equals(parameters.partyIdFrom))){
-	conditionList.add(EntityCondition.makeCondition("partyId", EntityOperator.EQUALS , parameters.partyIdFrom));
-}
-condition = EntityCondition.makeCondition(conditionList,EntityOperator.AND);
-periodBillingList = delegator.findList("PeriodBilling", condition, null, null, null, false);
-if(UtilValidate.isNotEmpty(periodBillingList)){
-	periodBillDetails = EntityUtil.getFirst(periodBillingList);
-	periodBillingIds = EntityUtil.getFieldListFromEntityList(periodBillingList, "periodBillingId", true);
-	periodBillingId = periodBillDetails.get("periodBillingId");
-	payRollHeaderList=[];
-	payConList=[];
-	payConList.add(EntityCondition.makeCondition("periodBillingId", EntityOperator.IN ,periodBillingIds));
-	payConList.add(EntityCondition.makeCondition("partyIdFrom", EntityOperator.IN ,employementIds));
-	payCond = EntityCondition.makeCondition(payConList,EntityOperator.AND);
-	payRollHeaderList = delegator.findList("PayrollHeader", payCond, null, null, null, false);
-	grTotalEarnings=0.0;
-	grTotalDeductions=0.0;
-	grNetAmt=0.0;
-	grRndNetAmt=0.0;
-	
-	if(UtilValidate.isNotEmpty(payRollHeaderList)){
-		locationGeoIds.each{ locationGeoId->
-			totEarnings=0.0;
-			totDeductions=0.0;
-			netAmount=0.0;
-			rndNetAmt=0.0;
-			amount=0.0;
-			Map payRollSummaryMap=FastMap.newInstance();
-			payRollHeaderList.each{ payRollHead->
-				payrollHeaderId=payRollHead.get("payrollHeaderId");
-				partyId=payRollHead.get("partyIdFrom");
-				if(locationGeoId==unitIdMap.get(partyId)){
-					itemConList=[];
-					itemConList.add(EntityCondition.makeCondition("payrollHeaderId", EntityOperator.EQUALS ,payrollHeaderId));
-					itemCond = EntityCondition.makeCondition(itemConList,EntityOperator.AND);
-					payRollHeaderItemsList = delegator.findList("PayrollHeaderItem", itemCond, null, ["payrollItemSeqId"], null, false);
-					if(UtilValidate.isNotEmpty(payRollHeaderItemsList)){
-						payRollHeaderItemsList.each{ payRollHeaderItem->
-							payrollHeaderItemTypeId=payRollHeaderItem.get("payrollHeaderItemTypeId");
-							amount=payRollHeaderItem.get("amount");
-							if(benefitTypeIds.contains(payrollHeaderItemTypeId)){
-								totEarnings=totEarnings+amount;
-								grTotalEarnings=grTotalEarnings+amount;
-							}
-							if(dedTypeIds.contains(payrollHeaderItemTypeId)){
-								totDeductions=totDeductions+amount;
-								grTotalDeductions=grTotalDeductions+amount;
+costCodeSummaryMap = [:];
+costCodeMap = [:];
+costCodes=0;
+partyRelationconditionList=[];
+partyRelationconditionList.add(EntityCondition.makeCondition("roleTypeIdFrom", EntityOperator.EQUALS , "DEPARTMENT"));
+partyRelationconditionList.add(EntityCondition.makeCondition("partyIdTo", EntityOperator.IN ,employementIds));
+partyCondition = EntityCondition.makeCondition(partyRelationconditionList,EntityOperator.AND);
+def orderBy = UtilMisc.toList("comments");
+partyRelationList = delegator.findList("PartyRelationship", partyCondition, null, orderBy, null, false);
+if(UtilValidate.isNotEmpty(partyRelationList)){
+	partyRelationList.each{ partyRelation->
+		costCode=partyRelation.get("comments");
+		center = partyRelation.get("partyIdFrom");
+		centerName = 0;
+		conditionList=[];
+		conditionList.add(EntityCondition.makeCondition("partyId", EntityOperator.EQUALS , center));
+		partyGroupCondition = EntityCondition.makeCondition(conditionList,EntityOperator.AND);
+		partyGroupList = delegator.findList("PartyGroup", partyGroupCondition, null, null, null, false);
+		if(UtilValidate.isNotEmpty(partyGroupList)){
+			costDetailsMap = [:];
+			partyGroupList.each{ partyGroup->
+				centerName=partyGroup.get("groupName");
+				costDetailsMap.put("centerName",centerName);
+				employementId = 0;
+				conditionList=[];
+				conditionList.add(EntityCondition.makeCondition("comments", EntityOperator.EQUALS , costCode));
+				condition = EntityCondition.makeCondition(conditionList,EntityOperator.AND);
+				partyLists = delegator.findList("PartyRelationship", condition, null, null, null, false);
+				if(UtilValidate.isNotEmpty(partyLists)){
+					partyLists.each{ partyList->
+						employementId = partyList.get("partyIdTo");
+						centerCode = 0;
+						centerDetList=[];
+						centerDetList.add(EntityCondition.makeCondition("partyIdTo", EntityOperator.EQUALS , employementId));
+						centerDetcondition = EntityCondition.makeCondition(centerDetList,EntityOperator.AND);
+						def centerOrderBy = UtilMisc.toList("locationGeoId");
+						centerDetLists = delegator.findList("Employment", centerDetcondition, null, centerOrderBy, null, false);
+						if(UtilValidate.isNotEmpty(centerDetLists)){
+							centerDetLists.each{ centerDetList->
+								centerCode = centerDetList.get("locationGeoId");
+								costDetailsMap.put("centerCode",centerCode);
+								List stautsList = UtilMisc.toList("GENERATED","APPROVED");
+								conditionList=[];
+								conditionList.add(EntityCondition.makeCondition("billingTypeId", EntityOperator.EQUALS , "PAYROLL_BILL"));
+								conditionList.add(EntityCondition.makeCondition("statusId", EntityOperator.IN  ,stautsList));
+								conditionList.add(EntityCondition.makeCondition("customTimePeriodId", EntityOperator.EQUALS ,parameters.customTimePeriodId));
+								if(UtilValidate.isNotEmpty(parameters.partyIdFrom) && (!"Company".equals(parameters.partyIdFrom))){
+									conditionList.add(EntityCondition.makeCondition("partyId", EntityOperator.EQUALS , parameters.partyIdFrom));
+								}
+								condition = EntityCondition.makeCondition(conditionList,EntityOperator.AND);
+								periodBillingList = delegator.findList("PeriodBilling", condition, null, null, null, false);
+								if(UtilValidate.isNotEmpty(periodBillingList)){
+									periodBillingIds = EntityUtil.getFieldListFromEntityList(periodBillingList, "periodBillingId", true);
+									periodBillingIds.each{ periodBillingId->
+										payRollHeaderList=[];
+										payConList=[];
+										payConList.add(EntityCondition.makeCondition("periodBillingId", EntityOperator.EQUALS ,periodBillingId));
+										payConList.add(EntityCondition.makeCondition("partyIdFrom", EntityOperator.EQUALS ,employementId));
+										payCond = EntityCondition.makeCondition(payConList,EntityOperator.AND);
+										payRollHeaderList = delegator.findList("PayrollHeader", payCond, null, null, null, false);
+										if(UtilValidate.isNotEmpty(payRollHeaderList)){
+											totEarnings=0.0;
+											totDeductions=0.0;
+											netAmount=0.0;
+											rndNetAmt=0.0;
+											amount=0.0;
+											tempDetailsMap =[:];
+											payRollHeaderList.each{ payRollHead->
+												payrollHeaderId=payRollHead.get("payrollHeaderId");
+												itemConList=[];
+												itemConList.add(EntityCondition.makeCondition("payrollHeaderId", EntityOperator.EQUALS ,payrollHeaderId));
+												itemCond = EntityCondition.makeCondition(itemConList,EntityOperator.AND);
+												payRollHeaderItemsList = delegator.findList("PayrollHeaderItem", itemCond, null, ["payrollItemSeqId"], null, false);
+												if(UtilValidate.isNotEmpty(payRollHeaderItemsList)){
+													payRollHeaderItemsList.each{ payRollHeaderItem->
+														payrollHeaderItemTypeId=payRollHeaderItem.get("payrollHeaderItemTypeId");
+														amount=payRollHeaderItem.get("amount");
+														if(benefitTypeIds.contains(payrollHeaderItemTypeId)){
+															totEarnings=totEarnings+amount;
+														}
+														if(dedTypeIds.contains(payrollHeaderItemTypeId)){
+															totDeductions=totDeductions+amount;
+														}
+													}
+												}
+												netAmount=totEarnings+totDeductions;
+												totDeductions = totDeductions.abs();
+												if(UtilValidate.isEmpty(costCodeSummaryMap[costCode])){
+													payRollMap = [:];
+													payRollMap["totEarnings"] = totEarnings;
+													payRollMap["totDeductions"] = totDeductions;
+													payRollMap["netAmount"] = netAmount;
+													payRollMap["rndNetAmt"] = netAmount.setScale(2,BigDecimal.ROUND_HALF_UP);
+													tempPRMap = [:]
+													tempPRMap.putAll(payRollMap);
+													costCodeSummaryMap[costCode] = tempPRMap;
+												}else{
+													payRollMap = [:];
+													payRollMap = costCodeSummaryMap[costCode];
+													payRollMap["totEarnings"] += totEarnings;
+													payRollMap["totDeductions"] += totDeductions;
+													payRollMap["netAmount"] += netAmount;
+													payRollMap["rndNetAmt"] += netAmount.setScale(2,BigDecimal.ROUND_HALF_UP);
+													tempPRMap = [:]
+													tempPRMap.putAll(payRollMap);
+													costCodeSummaryMap[costCode] = tempPRMap;
+												}
+											}
+										}
+									}
+								}
 							}
 						}
 					}
-					netAmount=totEarnings+totDeductions;
-					payRollSummaryMap.put("totEarnings",totEarnings);
-					payRollSummaryMap.put("totDeductions",totDeductions);
-					payRollSummaryMap.put("netAmount",netAmount);
-					payRollSummaryMap.put("rndNetAmt",(netAmount).setScale(2,BigDecimal.ROUND_HALF_UP));
 				}
-			}
-			grNetAmt=grTotalEarnings+grTotalDeductions;
-			grandTotalMap.put("grTotalEarnings",grTotalEarnings);
-			grandTotalMap.put("grTotalDeductions",grTotalDeductions);
-			grandTotalMap.put("grNetAmt",grNetAmt);
-			grandTotalMap.put("grRndNetAmt",(grNetAmt).setScale(2,BigDecimal.ROUND_HALF_UP));
-			if(UtilValidate.isNotEmpty(payRollSummaryMap)){
-				finalMap.put(locationGeoId,payRollSummaryMap);
+				if(UtilValidate.isNotEmpty(costDetailsMap)){
+					costCodeMap.put(costCode,costDetailsMap);
+				}
 			}
 		}
 	}
 }
-context.finalMap=finalMap;
-context.grandTotalMap=grandTotalMap;
+context.costCodeSummaryMap=costCodeSummaryMap;
+context.costCodeMap=costCodeMap;
--- applications/humanres/webapp/humanres/humanres/CostCodeReport.fo.ftl
+++ applications/humanres/webapp/humanres/humanres/CostCodeReport.fo.ftl
@@ -17,28 +17,28 @@
 		   </fo:flow>
 		</fo:page-sequence>        
 	<#else>
-	<#if finalMap?has_content>
-	<#assign costCodeDetailList = finalMap.entrySet()>
+	<#if costCodeSummaryMap?has_content>
+	<#assign costCodeDetailList = costCodeSummaryMap.entrySet()>
 		<fo:page-sequence master-reference="main">
 			<fo:static-content flow-name="xsl-region-before" font-size="8pt">
 				<fo:block font-size="5pt" white-space-collapse="false" font-weight="bold">&#160;.  </fo:block>
 				<fo:block text-align="left" white-space-collapse="false" keep-together="always">&#160;                  A.P.Dairy Development Co-op. Federation Limited.                                </fo:block>                      
 				<fo:block font-size="5pt" white-space-collapse="false" font-weight="bold">&#160;                                                  Lalapet : 	Hyderabad. </fo:block>
 				<#assign shedCode = delegator.findOne("PartyGroup", {"partyId" : ShedId}, true)>
-				<fo:block font-size="5pt" white-space-collapse="false" font-weight="bold">Unit Code:  <#if shedCode?has_content>${shedCode.comments?if_exists}</#if>         STATEMENT OF COST CODE WISE TOTALS FOR ${Static["org.ofbiz.base.util.UtilDateTime"].toDateString(timePeriodStart, "MM-yyyy")}</fo:block>    
+				<fo:block font-size="5pt" white-space-collapse="false" font-weight="bold">Unit Code:  <#if shedCode?has_content>${shedCode.comments?if_exists}</#if>         STATEMENT OF COST CODE WISE TOTALS FOR ${Static["org.ofbiz.base.util.UtilDateTime"].toDateString(timePeriodStart, "MM/yyyy")}</fo:block>    
 				<fo:block font-family="Courier,monospace" font-size="9pt">
 					<fo:table>
 		  				<fo:table-column column-width="15pt"/>
 						<fo:table-column column-width="15pt"/>
 						<fo:table-column column-width="50pt"/>
-						<fo:table-column column-width="35pt"/>
-						<fo:table-column column-width="35pt"/>
-						<fo:table-column column-width="35pt"/>
-						<fo:table-column column-width="35pt"/>
+						<fo:table-column column-width="43pt"/>
+						<fo:table-column column-width="43pt"/>
+						<fo:table-column column-width="45pt"/>
+						<fo:table-column column-width="45pt"/>
                        	<fo:table-body>
                        		<fo:table-row>
 					   			<fo:table-cell>
-					   				<fo:block font-size="8pt">--------------------------------------------------------------------------------------</fo:block>
+					   				<fo:block font-size="8pt">------------------------------------------------------------------------------------------</fo:block>
 					   			</fo:table-cell>
 					   		</fo:table-row>
 					       	<fo:table-row>
@@ -66,7 +66,7 @@
 			                </fo:table-row>
 			                <fo:table-row>
 					   			<fo:table-cell>
-					   				<fo:block font-size="8pt">--------------------------------------------------------------------------------------</fo:block>
+					   				<fo:block font-size="8pt">------------------------------------------------------------------------------------------</fo:block>
 					   			</fo:table-cell>
 					   		</fo:table-row>
 					   	</fo:table-body>
@@ -77,81 +77,84 @@
 					<fo:block font-family="Courier,monospae" font-size="9pt">
 						<fo:table>
 							<fo:table-column column-width="15pt"/>
-							<fo:table-column column-width="15pt"/>
-							<fo:table-column column-width="50pt"/>
-							<fo:table-column column-width="35pt"/>
-							<fo:table-column column-width="35pt"/>
-							<fo:table-column column-width="35pt"/>
-							<fo:table-column column-width="35pt"/>
+							<fo:table-column column-width="20pt"/>
+							<fo:table-column column-width="45pt"/>
+							<fo:table-column column-width="45pt"/>
+							<fo:table-column column-width="45pt"/>
+							<fo:table-column column-width="42pt"/>
+							<fo:table-column column-width="45pt"/>
+							<#assign totalEarnings =0>
+							<#assign totalDeductions =0>
+							<#assign totalNetAmount =0>
+							<#assign totalRndNetAmt =0>
 							<fo:table-body>
 								<#list costCodeDetailList as costCodeDetList>
+								<#assign costCode = costCodeDetList.getKey()>
 									<fo:table-row>	
 										<fo:table-cell>
-											<fo:block font-size="5pt" text-align="left" ><#if shedCode?has_content>${shedCode.comments?if_exists}</#if></fo:block>
+											<fo:block font-size="5pt"  keep-together="always" text-align="left">${costCodeMap.get(costCode).get("centerCode")}</fo:block>
 										</fo:table-cell>
 										<fo:table-cell>
-											<fo:block font-size="5pt" text-align="left">${costCodeDetList.getKey()}</fo:block>
+											<fo:block font-size="5pt"  keep-together="always" text-align="left">${costCodeDetList.getKey()?if_exists}</fo:block>
 										</fo:table-cell>
 										<fo:table-cell>
-											<fo:block font-size="5pt" text-align="left"></fo:block>
+											<fo:block font-size="5pt"  keep-together="always" text-align="left">${Static["org.ofbiz.order.order.OrderServices"].nameTrim((StringUtil.wrapString(costCodeMap.get(costCode).get("centerName"))),12)}</fo:block>
 										</fo:table-cell>
 										<fo:table-cell>
-											<fo:block font-size="5pt" text-align="right">${costCodeDetList.getValue().get("totEarnings")?if_exists?string('0.00')}</fo:block>
+											<fo:block font-size="5pt"  keep-together="always" text-align="right" >${costCodeDetList.getValue().get("totEarnings")?if_exists?string('0.00')}</fo:block>
 										</fo:table-cell>
+										<#assign totalEarnings =totalEarnings + costCodeDetList.getValue().get("totEarnings")>
 										<fo:table-cell>
-											<fo:block font-size="5pt" text-align="right">${costCodeDetList.getValue().get("totDeductions")?if_exists?string('0.00')}</fo:block>
+											<fo:block font-size="5pt"  keep-together="always" text-align="right" >${costCodeDetList.getValue().get("totDeductions")?if_exists?string('0.00')}</fo:block>
 										</fo:table-cell>
+										<#assign totalDeductions =totalDeductions + costCodeDetList.getValue().get("totDeductions")>
 										<fo:table-cell>
-											<fo:block font-size="5pt" text-align="right">${costCodeDetList.getValue().get("netAmount")?if_exists?string('0.00')}</fo:block>
+											<fo:block font-size="5pt"  keep-together="always" text-align="right" >${costCodeDetList.getValue().get("netAmount")?if_exists?string('0.00')}</fo:block>
 										</fo:table-cell>
+										<#assign totalNetAmount =totalNetAmount + costCodeDetList.getValue().get("netAmount")>
 										<fo:table-cell>
-											<fo:block font-size="5pt" text-align="right">${costCodeDetList.getValue().get("rndNetAmt")?if_exists?string('0.00')}</fo:block>
+											<fo:block font-size="5pt"  keep-together="always" text-align="right" >${costCodeDetList.getValue().get("rndNetAmt")?if_exists?string('0.00')}</fo:block>
 										</fo:table-cell>
+										<#assign totalRndNetAmt =totalRndNetAmt + costCodeDetList.getValue().get("rndNetAmt")>
 									</fo:table-row>
+									<fo:table-row>
+		                            	<fo:table-cell>
+		                                	<fo:block font-size="5pt">&#160;</fo:block>
+		                                </fo:table-cell>
+		                           </fo:table-row>
 								</#list>
+								 <fo:table-row>
+						   			<fo:table-cell>
+						   				<fo:block font-size="8pt">------------------------------------------------------------------------------------------</fo:block>
+						   			</fo:table-cell>
+						   		</fo:table-row>
+								<fo:table-row>	
+									<fo:table-cell>
+										<fo:block font-size="5pt" text-align="left"></fo:block>
+									</fo:table-cell>
+									<fo:table-cell>
+										<fo:block font-size="5pt" text-align="left"></fo:block>
+									</fo:table-cell>
+									<fo:table-cell>
+										<fo:block font-size="5pt" text-align="left">Grand Total:</fo:block>
+									</fo:table-cell>
+									<fo:table-cell>
+										<fo:block font-size="5pt" text-align="right">${totalEarnings?if_exists?string('0.00')}</fo:block>
+									</fo:table-cell>
+									<fo:table-cell>
+										<fo:block font-size="5pt" text-align="right">${totalDeductions?if_exists?string('0.00')}</fo:block>
+									</fo:table-cell>
+									<fo:table-cell>
+										<fo:block font-size="5pt" text-align="right">${totalNetAmount?if_exists?string('0.00')}</fo:block>
+									</fo:table-cell>
+									<fo:table-cell>
+										<fo:block font-size="5pt" text-align="right">${totalRndNetAmt?if_exists?string('0.00')}</fo:block>
+									</fo:table-cell>
+								</fo:table-row>
 							</fo:table-body>
 						</fo:table>
 			     	 </fo:block>
-					 <fo:block font-size="8pt">--------------------------------------------------------------------------------------</fo:block>
-					<fo:block font-size="9pt">
-					 <#if grandTotalMap?has_content>
-					<fo:table>
-						<fo:table-column column-width="15pt"/>
-						<fo:table-column column-width="15pt"/>
-						<fo:table-column column-width="50pt"/>
-						<fo:table-column column-width="35pt"/>
-						<fo:table-column column-width="35pt"/>
-						<fo:table-column column-width="35pt"/>
-						<fo:table-column column-width="35pt"/>
-						<fo:table-body> 
-							<fo:table-row>
-							<fo:table-cell>
-									<fo:block font-size="5pt" text-align="left"></fo:block>
-								</fo:table-cell>
-								<fo:table-cell>
-									<fo:block font-size="5pt" text-align="left"></fo:block>
-								</fo:table-cell>
-								<fo:table-cell>
-									<fo:block font-size="5pt" text-align="left">GRAND TOT:</fo:block>
-								</fo:table-cell>
-								<fo:table-cell>
-									<fo:block font-size="5pt" text-align="right">${grandTotalMap.get("grTotalEarnings")?if_exists?string('0.00')}</fo:block>
-								</fo:table-cell>
-								<fo:table-cell>
-									<fo:block font-size="5pt" text-align="right">${grandTotalMap.get("grTotalDeductions")?if_exists?string('0.00')}</fo:block>
-								</fo:table-cell>
-								<fo:table-cell>
-									<fo:block font-size="5pt" text-align="right">${grandTotalMap.get("grNetAmt")?if_exists?string('0.00')}</fo:block>
-								</fo:table-cell>
-								<fo:table-cell>
-									<fo:block font-size="5pt" text-align="right">${grandTotalMap.get("grRndNetAmt")?if_exists?string('0.00')}</fo:block>
-								</fo:table-cell>
-							</fo:table-row>	
-						</fo:table-body>
-					</fo:table>
-						</#if>
-					</fo:block>
-					<fo:block font-size="8pt">--------------------------------------------------------------------------------------</fo:block>
+					<fo:block font-size="8pt">------------------------------------------------------------------------------------------</fo:block>
 				</fo:flow>	
 			</fo:page-sequence>
 	<#else>
