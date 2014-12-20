d56ac6c1c2b658efe4659c9aa05a6579628abfd5
murali <murali@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-12-20 14:14:15 +0000

    Tanker Receipt : added functionality to track tanker movement
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@3343 40d85018-ee8e-4105-aa2c-066e80ea2389

---
+++ hot-deploy/procurement/webapp/milkreceipts/images/milkReciptEntryTab.js
@@ -0,0 +1,152 @@
+jQuery(document).ready(function() {
+	
+	var focusables = $(":focusable");
+	focusables.keyup(function(e) {  
+		var current = focusables.index(this);
+		if(e.keyCode==9){
+			var tempObject = focusables.eq(current-1).length ? focusables.eq(current-1) : focusables.eq(0);
+    		current = focusables.index(tempObject);
+    	}
+    	var curentElName = focusables.eq(current).attr("name");
+    	var prevEl = focusables.eq(current-1).length ? focusables.eq(current-1) : focusables.eq(0);
+		var prevElName = prevEl.attr("name");
+		
+		if(prevElName == "exitTime" || prevElName == "testTime" || prevElName == "sendTime" || prevElName == "grossTime" || prevElName == "tareTime"){
+    		var tempTime = prevEl.val();
+    		if(tempTime.length==0){
+				alert('invalid Time formate. length should be 4');
+				$('input[name='+curentElName+']').val('');
+				prevEl.val('');
+				prevEl.focus();
+				return false;
+			}
+    		if(tempTime!=''){
+    			if(tempTime.length<4 || tempTime.length==0){
+    				alert('invalid Time formate. length should be 4');
+    				$('input[name='+curentElName+']').val('');
+    				prevEl.val('');
+    				prevEl.focus();
+    				return false;
+    			}
+    			var hh = tempTime.substring(0,2);
+    			var mm = tempTime.substring(2,4);
+    			if(hh>23 || mm>59){
+    				alert('invalid Time formate');
+    				prevEl.val('');
+    				$('input[name='+curentElName+']').val('');
+    				prevEl.focus();
+    				return false;
+    			}
+    		}
+		}
+		// auto tab  for time tabs and those which need only number input
+    	if(curentElName == "recdMBRT" || curentElName == "exitTime" ||curentElName == "tareTime" || curentElName == "testTime" || curentElName == "sendTime" || curentElName == "grossTime" || curentElName == "numberOfCells"){
+    		if(e.which == 110 || e.which == 190){
+    			$(this).val( $(this).val().replace('.',''));
+    		}
+    		$(this).val( $(this).val().replace(/[^0-9\.]/g,''));
+    		var timeVal = $('input[name='+curentElName+']').val();
+    		var valLength = timeVal.length;
+    		if(curentElName == "numberOfCells"){
+    			if(valLength == 1){
+        			next = focusables.eq(current+1).length ? focusables.eq(current+1) : focusables.eq(0);
+        			next.focus();
+        		}
+    		}
+    		
+    		if(valLength == 4){
+    			if(curentElName == "exitTime"){
+    				var tempTime = $(this).val();
+    				var hh = tempTime.substring(0,2);
+        			var mm = tempTime.substring(2,4);
+        			if(hh>23 || mm>59){
+        				alert('invalid Time formate');
+        				$(this).val('');
+        				return false;
+        			}
+    				
+    			}
+    			
+    			next = focusables.eq(current+1).length ? focusables.eq(current+1) : focusables.eq(0);
+    			next.focus();
+    		}
+    	}
+    	
+    	prev = focusables.eq(current-1).length ? focusables.eq(current-1) : focusables.eq(0);
+		next = focusables.eq(current+1).length ? focusables.eq(current+1) : focusables.eq(0);
+    	
+    	
+    	if (e.keyCode == 38 ) {    		
+           prev.focus();
+   		}
+   		if (e.keyCode == 9) {
+   			next.focus();
+   		}
+    });
+	
+	
+});
+
+
+$(function() {
+    $('#submitEntry').click (function (){
+    	var displayScreen = $('[name=displayScreen]').val();
+    	var action = "createMilkTankerReceiptEntry";
+    	if(displayScreen == "VEHICLE_OUT"){
+    		action = "updateMilkTankerReceiptEntryOut";
+    	}else if(displayScreen == "VEHICLE_GRSWEIGHT"){
+    		action = "updateMilkTankerReceiptEntryGrsWeight";
+    	}else if(displayScreen == "VEHICLE_TAREWEIGHT"){
+    		action = "updateMilkTankerReceiptEntryTAREWeight";
+    	}else if(displayScreen == "VEHICLE_QC"){
+    		action = "updateMilkTankerReceiptEntryQC";
+    	}
+    	var dataString = $("#milkReceiptEntry").serialize();
+    	$('input[name=submitButton]').attr("disabled","disabled");
+    	$.ajax({
+            type: "POST",
+            url: action,
+            data: dataString,
+            dataType: 'json',
+            success: function(result) { 
+            	if(result["_ERROR_MESSAGE_"] || result["_ERROR_MESSAGE_LIST_"]){               	   
+             	   $('div#milkReceiptEntry_spinner').html();
+             	   $('div#milkReceiptEntry_spinner').removeClass("messageStr");            	 
+             	   $('div#milkReceiptEntry_spinner').addClass("errorMessage");
+             	   $('div#milkReceiptEntry_spinner').html('<label>'+ result["_ERROR_MESSAGE_"]+'</label>');
+             	   $('div#milkReceiptEntry_spinner').delay(5000).fadeOut('slow');
+             	  $('input[name=submitButton]').removeAttr("disabled");
+                }else{
+                	$("div#milkReceiptEntry_spinner").fadeIn();               	         	   
+             	   $('div#milkReceiptEntry_spinner').html(); 
+             	    $('div#milkReceiptEntry_spinner').removeClass("errorMessage");           	 
+             	   $('div#milkReceiptEntry_spinner').addClass("messageStr");
+             	   $('div#milkReceiptEntry_spinner').html('<label>succesfully updated.</label>'); 
+             	   $('div#milkReceiptEntry_spinner').delay(5000).fadeOut('slow');
+             	   $('input[name=submitButton]').removeAttr("disabled");
+                }
+                
+              },
+              error: function(result) {
+          	 	populateError(result["_ERROR_MESSAGE_"]+","+result["_ERROR_MESSAGE_LIST_"]);
+          	 }
+    	});
+    	return false;
+	});
+});
+function populateError(msg){
+	$("div#milkReceiptEntry_spinner").fadeIn();
+	$('div#milkReceiptEntry_spinner').removeClass("messageStr");
+	$('div#milkReceiptEntry_spinner').addClass("errorMessage");
+	$('div#milkReceiptEntry_spinner').html('<label>'+msg +'</label>');
+	 $('div#milkReceiptEntry_spinner').delay(5000).fadeOut('slow');
+	 $('input[name=submitButton]').removeAttr("disabled");
+	
+}
+	
+
+
+
+
+
+
