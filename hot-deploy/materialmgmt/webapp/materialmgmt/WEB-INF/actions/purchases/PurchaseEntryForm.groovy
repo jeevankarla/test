d8a0c16576be0fc50eebb07c980c5e2786162658
raja <raja@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-12-29 06:26:12 +0000

    fix for PurchaseOrder
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@3422 40d85018-ee8e-4105-aa2c-066e80ea2389

--- hot-deploy/materialmgmt/webapp/materialmgmt/WEB-INF/actions/purchases/PurchaseEntryForm.groovy
+++ hot-deploy/materialmgmt/webapp/materialmgmt/WEB-INF/actions/purchases/PurchaseEntryForm.groovy
@@ -19,7 +19,16 @@ import in.vasista.vbiz.facility.util.FacilityUtil;
 
 import in.vasista.vbiz.purchase.PurchaseStoreServices;
 
-
+if(parameters.boothId){
+	parameters.boothId = parameters.boothId.toUpperCase();
+}
+boothId = parameters.boothId;
+subscriptionTypeId = parameters.subscriptionTypeId;
+productSubscriptionTypeId = parameters.productSubscriptionTypeId;
+shipmentTypeId = parameters.shipmentTypeId;
+dctx = dispatcher.getDispatchContext();
+effectiveDate = parameters.effectiveDate;
+priceTypeId=parameters.priceTypeId;
 changeFlag=parameters.changeFlag;
 
 productCatageoryId=parameters.productCatageoryId;
@@ -33,7 +42,7 @@ subscriptionProdList = [];
 displayGrid = true;
 effDateDayBegin="";
 effDateDayEnd="";
-effectiveDate = parameters.effectiveDate;
+
 SimpleDateFormat sdf = new SimpleDateFormat("dd MMMMM, yyyy");
 if(UtilValidate.isNotEmpty(effectiveDate)){
 try {
@@ -48,9 +57,13 @@ try {
 	effDateDayBegin = UtilDateTime.getDayStart(UtilDateTime.nowTimestamp());
 	effDateDayEnd = UtilDateTime.getDayEnd(UtilDateTime.nowTimestamp());
 }
+subscriptionId = null;
 conditionList = [];
+lastIndentDate = null;
+subscriptionId = null;
 exprList = [];
 result = [:];
+routeId = parameters.routeId;
 partyId="";
 billToParty="";
 billToPartyId = parameters.billToPartyId;
@@ -58,17 +71,41 @@ billToPartyId = parameters.billToPartyId;
 issueToDeptId = parameters.issueToDeptId;
 facility = null;
 prodPriceMap = [:];
+if(changeFlag == "PurchaseOrder" || changeFlag == "InterUnitPurchase"){
+	partyId = parameters.partyId;
+	party = delegator.findOne("PartyGroup", UtilMisc.toMap("partyId", partyId), false);
+	context.party = party;
+}
+if(changeFlag == "PurchaseOrder" || changeFlag == "InterUnitPurchase"){
+	billToParty = delegator.findOne("PartyGroup", UtilMisc.toMap("partyId", billToPartyId), false);
+	context.billToParty = billToParty;
+	issueToDept = delegator.findOne("PartyGroup", UtilMisc.toMap("partyId", issueToDeptId), false);
+	context.issueToDept = issueToDept;
+	
+}
 
 if(UtilValidate.isNotEmpty(billToPartyId)){
 	context.billToPartyId = billToPartyId;
 	context.billToPartyName = org.ofbiz.party.party.PartyHelper.getPartyName(delegator,billToPartyId, false);;
 	
 }
+partyPostalAddress = delegator.findList("PartyAndPostalAddress", EntityCondition.makeCondition("partyId", EntityOperator.EQUALS, partyId), null,null,null, false);
+if(partyPostalAddress){
+	partyPostalAddress = EntityUtil.getFirst(partyPostalAddress);
+	partyAddress = partyPostalAddress.address1;
+	context.partyAddress = partyAddress;
+}
+billToPartyPostalAddress = delegator.findList("PartyAndPostalAddress", EntityCondition.makeCondition("partyId", EntityOperator.EQUALS, billToPartyId), null,null,null, false);
+if(billToPartyPostalAddress){
+	billToPartyPostalAddress = EntityUtil.getFirst(billToPartyPostalAddress);
+	billToAddress = billToPartyPostalAddress.address1;
+	context.billToAddress = billToAddress;
+}
 
 prodList=[];
 
 //if(UtilValidate.isNotEmpty(changeFlag) && changeFlag == "PurchaseOrder"){
-
+if(UtilValidate.isNotEmpty(changeFlag) &&( (changeFlag == "InterUnitPurchase")||(changeFlag == "PurchaseOrder"))){
 	exprList.clear();
 	exprList.add(EntityCondition.makeCondition("productId", EntityOperator.NOT_EQUAL, "_NA_"));
 	exprList.add(EntityCondition.makeCondition("productTypeId", EntityOperator.EQUALS, "RAW_MATERIAL"));
@@ -76,20 +113,45 @@ prodList=[];
 			 EntityCondition.makeCondition("salesDiscontinuationDate", EntityOperator.GREATER_THAN, effDateDayBegin)));
 	  EntityCondition discontinuationDateCondition = EntityCondition.makeCondition(exprList, EntityOperator.AND);
 	prodList =delegator.findList("Product", discontinuationDateCondition,null, null, null, false);
-
-
+}
+/*if(UtilValidate.isNotEmpty(productCatageoryId)){
+	exprList.clear();
+	exprList.add(EntityCondition.makeCondition("productId", EntityOperator.NOT_EQUAL, "_NA_"));
+	//exprList.add(EntityCondition.makeCondition("isVirtual", EntityOperator.NOT_EQUAL, "Y"));
+	exprList.add(EntityCondition.makeCondition("productTypeId", EntityOperator.EQUALS, "RAW_MATERIAL"));
+	exprList.add(EntityCondition.makeCondition("primaryProductCategoryId", EntityOperator.EQUALS, productCatageoryId));
+	exprList.add(EntityCondition.makeCondition(EntityCondition.makeCondition("salesDiscontinuationDate", EntityOperator.EQUALS, null),EntityOperator.OR,
+			 EntityCondition.makeCondition("salesDiscontinuationDate", EntityOperator.GREATER_THAN, effDateDayBegin)));
+	  EntityCondition discontinuationDateCondition = EntityCondition.makeCondition(exprList, EntityOperator.AND);
+	  //Debug.log("=====EntityCondition===="+EntityCondition);
+		prodList =delegator.findList("Product", discontinuationDateCondition,null, null, null, false);
+		
+}*/
 if(UtilValidate.isNotEmpty(changeFlag) &&( (changeFlag == "InterUnitPurchase")||(changeFlag == "PurchaseOrder"))){
-	tempProdList=ProductWorker.getProductsByCategory(delegator,"GEL_STATIONERY",null);
-	
+	tempProdList=ProductWorker.getProductsByCategory(delegator,"PUR_WSD",null);
+	Debug.log("===WSD==prodList==Size==="+tempProdList.size()+"prodList==Size==="+prodList.size());
 	prodList.addAll(tempProdList);
 }
-
+Debug.log("=====prodList=Size==="+prodList.size()+"==changeFlag=="+changeFlag);
+productStoreId =PurchaseStoreServices.getPurchaseFactoryStore(delegator).get("factoryStoreId");
+Map inputProductRate = FastMap.newInstance();
+inputProductRate.put("productStoreId", "9000");
+inputProductRate.put("fromDate",effDateDayBegin);
+inputProductRate.put("facilityId",boothId);
+inputProductRate.put("partyId",partyId);
+if(facility){
+	inputProductRate.put("facilityCategory",facility.categoryTypeEnum);
+}
+inputProductRate.put("userLogin",userLogin);
+inputProductRate.put("productsList",prodList);
+//Map priceResultMap =ByProductNetworkServices.getProductPricesByDate(delegator, dctx.getDispatcher(), inputProductRate);
+//prodPriceMap = (Map)priceResultMap.get("priceMap");
+prodPriceMap=[];
 
 JSONArray productItemsJSON = new JSONArray();
 JSONObject productIdLabelJSON = new JSONObject();
 JSONObject productLabelIdJSON=new JSONObject();
 context.productList = prodList;
-
 prodList.each{eachItem ->
 	JSONObject newObj = new JSONObject();
 	newObj.put("value",eachItem.productId);
@@ -98,41 +160,19 @@ prodList.each{eachItem ->
 	productIdLabelJSON.put(eachItem.productId, eachItem.brandName+" [ "+eachItem.description +"]");
 	productLabelIdJSON.put(eachItem.brandName+" [ "+eachItem.description+"]", eachItem.productId);
 }
+productPrices = [];
 
+JSONObject productCostJSON = new JSONObject();
+//productCostJSON=prodPriceMap;
+
+JSONObject prodIndentQtyCat = new JSONObject();
+JSONObject qtyInPieces = new JSONObject();
 
 context.productItemsJSON = productItemsJSON;
 context.productIdLabelJSON = productIdLabelJSON;
+context.productCostJSON = productCostJSON;
 context.productLabelIdJSON = productLabelIdJSON;
-Debug.log("productItemsJSON======="+productItemsJSON);
 if(displayGrid){
 	context.partyCode = facility;
 }
-
-paymentTerms = delegator.findByAnd("TermType",UtilMisc.toMap("parentTypeId","FEE_PAYMENT_TERM"));
-deliveryTerms = delegator.findByAnd("TermType",UtilMisc.toMap("parentTypeId","DELIVERY_TERM"));
-
-JSONArray paymentTermsJSON = new JSONArray();
-JSONArray deliveryTermsJSON = new JSONArray();
-
-paymentTerms.each{ eachTerm ->
-	JSONObject newObj = new JSONObject();
-	newObj.put("value",eachTerm.termTypeId);
-	newObj.put("label",eachTerm.description);
-	paymentTermsJSON.add(newObj);
-}
-deliveryTerms.each{ eachTerm ->
-	JSONObject newObj = new JSONObject();
-	newObj.put("value",eachTerm.termTypeId);
-	newObj.put("label",eachTerm.description);
-	deliveryTermsJSON.add(newObj);
-}
-
-context.paymentTermsJSON =paymentTermsJSON;
-context.deliveryTermsJSON =deliveryTermsJSON;
-// orderTypes here
-orderTypes = delegator.findByAnd("OrderType",UtilMisc.toMap("parentTypeId","PURCHASE_ORDER"));
-context.orderTypes =orderTypes;
-
-productStoreId =PurchaseStoreServices.getPurchaseFactoryStore(delegator).get("factoryStoreId");
-context.productStoreId = productStoreId;
 
