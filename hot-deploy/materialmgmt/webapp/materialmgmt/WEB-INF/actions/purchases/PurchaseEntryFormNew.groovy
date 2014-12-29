5b2169536206e44638a4e640fa5040fa69ab5cb0
raja <raja@40d85018-ee8e-4105-aa2c-066e80ea2389>
2014-12-29 07:14:37 +0000

    new groovy created to fix old purchaseorder
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@3425 40d85018-ee8e-4105-aa2c-066e80ea2389

---
+++ hot-deploy/materialmgmt/webapp/materialmgmt/WEB-INF/actions/purchases/PurchaseEntryFormNew.groovy
@@ -0,0 +1,138 @@
+
+import org.ofbiz.base.util.*;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.util.EntityUtil;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONArray;
+import javolution.util.FastMap;
+import java.sql.Timestamp;
+import org.ofbiz.base.util.UtilDateTime;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import org.ofbiz.service.ServiceUtil;
+import in.vasista.vbiz.byproducts.ByProductNetworkServices;
+import in.vasista.vbiz.byproducts.ByProductServices;
+import org.ofbiz.product.product.ProductWorker;
+import in.vasista.vbiz.facility.util.FacilityUtil;
+
+import in.vasista.vbiz.purchase.PurchaseStoreServices;
+
+
+changeFlag=parameters.changeFlag;
+
+productCatageoryId=parameters.productCatageoryId;
+
+if(changeFlag=="PurchaseOrder"){
+	productCatageoryId="PACKING_PRODUCT";
+}
+
+//Debug.log("========changeFlagAFTERR NEWAPPLICATIONNNNNN=="+changeFlag+"=====in OrderINDesdff");
+subscriptionProdList = [];
+displayGrid = true;
+effDateDayBegin="";
+effDateDayEnd="";
+effectiveDate = parameters.effectiveDate;
+SimpleDateFormat sdf = new SimpleDateFormat("dd MMMMM, yyyy");
+if(UtilValidate.isNotEmpty(effectiveDate)){
+try {
+	effectiveDate = new java.sql.Timestamp(sdf.parse(effectiveDate).getTime());
+}catch (ParseException e) {
+	Debug.logError(e, "Cannot parse date string: " + effDate, "");
+	displayGrid = false;
+}
+	effDateDayBegin = UtilDateTime.getDayStart(effectiveDate);
+	effDateDayEnd = UtilDateTime.getDayEnd(effectiveDate);
+}else{
+	effDateDayBegin = UtilDateTime.getDayStart(UtilDateTime.nowTimestamp());
+	effDateDayEnd = UtilDateTime.getDayEnd(UtilDateTime.nowTimestamp());
+}
+conditionList = [];
+exprList = [];
+result = [:];
+partyId="";
+billToParty="";
+billToPartyId = parameters.billToPartyId;
+
+issueToDeptId = parameters.issueToDeptId;
+facility = null;
+prodPriceMap = [:];
+
+if(UtilValidate.isNotEmpty(billToPartyId)){
+	context.billToPartyId = billToPartyId;
+	context.billToPartyName = org.ofbiz.party.party.PartyHelper.getPartyName(delegator,billToPartyId, false);;
+	
+}
+
+prodList=[];
+
+//if(UtilValidate.isNotEmpty(changeFlag) && changeFlag == "PurchaseOrder"){
+
+	exprList.clear();
+	exprList.add(EntityCondition.makeCondition("productId", EntityOperator.NOT_EQUAL, "_NA_"));
+	exprList.add(EntityCondition.makeCondition("productTypeId", EntityOperator.EQUALS, "RAW_MATERIAL"));
+	exprList.add(EntityCondition.makeCondition(EntityCondition.makeCondition("salesDiscontinuationDate", EntityOperator.EQUALS, null),EntityOperator.OR,
+			 EntityCondition.makeCondition("salesDiscontinuationDate", EntityOperator.GREATER_THAN, effDateDayBegin)));
+	  EntityCondition discontinuationDateCondition = EntityCondition.makeCondition(exprList, EntityOperator.AND);
+	prodList =delegator.findList("Product", discontinuationDateCondition,null, null, null, false);
+
+
+if(UtilValidate.isNotEmpty(changeFlag) &&( (changeFlag == "InterUnitPurchase")||(changeFlag == "PurchaseOrder"))){
+	tempProdList=ProductWorker.getProductsByCategory(delegator,"GEL_STATIONERY",null);
+	
+	prodList.addAll(tempProdList);
+}
+
+
+JSONArray productItemsJSON = new JSONArray();
+JSONObject productIdLabelJSON = new JSONObject();
+JSONObject productLabelIdJSON=new JSONObject();
+context.productList = prodList;
+
+prodList.each{eachItem ->
+	JSONObject newObj = new JSONObject();
+	newObj.put("value",eachItem.productId);
+	newObj.put("label",eachItem.brandName +" [ " +eachItem.description+"]");
+	productItemsJSON.add(newObj);
+	productIdLabelJSON.put(eachItem.productId, eachItem.brandName+" [ "+eachItem.description +"]");
+	productLabelIdJSON.put(eachItem.brandName+" [ "+eachItem.description+"]", eachItem.productId);
+}
+
+
+context.productItemsJSON = productItemsJSON;
+context.productIdLabelJSON = productIdLabelJSON;
+context.productLabelIdJSON = productLabelIdJSON;
+Debug.log("productItemsJSON======="+productItemsJSON);
+if(displayGrid){
+	context.partyCode = facility;
+}
+
+paymentTerms = delegator.findByAnd("TermType",UtilMisc.toMap("parentTypeId","FEE_PAYMENT_TERM"));
+deliveryTerms = delegator.findByAnd("TermType",UtilMisc.toMap("parentTypeId","DELIVERY_TERM"));
+
+JSONArray paymentTermsJSON = new JSONArray();
+JSONArray deliveryTermsJSON = new JSONArray();
+
+paymentTerms.each{ eachTerm ->
+	JSONObject newObj = new JSONObject();
+	newObj.put("value",eachTerm.termTypeId);
+	newObj.put("label",eachTerm.description);
+	paymentTermsJSON.add(newObj);
+}
+deliveryTerms.each{ eachTerm ->
+	JSONObject newObj = new JSONObject();
+	newObj.put("value",eachTerm.termTypeId);
+	newObj.put("label",eachTerm.description);
+	deliveryTermsJSON.add(newObj);
+}
+
+context.paymentTermsJSON =paymentTermsJSON;
+context.deliveryTermsJSON =deliveryTermsJSON;
+// orderTypes here
+orderTypes = delegator.findByAnd("OrderType",UtilMisc.toMap("parentTypeId","PURCHASE_ORDER"));
+context.orderTypes =orderTypes;
+
+productStoreId =PurchaseStoreServices.getPurchaseFactoryStore(delegator).get("factoryStoreId");
+context.productStoreId = productStoreId;
+
