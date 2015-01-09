bbd1b58d92cbd891456067c9f9eaa6a50cdca908
suneel <suneel@40d85018-ee8e-4105-aa2c-066e80ea2389>
2015-01-09 11:39:36 +0000

    Modified quotationoverview screen for createPOButton
    git-svn-id: svn+ssh://svn.vasista.in/srv/svn/repos/kmf/trunk@3563 40d85018-ee8e-4105-aa2c-066e80ea2389

---
+++ hot-deploy/materialmgmt/webapp/materialmgmt/WEB-INF/actions/purchases/createPOButtonCheck.groovy
@@ -0,0 +1,42 @@
+import org.ofbiz.base.util.*;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.util.EntityListIterator;
+import org.ofbiz.entity.util.EntityUtil;
+import java.util.*;
+import java.lang.*;
+import org.ofbiz.entity.*;
+import org.ofbiz.entity.condition.*;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import java.sql.*;
+import javolution.util.FastList;
+import javolution.util.FastMap;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import org.ofbiz.service.DispatchContext;
+import org.ofbiz.service.GenericServiceException;
+import org.ofbiz.service.ServiceUtil;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import org.ofbiz.base.util.UtilNumber;
+import in.vasista.vbiz.purchase.MaterialHelperServices;
+import org.ofbiz.accounting.invoice.InvoiceWorker;
+import in.vasista.vbiz.byproducts.SalesInvoiceServices;
+import org.ofbiz.party.party.PartyHelper;
+
+statusFlag="N";
+quoteDetails = delegator.findList("QuoteItem",EntityCondition.makeCondition("quoteId", EntityOperator.EQUALS , quoteId)  , null, null, null, false );
+if(UtilValidate.isNotEmpty(quoteDetails)){
+	quoteDetails.each{quoteitems->
+if("QTITM_QUALIFIED".equals(quoteitems.statusId)){
+
+	statusFlag="Y";
+}		
+	}
+}
+context.statusFlag=statusFlag;
