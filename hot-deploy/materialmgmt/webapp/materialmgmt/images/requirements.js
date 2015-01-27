d107d92eb6ded351c7fbc5d22b7d7b6efb9d060d
nareshGuntur <naresh@vasista.in>
2015-01-27 11:42:03 +0530

    In indent Entry dept Head approval issue fixed.

--- hot-deploy/materialmgmt/webapp/materialmgmt/images/requirements.js
+++ hot-deploy/materialmgmt/webapp/materialmgmt/images/requirements.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function() {
-	
+	//alert("welcome");
 });
 
 function raiseRequirement(element){
@@ -9,8 +9,35 @@ function raiseRequirement(element){
     var custmQuantity = $(custmQuantity).val();
 }
 function approveRequestByHOD(element){
+	//alert("In Approve");
+	var statusId="CRQ_SUBMITTED";
 	var curreElem = $(element);
-    var form = curreElem.parent().parent();
-    var quantity = $(form).find( "[name='"+"quantity"+"']");
-    var quantity = $(quantity).val();
+	var varform = curreElem.parent().parent();
+    var form = curreElem.parent().parent().find("form");
+    var formId = form.attr('id');
+    var str = "#"+formId;
+    var tempQty=$(varform).find("[name='"+"tempQty"+"']");
+    var tempQty=$(tempQty).val();
+    var approveRequestByHOD = $(str).attr("action", "approveRequestByHOD");
+    approveRequestByHOD.append("<input type='hidden' name='quantity' value='"+tempQty+"'/>");
+    approveRequestByHOD.append("<input type='hidden' name='statusId' value='"+statusId+"'/>");
+    approveRequestByHOD.submit();
+    
+}
+function rejectMaterialRequest(element){
+	var statusId ="CRQ_REJECTED";
+	var curreElem = $(element);
+	var varform = curreElem.parent().parent();
+	var form = curreElem.parent().parent().find("form");
+    var formId = form.attr('id');
+    var str = "#"+formId;
+    var description = $(varform).find("[name='"+"description"+"']");
+    var description = $(description).val();
+    if(description.length==0){
+    	alert("Please Write The Reason For Rejection");
+    }else{
+	    var rejectMaterialRequest = $(str).attr("action", "rejectMaterialRequest");
+	    rejectMaterialRequest.append("<input type='hidden' name='statusId' value='"+statusId+"'/>");
+	    rejectMaterialRequest.submit();
+    }
 }
