a6f43d39e2e95a6f0afdc7ffe8a5abdc0477c499
leelarani <leelarani@vasista.in>
2015-01-30 14:56:06 +0530

    Added faxNo.,closed date and emailId.Displays all Vendors Address in material Enquiry Report.

--- hot-deploy/materialmgmt/webapp/materialmgmt/WEB-INF/actions/reports/MaterialEnquiryReport.groovy
+++ hot-deploy/materialmgmt/webapp/materialmgmt/WEB-INF/actions/reports/MaterialEnquiryReport.groovy
@@ -20,68 +20,187 @@ import org.ofbiz.service.GenericServiceException;
 import org.ofbiz.service.ServiceUtil;
 import org.ofbiz.party.contact.ContactHelper;
 
+conditionList =[];
 dctx = dispatcher.getDispatchContext();
 companyDetail = (Map)(org.ofbiz.party.party.PartyWorker.getPartyIdentificationDetails(delegator, "Company")).get("partyDetails");
 	if(UtilValidate.isNotEmpty(companyDetail)){
 	  companyTinNumber=companyDetail.get('TIN_NUMBER');
 	}
 context.put("companyTinNumber",companyTinNumber);
-custRequestId=parameters.issueToCustReqId;
+if(parameters.issueToCustReqId){
+      custRequestId=parameters.issueToCustReqId;
+      context.custRequestId=custRequestId;
+}
+else{
+     fromPartyId=parameters.partyId;
+     custRequestId=parameters.custRequestId;
+     context.custRequestId=custRequestId;
+}
 partyAddressMap=[:];
 custReqDetails = delegator.findOne("CustRequest", [custRequestId : custRequestId], false);
 if(UtilValidate.isNotEmpty(custReqDetails)){
-   custReqDate = custReqDetails.custRequestDate;
-   responseRequiredDate=custReqDetails.responseRequiredDate;   
-   fromPartyId	 = custReqDetails.fromPartyId;  
-   partyPostalAddress= dispatcher.runSync("getPartyPostalAddress", [partyId:fromPartyId, userLogin: userLogin]);
-   if(UtilValidate.isNotEmpty(partyPostalAddress)){
-       if(UtilValidate.isNotEmpty(partyPostalAddress.address1)){
-        address1=partyPostalAddress.address1;
-		partyAddressMap.put("address1",address1);
-       }	  
-	  if(UtilValidate.isNotEmpty(partyPostalAddress.address2)){
-		  address2=partyPostalAddress.address2;
-		  partyAddressMap.put("address2",address2);
-	  }
-	  if(UtilValidate.isNotEmpty(partyPostalAddress.city)){
-		  city=partyPostalAddress.city;		
-		  partyAddressMap.put("city",city);
-	  }	  
-	  if(UtilValidate.isNotEmpty(partyPostalAddress.postalCode)){
-		  postalCode=partyPostalAddress.postalCode;
-		  partyAddressMap.put("postalCode",postalCode);
-	  }	  
-   } 
-   
-   partyContactDetails=dispatcher.runSync("getPartyTelephone", [partyId:fromPartyId, userLogin: userLogin]);
-   if(UtilValidate.isNotEmpty(partyContactDetails)){
-	   if(UtilValidate.isNotEmpty(partyContactDetails.contactNumber)){
-		   contactNumber=partyContactDetails.contactNumber;
-		   context.put("contactNumber",contactNumber);
-	   }
-   }
-   context.partyAddressMap=partyAddressMap;   
-   context.put("custReqDate",custReqDate);
-   context.put("responseRequiredDate",responseRequiredDate);
+      custReqDate = custReqDetails.custRequestDate;   
+      dueDate= custReqDetails.closedDateTime;
+      partyDetails=delegator.findList("CustRequestParty",EntityCondition.makeCondition("custRequestId", EntityOperator.EQUALS , custRequestId)  , null, null, null, false );   
+     if(UtilValidate.isNotEmpty(partyDetails)){
+	       partyIds = EntityUtil.getFieldListFromEntityList(partyDetails, "partyId", true);
+	       partyIds.each{eachPartyId->
+		   partyMap=[:];
+		   
+	       if((parameters.partyId))
+		   {			   
+			    if(eachPartyId.equals(parameters.partyId)){	   	   
+                      partyPostalAddress= dispatcher.runSync("getPartyPostalAddress", [partyId:eachPartyId, userLogin: userLogin]);
+                      if(UtilValidate.isNotEmpty(partyPostalAddress)){
+                            if(UtilValidate.isNotEmpty(partyPostalAddress.address1)){
+                               address1=partyPostalAddress.address1;
+		                       partyMap.put("address1",address1);
+                             }	  
+	                         if(UtilValidate.isNotEmpty(partyPostalAddress.address2)){
+		                          address2=partyPostalAddress.address2;
+		                          partyMap.put("address2",address2);
+	                         }
+	                        if(UtilValidate.isNotEmpty(partyPostalAddress.city)){
+		                           city=partyPostalAddress.city;		
+		                           partyMap.put("city",city);
+	                        }	  
+	                       if(UtilValidate.isNotEmpty(partyPostalAddress.postalCode)){
+		                          postalCode=partyPostalAddress.postalCode;
+		                          partyMap.put("postalCode",postalCode);
+	                        }	  
+                       } 	    
+                       partyContactDetails=dispatcher.runSync("getPartyTelephone", [partyId:eachPartyId, userLogin: userLogin]);
+                       if(UtilValidate.isNotEmpty(partyContactDetails)){
+	                         if(UtilValidate.isNotEmpty(partyContactDetails.contactNumber)){
+		                          contactNumber=partyContactDetails.contactNumber;
+		                          partyMap.put("contactNumber",contactNumber);
+	                          }
+                        }  
+					   conditionList.clear();
+					   conditionList.add(EntityCondition.makeCondition("contactMechPurposeTypeId", EntityOperator.EQUALS, "FAX_BILLING"));
+					   conditionList.add(EntityCondition.makeCondition("partyId", EntityOperator.EQUALS, eachPartyId));
+					   cond=EntityCondition.makeCondition(conditionList,EntityOperator.AND);
+					   faxNumber="";
+					   listAddress = delegator.findList("PartyContactDetailByPurpose", cond , null, null, null, false );
+					   if(UtilValidate.isNotEmpty(listAddress.contactNumber)){
+						  faxNumber = listAddress.contactNumber;
+						  partyMap.put("faxNuber", faxNumber);
+						}
+            
+				      context.partyMap=partyMap;
+				      context.put("custReqDate",custReqDate);
+			          context.put("dueDate",dueDate);
+			          partyAddressMap.put(eachPartyId,partyMap);
+			          enquiryMap=[:];
+			          productId ="";
+			          custReqItemDetails = delegator.findList("CustRequestItem",EntityCondition.makeCondition("custRequestId", EntityOperator.EQUALS , custRequestId)  , null, null, null, false );
+			          if(UtilValidate.isNotEmpty(custReqItemDetails)){
+				           custReqItemDetails=EntityUtil.getFirst(custReqItemDetails);
+				           productId=custReqItemDetails.productId;
+				           requrdqty=custReqItemDetails.quantity;
+				           enquiryMap.put("productId",productId);
+				           enquiryMap.put("requrdqty",requrdqty);
+			           }
+			           productDetails = delegator.findList("Product",EntityCondition.makeCondition("productId", EntityOperator.EQUALS , productId)  , null, null, null, false );
+			           productUomDetails=EntityUtil.getFirst(productDetails);
+			           if(UtilValidate.isNotEmpty(productUomDetails)){
+				            uomId=productUomDetails.quantityUomId;
+				            if(UtilValidate.isNotEmpty(uomId)){
+				                    uomDesc = delegator.findList("Uom",EntityCondition.makeCondition("uomId", EntityOperator.EQUALS , uomId)  , null, null, null, false );
+				                    uomDesc=EntityUtil.getFirst(uomDesc);
+				                    enquiryMap.put("unit",uomDesc.abbreviation);
+				             }
+			            }
+			            if(UtilValidate.isNotEmpty(productDetails)){
+				               productDetails=EntityUtil.getFirst(productDetails);
+				               itemCode=productDetails.internalName;
+				               description=productDetails.description;
+				               quantityUomId=productDetails.quantityUomId;
+				               enquiryMap.put("itemCode",itemCode);
+				               enquiryMap.put("description",description);
+				               enquiryMap.put("quantityUomId",quantityUomId);
+			             }
+			             context.enquiryMap=enquiryMap;
+			      }  
+			   
+		   }
+           else{	   	   
+              partyPostalAddress= dispatcher.runSync("getPartyPostalAddress", [partyId:eachPartyId, userLogin: userLogin]);
+              if(UtilValidate.isNotEmpty(partyPostalAddress)){
+                   if(UtilValidate.isNotEmpty(partyPostalAddress.address1)){
+                          address1=partyPostalAddress.address1;
+		                  partyMap.put("address1",address1);
+                    }	  
+	               if(UtilValidate.isNotEmpty(partyPostalAddress.address2)){
+		                  address2=partyPostalAddress.address2;
+		                  partyMap.put("address2",address2);
+	                }
+	               if(UtilValidate.isNotEmpty(partyPostalAddress.city)){
+		                  city=partyPostalAddress.city;		
+		                  partyMap.put("city",city);
+	                }	  
+	               if(UtilValidate.isNotEmpty(partyPostalAddress.postalCode)){
+		                  postalCode=partyPostalAddress.postalCode;
+		                  partyMap.put("postalCode",postalCode);
+	                }	  
+              } 	    
+               partyContactDetails=dispatcher.runSync("getPartyTelephone", [partyId:eachPartyId, userLogin: userLogin]);
+               if(UtilValidate.isNotEmpty(partyContactDetails)){
+	                if(UtilValidate.isNotEmpty(partyContactDetails.contactNumber)){
+		                  contactNumber=partyContactDetails.contactNumber;
+		                  partyMap.put("contactNumber",contactNumber);
+	                  }
+               } 
+			   conditionList.clear();
+			   conditionList.add(EntityCondition.makeCondition("contactMechPurposeTypeId", EntityOperator.EQUALS, "FAX_BILLING"));
+			   conditionList.add(EntityCondition.makeCondition("partyId", EntityOperator.EQUALS, eachPartyId));
+			   cond=EntityCondition.makeCondition(conditionList,EntityOperator.AND);
+			   faxNumber="";
+			   listAddress = delegator.findList("PartyContactDetailByPurpose", cond , null, null, null, false );
+			   if(UtilValidate.isNotEmpty(listAddress.contactNumber)){
+				  faxNumber = listAddress.contactNumber;
+				  partyMap.put("faxNuber", faxNumber);
+				}
+			   context.partyMap=partyMap;
+			   context.put("custReqDate",custReqDate);
+			   context.put("dueDate",dueDate);
+			   partyAddressMap.put(eachPartyId,partyMap);
+			   enquiryMap=[:];
+			   productId ="";
+			   custReqItemDetails = delegator.findList("CustRequestItem",EntityCondition.makeCondition("custRequestId", EntityOperator.EQUALS , custRequestId)  , null, null, null, false );
+			   if(UtilValidate.isNotEmpty(custReqItemDetails)){
+				  custReqItemDetails=EntityUtil.getFirst(custReqItemDetails);
+				  productId=custReqItemDetails.productId;
+				  requrdqty=custReqItemDetails.quantity;
+				  enquiryMap.put("productId",productId);
+				  enquiryMap.put("requrdqty",requrdqty);
+			   }
+			   productDetails = delegator.findList("Product",EntityCondition.makeCondition("productId", EntityOperator.EQUALS , productId)  , null, null, null, false );
+			   productUomDetails=EntityUtil.getFirst(productDetails);
+			   if(UtilValidate.isNotEmpty(productUomDetails)){
+				     uomId=productUomDetails.quantityUomId;
+				     if(UtilValidate.isNotEmpty(uomId)){
+				        uomDesc = delegator.findList("Uom",EntityCondition.makeCondition("uomId", EntityOperator.EQUALS , uomId)  , null, null, null, false );
+				        uomDesc=EntityUtil.getFirst(uomDesc);
+				        enquiryMap.put("unit",uomDesc.abbreviation);
+				      }
+			   }
+			  if(UtilValidate.isNotEmpty(productDetails)){
+				   productDetails=EntityUtil.getFirst(productDetails);
+				   itemCode=productDetails.internalName;
+				   description=productDetails.description;
+				   quantityUomId=productDetails.quantityUomId;
+				   enquiryMap.put("itemCode",itemCode);
+				   enquiryMap.put("description",description);
+				   enquiryMap.put("quantityUomId",quantityUomId);
+			   }
+			   context.enquiryMap=enquiryMap;
+			  
+            }
+	   
+	  
+        }    
+    }
+    context.partyAddressMap=partyAddressMap;
 }
-enquiryMap=[:];
-productId ="";
-custReqItemDetails = delegator.findList("CustRequestItem",EntityCondition.makeCondition("custRequestId", EntityOperator.EQUALS , custRequestId)  , null, null, null, false );
-if(UtilValidate.isNotEmpty(custReqItemDetails)){
-   custReqItemDetails=EntityUtil.getFirst(custReqItemDetails);	
-   productId=custReqItemDetails.productId;
-   requrdqty=custReqItemDetails.quantity;
-   enquiryMap.put("productId",productId);
-   enquiryMap.put("requrdqty",requrdqty);   
-}
-productDetails = delegator.findList("Product",EntityCondition.makeCondition("productId", EntityOperator.EQUALS , productId)  , null, null, null, false );
-if(UtilValidate.isNotEmpty(productDetails)){
-  productDetails=EntityUtil.getFirst(productDetails);
-  itemCode=productDetails.internalName;
-  description=productDetails.description;
-  quantityUomId=productDetails.quantityUomId;
-  enquiryMap.put("itemCode",itemCode);
-  enquiryMap.put("description",description);
-  enquiryMap.put("quantityUomId",quantityUomId);
-}
-context.enquiryMap=enquiryMap;
+
--- hot-deploy/materialmgmt/webapp/materialmgmt/reports/MaterialEnquiryReport.fo.ftl
+++ hot-deploy/materialmgmt/webapp/materialmgmt/reports/MaterialEnquiryReport.fo.ftl
@@ -28,33 +28,40 @@ under the License.
         <fo:region-after extent="1.5in"/>        
     </fo:simple-page-master>   
 </fo:layout-master-set>
-		<#if enquiryMap?has_content>
-			<fo:page-sequence master-reference="main">
+		<#if partyAddressMap?has_content>
+         
+        <#assign partyList = partyAddressMap.entrySet()> 
+           <#if enquiryMap?has_content>
+         <#list partyList as partyEntry>   
+          <#assign partyId=partyEntry.getKey()> 
+          <#assign addressDetails=partyEntry.getValue()>
+          
+          <fo:page-sequence master-reference="main">
 			    <fo:static-content font-size="13pt" font-family="Courier,monospace"  flow-name="xsl-region-before" font-weight="bold">
 				    <fo:block  keep-together="always" text-align="center" font-weight = "bold" font-family="Courier,monospace" white-space-collapse="false">${uiLabelMap.KMFDairyHeader}</fo:block>
 				    <fo:block  keep-together="always" text-align="center" font-family="Courier,monospace" white-space-collapse="false" font-weight="bold">${uiLabelMap.KMFDairySubHeader}</fo:block>
 				    <fo:block text-align="center" keep-together="always"  >&#160;--------------------------------------------------------------</fo:block>
 				    <fo:block  keep-together="always" text-align="left" font-family="Courier,monospace" white-space-collapse="false" font-size="12pt" >&#160;&#160;        TEL NOS:22179004 /41        FAX :   080-20462652                    TIN:  ${companyTinNumber?if_exists} </fo:block>
                     <fo:block  keep-together="always" text-align="center" font-family="Courier,monospace" white-space-collapse="false" font-size="12pt" >       &#160; 22179074 /55        Email: purchase@motherdairykmf.in       KST NO: 90700065  </fo:block>
-                    <fo:block  keep-together="always" text-align="right" font-family="Courier,monospace" white-space-collapse="false" font-size="12pt" >                CST NO: 90750060              &#160;&#160; </fo:block>
+                    <fo:block  keep-together="always" text-align="center" font-family="Courier,monospace" white-space-collapse="false" font-size="12pt" >       &#160;                               enggpur@motherdairykmf.in     CST NO: 90750060 </fo:block>
                     <fo:block  keep-together="always" text-align="center" font-family="Courier,monospace" white-space-collapse="false" font-size="12pt" >------------------------------------------------------------------------------------------------------- </fo:block>
 				    <fo:block text-align="center" keep-together="always">                                               ENQUIRY                                                  </fo:block>				    
 			        </fo:static-content>	
 			        <fo:flow flow-name="xsl-region-body"   font-family="Courier,monospace">	 			       
 			        <fo:block linefeed-treatment="preserve">&#xA;</fo:block>				   
-				    <fo:block text-align="left" keep-together="always" white-space-collapse="false">&#160;&#160; ENQUIRY NO.: ${parameters.issueToCustReqId}                                                          ENQUIRY DATE: ${Static["org.ofbiz.base.util.UtilDateTime"].toDateString(custReqDate, "dd-MMM-yyyy")}</fo:block>
-			        <fo:block  keep-together="always" text-align="left" font-family="Courier,monospace" white-space-collapse="false" font-size="12pt" >&#160;&#160; To:                                                                         FAX NO:</fo:block>
-				    <fo:block text-align="left" white-space-collapse="false">&#160;&#160; ${partyAddressMap.get("address1")?if_exists}</fo:block>
-				    <fo:block text-align="left" white-space-collapse="false">&#160;&#160; ${partyAddressMap.get("address2")?if_exists}</fo:block>
-				    <fo:block text-align="left" white-space-collapse="false">&#160;&#160; ${partyAddressMap.get("city")?if_exists}</fo:block>				 
-				    <fo:block text-align="left" white-space-collapse="false">&#160;&#160; ${partyAddressMap.get("postalCode")?if_exists}</fo:block>
-				    <fo:block text-align="left" white-space-collapse="false">&#160;&#160; PHONE NO: ${contactNumber?if_exists}</fo:block>	                
+				    <fo:block text-align="left" keep-together="always" white-space-collapse="false">&#160;&#160; ENQUIRY NO.: ${custRequestId?if_exists}                                                          ENQUIRY DATE: ${Static["org.ofbiz.base.util.UtilDateTime"].toDateString(custReqDate, "dd-MMM-yyyy")?if_exists}</fo:block>
+			        <fo:block  keep-together="always" text-align="left" font-family="Courier,monospace" white-space-collapse="false" font-size="12pt" >&#160;&#160; To:                                                                         FAX NO: ${partyEntry.getValue().get("contactNumber")?if_exists}</fo:block>
+				    <fo:block text-align="left" white-space-collapse="false">&#160;&#160; ${partyEntry.getValue().get("address1")?if_exists}</fo:block>
+				    <fo:block text-align="left" white-space-collapse="false">&#160;&#160; ${partyEntry.getValue().get("address2")?if_exists}</fo:block>
+				    <fo:block text-align="left" white-space-collapse="false">&#160;&#160; ${partyEntry.getValue().get("city")?if_exists}</fo:block>				 
+				    <fo:block text-align="left" white-space-collapse="false">&#160;&#160; ${partyEntry.getValue().get("postalCode")?if_exists}</fo:block>
+				    <fo:block text-align="left" white-space-collapse="false">&#160;&#160; PHONE NO: ${partyEntry.getValue().get("contactNumber")?if_exists}</fo:block>	                
 	                <fo:block linefeed-treatment="preserve">&#xA;</fo:block>
 	                <fo:block  font-size="12pt" text-align="left">&#160;&#160;   Dear Sir,                                                                                                     </fo:block> 	             
 	                <fo:block  font-size="12pt" keep-together="always" text-align="center">&#160;&#160;          We Intend to purchase the following items from the regular manufacturer/dealer and requested to</fo:block>   
 	                <fo:block  font-size="12pt" keep-together="always"  white-space-collapse="false">&#160;&#160; quote your lowest competitive rate subjected to the terms and conditions stipulated below.The sealed</fo:block>     
-	                <fo:block  font-size="12pt" keep-together="always" white-space-collapse="false">&#160;&#160; quotation duly superscribed mentioning the enquiry no,date and due date should reach this office on or </fo:block>      
-	                <fo:block  font-size="12pt" keep-together="always" white-space-collapse="false">&#160;&#160; before ${responseRequiredDate?if_exists} </fo:block>
+	                <fo:block  font-size="12pt" keep-together="always" white-space-collapse="false">&#160;&#160; quotation duly superscribed mentioning the enquiry no,date and due date should reach this office on or </fo:block>      	                
+                    <fo:block  font-size="12pt" keep-together="always" white-space-collapse="false">&#160;&#160; before <#if dueDate?has_content>${Static["org.ofbiz.base.util.UtilDateTime"].toDateString(dueDate, "dd-MMM-yyyy")?if_exists}<#else>.</#if> </fo:block>
 	                <fo:block linefeed-treatment="preserve">&#xA;</fo:block>
 	                <fo:block font-family="Courier,monospace">
 	                <fo:table>
@@ -97,11 +104,12 @@ under the License.
 	               <fo:block font-family="Courier,monospace"  font-size="10pt">
 	                   <fo:table>
 					      <fo:table-column column-width="80pt"/>
-					      <fo:table-column column-width="120pt"/>
-					      <fo:table-column column-width="220pt"/>
-					      <fo:table-column column-width="180pt"/>
-					      <fo:table-column column-width="120pt"/>
+					     <fo:table-column column-width="120pt"/>
+					     <fo:table-column column-width="220pt"/>
+					     <fo:table-column column-width="180pt"/>
+					     <fo:table-column column-width="120pt"/>
 					          <fo:table-body>
+					          
 					          <#assign sno=1>
 				                  <fo:table-row >
 					                 <fo:table-cell>
@@ -114,13 +122,14 @@ under the License.
 									   <fo:block text-align="left"  >${enquiryMap.get("description")?if_exists}</fo:block>
 								   </fo:table-cell>
 								   <fo:table-cell >
-									  <fo:block text-align="left" keep-together="always" >${enquiryMap.get("quantityUomId")?if_exists}</fo:block>
+									  <fo:block text-align="center" keep-together="always" >${enquiryMap.get("quantityUomId")?if_exists}</fo:block>
 								   </fo:table-cell>
 								   <fo:table-cell >
 									  <fo:block text-align="center" keep-together="always" >${enquiryMap.get("requrdqty")?if_exists}</fo:block>
 								   </fo:table-cell>
 					           </fo:table-row>
 					      <#assign sno=sno+1>
+                              
 					     </fo:table-body>  
 					  </fo:table>
 					 <fo:block text-align="left" keep-together="always"  >&#160;&#160;--------------------------------------------------------------------------------------------------------------------------</fo:block>
@@ -129,7 +138,7 @@ under the License.
 	               <fo:block  font-size="12pt" keep-together="always"  white-space-collapse="false"  text-align="left">&#160;Terms and Conditions :</fo:block>     
 	               <fo:block  font-size="12pt" keep-together="always"  white-space-collapse="false"  text-align="left">&#160;1.Mother Dairy is not responsible for the premature opening of the quatations.When they are not properly </fo:block> 
 	               <fo:block  font-size="12pt" keep-together="always"  white-space-collapse="false"  text-align="left">&#160;sealed subscribed.</fo:block>
-	               <fo:block  font-size="12pt" keep-together="always"  white-space-collapse="false"  text-align="left">&#160;2.All statutary taxes,duties,levies and other expenditure, should be clearly indicated with breakup.</fo:block>
+	               <fo:block  font-size="12pt" keep-together="always"  white-space-collapse="false"  text-align="left">&#160;2.All statutory taxes,duties,levies and other expenditure, should be clearly indicated with breakup.</fo:block>
 	               <fo:block  font-size="12pt" keep-together="always"  white-space-collapse="false"  text-align="left">&#160;3.The price should be quoted on F.O.R Mother Dairy basis.  </fo:block>
 	               <fo:block  font-size="12pt" keep-together="always"  white-space-collapse="false"  text-align="left">&#160;4.Please mention your TIN and CST No.</fo:block>
 	               <fo:block  font-size="12pt" keep-together="always"  white-space-collapse="false"  text-align="left">&#160;5.The validity of your offers should be 60 days from date of due date.</fo:block>
@@ -144,6 +153,9 @@ under the License.
 	              <fo:block  font-size="12pt" keep-together="always"  white-space-collapse="false"  text-align="left">&#160;                                                                                 MANAGER(PURCHASE)</fo:block>
 	           </fo:flow>   
 	      </fo:page-sequence>
+          
+         </#list>
+          </#if> 
          <#else>
            <fo:page-sequence master-reference="main">
 	    			<fo:flow flow-name="xsl-region-body" font-family="Courier,monospace">
@@ -155,23 +167,4 @@ under the License.
 	    </#if>  
      </fo:root>
 </#escape>	    
-				    
-				    
-				    
-				    
-				    
-				    
-				    
-				    
-				    
-				    
-				    
-				    
-				    
-				    
-				    
-				    
-				    
-				    
-				    
-				    
+    
