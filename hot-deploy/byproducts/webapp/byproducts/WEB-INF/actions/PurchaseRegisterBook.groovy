5e4f0ed28425e62485a04f89712b9f50f379e429
g-harish <harish@vasista.in>
2015-02-07 10:27:54 +0530

    merged

--- hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/PurchaseRegisterBook.groovy
+++ hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/PurchaseRegisterBook.groovy
@@ -264,15 +264,6 @@ context.put("InvoicePartyMapReg",InvoicePartyMapReg);
 // FOR CSV REPORT.............
 
 purchaseRegisterCsvList=[];
-totalBasicRev=0;
-totalBedRev=0;
-totalVatRev=0;
-totalCstRev=0;
-totalRevenue=0;
-totalFreightAmount=0;
-totalDiscountAmount=0;
-totalInsuranceAmount=0;
-totalAmount=0;
 purchaseRegisterList.each { purchaseRegisterList ->
 		eachlist=[];
 		eachlist=purchaseRegisterList;
@@ -296,41 +287,23 @@ purchaseRegisterList.each { purchaseRegisterList ->
 	partyMap["grandTotal"]=eachlist.grandTotal;
 	partyMap["tinNumber"]=eachlist.tinNumber;
 	//Debug.log("eachlist.partyId=============================="+eachlist.partyId);
-	totalBasicRev=totalBasicRev+eachlist.basicRevenue;
-	totalBedRev=totalBedRev+eachlist.bedRevenue;
-	totalVatRev=totalVatRev+eachlist.vatRevenue;
-	totalCstRev=totalCstRev+eachlist.cstRevenue;
-	totalRevenue=totalRevenue+eachlist.totalRevenue;
-	totalFreightAmount=totalFreightAmount+eachlist.freightAmount;
-	totalDiscountAmount=totalDiscountAmount+eachlist.discountAmount;
-	totalInsuranceAmount=totalInsuranceAmount+eachlist.insuranceAmount;
-	totalAmount=totalAmount+eachlist.grandTotal;
+	
 	if(partyId){
+		
 		partyName = org.ofbiz.party.party.PartyHelper.getPartyName(delegator, partyId, false);
+		
 	partyMap["partyName"]=partyName;
-	partyMap["partyId"]=[partyId];
+	partyMap["partyId"]=partyId;
 	
 	}else{
-	partyName = org.ofbiz.party.party.PartyHelper.getPartyName(delegator, eachlist.partyId, false);
-	partyMap["partyName"]=partyName;
-	partyMap["partyId"]=[eachlist.partyId];
+	
+	partyMap["partyName"]=eachlist.partyName;
+	partyMap["partyId"]=eachlist.partyId;
 	
 	}
 	//Debug.log("partyMap=============================="+partyMap);
-	
 	purchaseRegisterCsvList.addAll(partyMap);
 }
-grandtotalsMap=[:];
-grandtotalsMap["invoiceDate"]="grandtotal";
-	grandtotalsMap["basicRevenue"]=totalBasicRev;
-	grandtotalsMap["bedRevenue"]=totalBedRev;
-	grandtotalsMap["vatRevenue"]=totalVatRev;
-	grandtotalsMap["cstRevenue"]=totalCstRev;
-	grandtotalsMap["freightAmount"]=totalFreightAmount;
-	grandtotalsMap["discountAmount"]=totalDiscountAmount;
-	grandtotalsMap["insuranceAmount"]=totalInsuranceAmount;
-	grandtotalsMap["grandTotal"]=totalAmount;
-	purchaseRegisterCsvList.addAll(grandtotalsMap);
 context.purchaseRegisterCsvList=purchaseRegisterCsvList;
 
  /*// for vat totals
