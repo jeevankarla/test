593f86ed4d9c99737145502e2db567750f55b42c
t-suneel <suneel@vasista.in>
2015-02-10 16:39:25 +0530

    Mrr Report

--- hot-deploy/materialmgmt/webapp/materialmgmt/WEB-INF/actions/purchases/MRRreport.groovy
+++ hot-deploy/materialmgmt/webapp/materialmgmt/WEB-INF/actions/purchases/MRRreport.groovy
@@ -169,7 +169,7 @@ if(UtilValidate.isNotEmpty(shipmentId)){
 	ordDetails = delegator.findList("OrderItem", condition , null, null, null, false );
 	orderDetails=EntityUtil.getFirst(ordDetails);
 	quantity=orderDetails.quantity;
-	unitPrice=orderDetails.unitPrice;
+	unitPrice=orderDetails.unitListPrice;
 	grnDetailsMap["unitPrice"]=unitPrice;
 	grnDetailsMap["quantity"]=quantity;
 	}
