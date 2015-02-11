5bb4f513306f15700874f480b57e94b0371959f9
Raja-Kankanala <raja@vasista.in>
2015-02-11 15:44:53 +0530

    sendCC MailId added with tenant config

--- hot-deploy/materialmgmt/webapp/materialmgmt/WEB-INF/actions/purchases/email/getEnquiryEmailData.groovy
+++ hot-deploy/materialmgmt/webapp/materialmgmt/WEB-INF/actions/purchases/email/getEnquiryEmailData.groovy
@@ -20,27 +20,35 @@ import org.ofbiz.base.util.*;
 import org.ofbiz.entity.*;
 import org.ofbiz.entity.condition.EntityCondition;
 import org.ofbiz.entity.condition.EntityOperator;
+
 import net.sf.json.JSONObject;
 import net.sf.json.JSONArray;
-import org.ofbiz.product.product.ProductWorker;
 
+import org.ofbiz.product.product.ProductWorker;
 import org.ofbiz.entity.util.EntityUtil;
+
 uomList=[];
 condList=[];
 supplierPartyId=parameters.supplierPartyId;
 custRequestId=parameters.custRequestId;
 sendFromEmail="";
-sendDeptEmail="";
+sendCcEmail="";
+
+emailAddress="";
+
 	partyEmail= dispatcher.runSync("getPartyEmail", [partyId: supplierPartyId, userLogin: userLogin]);
-	emailAddress="";
 	if (partyEmail != null && partyEmail.emailAddress != null) {
 			emailAddress = partyEmail.emailAddress;
 	}
-	Debug.log("====emailAddress=="+emailAddress+"==partyEmail=="+partyEmail+"=supplierPartyId="+supplierPartyId);
+	GenericValue tenantConfigSendCc = delegator.findOne("TenantConfiguration", UtilMisc.toMap("propertyTypeEnumId","PURCHASE_OR_STORES", "propertyName","sendCcMailId"), false);
+	if (UtilValidate.isNotEmpty(tenantConfigSendCc)) {
+		sendCcEmail=tenantConfigSendCc.getString("propertyValue");
+	}
+	
 	JSONObject emailData = new JSONObject();
 	emailData.put("sendToEmail",emailAddress);
 	emailData.put("sendFromEmail",sendFromEmail);
-	emailData.put("sendCcEmail",sendDeptEmail);
+	emailData.put("sendCcEmail",sendCcEmail);
 	request.setAttribute("emailData", emailData);
 	return "success";
 
