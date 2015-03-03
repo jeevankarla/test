fd09113ba7a1f528922a6366b8bd0d62e368fe54
Raja-Kankanala <raja@vasista.in>
2015-03-03 20:35:39 +0530

    indent products auto complete changes

--- hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/facility/AdhocIndentForm.groovy
+++ hot-deploy/byproducts/webapp/byproducts/WEB-INF/actions/facility/AdhocIndentForm.groovy
@@ -227,10 +227,10 @@ context.productList = prodList;
 prodList.each{eachItem ->
 	JSONObject newObj = new JSONObject();
 	newObj.put("value",eachItem.productId);
-	newObj.put("label",eachItem.description +" [ " +eachItem.brandName+"]");
+	newObj.put("label","[" +eachItem.productId+"] " +eachItem.description+"-"+eachItem.internalName);
 	productItemsJSON.add(newObj);
 	productIdLabelJSON.put(eachItem.productId, eachItem.description);
-	productLabelIdJSON.put(eachItem.description+" [ "+eachItem.brandName+"]", eachItem.productId);
+	productLabelIdJSON.put("[" +eachItem.productId+"] " +eachItem.description+"-"+eachItem.internalName, eachItem.productId);
 	
 	if(productUomMap){
 		uomId = productUomMap.get(eachItem.productId);
