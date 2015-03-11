7c3d7fb9ebf22e6a1e4f143ab88cf81c3453e7ba
g-harish <harish@vasista.in>
2015-03-11 12:24:29 +0530

    interunit party ledger report

--- applications/accounting/webapp/accounting/WEB-INF/actions/finaccounttrans/FindFinAccountTrans.groovy
+++ applications/accounting/webapp/accounting/WEB-INF/actions/finaccounttrans/FindFinAccountTrans.groovy
@@ -18,6 +18,40 @@
 */
 
 import org.ofbiz.base.util.*
+import java.util.ArrayList;
+import org.ofbiz.base.util.UtilHttp;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.base.util.UtilNumber;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.entity.util.EntityUtil;
+import org.ofbiz.base.util.*;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.util.EntityUtil;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.service.LocalDispatcher;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import javolution.util.FastList;
+import javolution.util.FastMap;
+import org.ofbiz.base.util.UtilMisc;
+import java.math.RoundingMode;
+import java.sql.Timestamp;
+import java.util.List;
+import java.text.SimpleDateFormat;
+import javax.swing.text.html.parser.Entity;
+import in.vasista.vbiz.byproducts.ByProductNetworkServices;
+import in.vasista.vbiz.byproducts.ByProductServices;
+import org.ofbiz.product.product.ProductWorker;
+import java.util.Map;
+import org.ofbiz.entity.util.EntityFindOptions;
+import org.ofbiz.service.ServiceUtil;
+import org.ofbiz.network.LmsServices;
+import in.vasista.vbiz.byproducts.TransporterServices;
+import org.ofbiz.party.party.PartyHelper;
 
 reportTypeFlag = parameters.reportTypeFlag;
 finAccountReconciliationList=[];
@@ -34,7 +68,7 @@ if(parameters.multifinAccount == "Y"){
 			getmultipleFinAccountList(eachFinAccount.getKey(),eachFinAccount.getValue());
 			}
 }else{
-	Debug.log("===finAccountTransList==IN==FindFinAccntttt======"+finAccountTransList);
+	//Debug.log("===finAccountTransList==IN==FindFinAccntttt======"+finAccountTransList);
 	finAccountIdList= EntityUtil.getFieldListFromEntityList(finAccountTransList,"finAccountId", true);
 getmultipleFinAccountList("",finAccountTransList);
 }
@@ -150,8 +184,9 @@ context.partyTotalCredits=partyTotalCredits;
 context.partyDayWiseFinHistryMap=partyDayWiseFinHistryMap;
 
 }
+if(parameters.multifinAccount == "Y"){
 context.finalpartyDayWiseFinHistryMap=finalpartyDayWiseFinHistryMap;
-
+}
 //Debug.log("==finAccountReconciliationList=="+finAccountReconciliationList);
 //Debug.log("==partyFinAccountTransList=="+partyFinAccountTransList);
 
