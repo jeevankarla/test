d8364e15c4a604264ac2442386eae208f4b5ab14
NareshGuntur <naresh@vasista.in>
2015-04-13 12:13:17 +0530

    PrePacking Screens done.

--- framework/images/webapp/images/jquery/plugins/slickgrid/slick.editors.js
+++ framework/images/webapp/images/jquery/plugins/slickgrid/slick.editors.js
@@ -429,6 +429,98 @@
             this.init();
         },
         
+        TimestampCellEditor : function(args) {
+            var $input;
+            var defaultValue;
+            var scope = this;
+            var calendarOpen = false;
+            this.init = function() {
+                $input = $("<INPUT type=text class='editor-text' />");
+                $input.appendTo(args.container);
+                $input.focus().select();
+                $input.datetimepicker({
+                    showOn: "button",
+                   // dateFormat:'dd:mm:yy',
+                    //timeFormat: 'HH:mm',
+                    beforeShow: function() { calendarOpen = true },
+                    onClose: function() { calendarOpen = false },
+                	dateFormat:'dd:mm:yy',
+                	onSelect: function(datetext){
+                	var d = new Date(); // for now
+                	var hh=d.getHours();
+                	if(hh<10){
+                		hh="0"+hh;
+                	}
+                	var mm=d.getMinutes();
+                	if(mm<10){
+                	 mm="0"+mm;	
+                	}
+                	datetext=datetext+" "+hh+":"+mm;
+                	$input.val(datetext);
+                	},
+                });
+                $input.width($input.width() - 18);
+            };
+
+            this.destroy = function() {
+                $.datepicker.dpDiv.stop(true,true);
+                $input.datetimepicker("hide");
+                $input.datetimepicker("destroy");
+                $input.remove();
+            };
+
+            this.show = function() {
+                if (calendarOpen) {
+                    $.datepicker.dpDiv.stop(true,true).show();
+                }
+            };
+
+            this.hide = function() {
+                if (calendarOpen) {
+                    $.datepicker.dpDiv.stop(true,true).hide();
+                }
+            };
+
+            this.position = function(position) {
+                if (!calendarOpen) return;
+                $.datepicker.dpDiv
+                    .css("top", position.top + 30)
+                    .css("left", position.left);
+            };
+
+            this.focus = function() {
+                $input.focus();
+            };
+
+            this.loadValue = function(item) {
+                defaultValue = item[args.column.field];
+                $input.val(defaultValue);
+                $input[0].defaultValue = defaultValue;
+                $input.select();
+            };
+
+            this.serializeValue = function() {
+                return $input.val();
+            };
+
+            this.applyValue = function(item,state) {
+                item[args.column.field] = state;
+            };
+
+            this.isValueChanged = function() {
+                return (!($input.val() == "" && defaultValue == null)) && ($input.val() != defaultValue);
+            };
+
+            this.validate = function() {
+                return {
+                    valid: true,
+                    msg: null
+                };
+            };
+
+            this.init();
+        },
+        
         SelectCellEditor : function(args) {
             var $select;
             var defaultValue;
