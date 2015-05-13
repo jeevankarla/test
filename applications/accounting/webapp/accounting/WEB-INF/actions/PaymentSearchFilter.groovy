0cf4b69f77a560330d42bfa942a09854519c8d7a
skvalaboju <santosh@vasista.in>
2015-05-13 18:31:24 +0530

    #2113 provision to find unapplied payments

---
+++ applications/accounting/webapp/accounting/WEB-INF/actions/PaymentSearchFilter.groovy
@@ -0,0 +1,28 @@
+import org.ofbiz.base.util.*;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.util.EntityUtil;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import javolution.util.FastMap;
+import java.text.ParseException;
+import org.ofbiz.service.ServiceUtil;
+import in.vasista.vbiz.facility.util.FacilityUtil;
+import org.ofbiz.accounting.payment.PaymentWorker;
+ 
+unAppliedCheck=parameters.unAppliedCheck;
+
+	if((UtilValidate.isNotEmpty(unAppliedCheck)) && "Y".equals(unAppliedCheck)){
+		if(UtilValidate.isNotEmpty(result.listIt)){
+			resultList = [];
+			list=result.listIt;
+			GenericValue paymentEntry = null;
+			while((paymentEntry=list.next()) != null){
+				paymentNotApplied = PaymentWorker.getPaymentNotApplied(delegator,paymentEntry.paymentId);
+				if(paymentNotApplied>0){
+					resultList.add(paymentEntry);
+				}
+			}
+			context.listIt=resultList;
+		}
+	}
