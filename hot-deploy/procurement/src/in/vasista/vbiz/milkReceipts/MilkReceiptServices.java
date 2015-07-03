f905c9f90a14030ecf3f433a5f2bf8b8acd9e6a8
NareshGuntur <naresh@vasista.in>
2015-07-03 15:25:42 +0530

    Modified.

--- hot-deploy/procurement/src/in/vasista/vbiz/milkReceipts/MilkReceiptServices.java
+++ hot-deploy/procurement/src/in/vasista/vbiz/milkReceipts/MilkReceiptServices.java
@@ -2883,35 +2883,6 @@ public class MilkReceiptServices {
 	 	        	resultMap = ServiceUtil.returnError("Cannot parse date string: ");
 		 			return resultMap;
 	 	        }
-	 			/*try{
-		 			List conList = FastList.newInstance();
-		 			conList.add(EntityCondition.makeCondition("vehicleId",EntityOperator.EQUALS,(String) milkTransfer.getString("containerId")));
-		 			conList.add(EntityCondition.makeCondition("sequenceNum", EntityOperator.EQUALS,(String) milkTransfer.getString("sequenceNum")));
-		 			conList.add(EntityCondition.makeCondition("estimatedEndDate", EntityOperator.EQUALS,null));
-		 			EntityCondition con = EntityCondition.makeCondition(conList);
-		 			List<GenericValue> vehicleTripStatusList = delegator.findList("VehicleTripStatus", con, null, null, null, false);
-		 			if(UtilValidate.isEmpty(vehicleTripStatusList)){
-		 				Debug.logError("VehicleId Not Found..:",module);
-						resultMap = ServiceUtil.returnError("VehicleId Not Found.");
-			 			return resultMap;
-		 			}
-		 			GenericValue vehicleTripStatus = EntityUtil.getFirst(vehicleTripStatusList);
-		 			String currentStatusId = (String)vehicleTripStatus.get("statusId");
-		 			if(!currentStatusId.equals("MR_VEHICLE_UNLOAD") && UtilValidate.isEmpty((String)milkTransfer.getString("isCipChecked"))){
-		 				Debug.logError("Vehicle Not Yet Un-loaded:",module);
-						resultMap = ServiceUtil.returnError("Vehicle Not Yet Un-loaded.");
-			 			return resultMap;
-		 			}
-		 			if(!currentStatusId.equals("MR_VEHICLE_CIP") && UtilValidate.isNotEmpty((String)milkTransfer.getString("isCipChecked"))){
-		 				Debug.logError("CIP Already Done.",module);
-						resultMap = ServiceUtil.returnError("CIP Already Done.");
-			 			return resultMap;
-		 			}
-	 			}catch(Exception e){
-					Debug.logError("Error While getting the current status  :",module);
-					resultMap = ServiceUtil.returnError("Error While getting the current status:"+e.getMessage());
-		 			return resultMap;
-				}*/
 	 			updateVehStatusInMap.put("estimatedStartDate",cipDate);
 	 	        try{
 	 	        	updateVehStatResultMap = dispatcher.runSync("updateReceiptVehicleTripStatus", updateVehStatusInMap);
