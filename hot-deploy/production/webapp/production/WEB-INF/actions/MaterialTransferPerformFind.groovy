a95055455252188a94e605eba240551238c612d7
NareshGuntur <naresh@vasista.in>
2015-09-07 11:27:33 +0530

    Department search option added in Find Transfer.

---
+++ hot-deploy/production/webapp/production/WEB-INF/actions/MaterialTransferPerformFind.groovy
@@ -0,0 +1,27 @@
+import java.util.List;
+import org.ofbiz.entity.GenericValue;
+
+import org.ofbiz.entity.condition.*;
+import org.ofbiz.base.util.*;
+
+import javolution.util.FastList;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.service.ServiceUtil;
+import org.ofbiz.entity.util.EntityUtil;
+List facilityIds = FastList.newInstance();
+if(UtilValidate.isNotEmpty(parameters.fromPartyId)){
+	List facilityList = delegator.findList("Facility",EntityCondition.makeCondition("ownerPartyId",EntityOperator.EQUALS,parameters.fromPartyId),null,null,null,false);
+	facilityIds = EntityUtil.getFieldListFromEntityList(facilityList, "facilityId", true);
+	requestParameters.fromFacilityId= facilityIds;
+	requestParameters.fromFacilityId_op="in";
+}else{
+	List facilityList = delegator.findList("Facility",EntityCondition.makeCondition("ownerPartyId",EntityOperator.IN,context.get("partyId")),null,null,null,false);
+	facilityIds = EntityUtil.getFieldListFromEntityList(facilityList, "facilityId", true);
+	requestParameters.fromFacilityId= facilityIds;
+	requestParameters.fromFacilityId_op="in";
+}
