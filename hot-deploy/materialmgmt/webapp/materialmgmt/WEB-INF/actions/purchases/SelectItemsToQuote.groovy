43e9d1b5846090ec5a994f8bdf06acf946daa3c8
Balaji2337 <Balaji@vasista.in>
2015-10-16 12:35:02 +0530

    Now Not showing ordered items in add qotation tab

--- hot-deploy/materialmgmt/webapp/materialmgmt/WEB-INF/actions/purchases/SelectItemsToQuote.groovy
+++ hot-deploy/materialmgmt/webapp/materialmgmt/WEB-INF/actions/purchases/SelectItemsToQuote.groovy
@@ -13,19 +13,31 @@ import org.ofbiz.base.util.UtilDateTime;
 tenderId = context.custRequestId;
 itemList = [];
 tenderItemsList = delegator.findList("CustRequestItem", EntityCondition.makeCondition("custRequestId", EntityOperator.EQUALS, tenderId), null , null, null, false);
+quoteItemsList = delegator.findList("QuoteItem", EntityCondition.makeCondition("custRequestId", EntityOperator.EQUALS, tenderId), null , null, null, false);
 if(tenderItemsList.size()>0){
 	tenderItemsList.each{eachItem ->
 		tenderItemsMap = [:];
 		tenderItem = eachItem.getAt("custRequestItemSeqId");
 		productId = eachItem.getAt("productId");
 		quantity = eachItem.getAt("quantity");
-		product = delegator.findOne("Product",["productId":productId],false);
-		tenderItemsMap.putAt("custRequestId", tenderId);
-		tenderItemsMap.putAt("custRequestItemSeqId", tenderItem);
-		tenderItemsMap.putAt("productId", productId);
-		tenderItemsMap.putAt("productName", product.get("productName"));
-		tenderItemsMap.putAt("quantity", quantity);
-		itemList.add(tenderItemsMap);		
+		
+		conditionList = [];
+		conditionList.add(EntityCondition.makeCondition("custRequestId", EntityOperator.EQUALS, tenderId));
+		conditionList.add(EntityCondition.makeCondition("custRequestItemSeqId",EntityOperator.EQUALS, tenderItem));
+		conditionList.add(EntityCondition.makeCondition("productId", EntityOperator.EQUALS, productId));
+		condExpr = EntityCondition.makeCondition(conditionList, EntityOperator.AND);
+		quoteStatusList = EntityUtil.getFieldListFromEntityList(EntityUtil.filterByCondition(quoteItemsList, condExpr), "statusId", true);
+		
+		if(!quoteStatusList.contains("QTITM_ORDERED")){
+			product = delegator.findOne("Product",["productId":productId],false);
+			tenderItemsMap.putAt("custRequestId", tenderId);
+			tenderItemsMap.putAt("custRequestItemSeqId", tenderItem);
+			tenderItemsMap.putAt("productId", productId);
+			tenderItemsMap.putAt("productName", product.get("productName"));
+			tenderItemsMap.putAt("quantity", quantity);
+			itemList.add(tenderItemsMap);
+		}
+			
 	}
 }
 context.putAt("itemList", itemList);
