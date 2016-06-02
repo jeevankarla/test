e9b82522dc4b6bef7461fcab6a869ffe802a3d3e
Vikram Varma D <vikram@vasista.in>
2016-06-02 13:56:40 +0530

    Nhdc Gc issue

--- hot-deploy/depotsales/src/in/vasista/vbiz/depotsales/DepotSalesServices.java
+++ hot-deploy/depotsales/src/in/vasista/vbiz/depotsales/DepotSalesServices.java
@@ -8936,7 +8936,7 @@ public class DepotSalesServices{
    	    Map result = ServiceUtil.returnSuccess();
    	    String purposeTypeId = null;
    	    
-   	    List yarnProdCategoryTypeIds = FastList.newInstance();
+   	    /*List yarnProdCategoryTypeIds = FastList.newInstance();
    	    List<GenericValue> yarnProductCategoryTypes = null;
 		try {
 			yarnProductCategoryTypes = delegator.findList("ProductCategoryType", EntityCondition.makeCondition("parentTypeId", EntityOperator.EQUALS,"YARN"), null, null, null, false);
@@ -9024,7 +9024,7 @@ public class DepotSalesServices{
 		  	}
 	  	}
 	  	
-	  	Debug.log("purposeTypeId ======="+purposeTypeId);
+	  	Debug.log("purposeTypeId ======="+purposeTypeId);*/
 	  	
 	  	if(UtilValidate.isEmpty(purposeTypeId)){
 	  		purposeTypeId = "YARN_SALE";
