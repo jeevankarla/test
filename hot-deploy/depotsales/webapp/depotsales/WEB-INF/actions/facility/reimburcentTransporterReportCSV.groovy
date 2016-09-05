325a716206c8dbfe9beb81cc376d20684bc5d846
p-vamsi <vamsikrishna@vasista.in>
2016-09-06 02:00:09 +0530

    Depot Reimbursent...

---
+++ hot-deploy/depotsales/webapp/depotsales/WEB-INF/actions/facility/reimburcentTransporterReportCSV.groovy
@@ -0,0 +1,287 @@
+import java.math.BigDecimal;
+import java.util.*;
+import java.sql.Timestamp;
+import org.ofbiz.entity.*;
+import org.ofbiz.entity.condition.*;
+import org.ofbiz.entity.util.*;
+import org.ofbiz.base.util.*;
+import java.util.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.SortedMap;
+
+import javolution.util.FastMap;
+import javolution.util.FastList;
+import org.ofbiz.entity.util.EntityTypeUtil;
+import org.ofbiz.party.party.PartyHelper;
+import in.vasista.vbiz.byproducts.ByProductNetworkServices;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import org.ofbiz.service.GenericServiceException;
+import org.ofbiz.service.ServiceUtil;
+import java.util.Map.Entry;
+
+
+
+SimpleDateFormat sdf = new SimpleDateFormat("yyyy, MMM dd");
+dayend = null;
+daystart = null;
+
+Timestamp fromDate;
+Timestamp thruDate;
+
+DateList=[];
+DateMap = [:];
+partyfromDate=parameters.partyfromDate;
+partythruDate=parameters.partythruDate;
+partyId=parameters.partyId;
+
+DateMap.put("partyfromDate", partyfromDate);
+DateMap.put("partythruDate", partythruDate);
+
+DateList.add(DateMap);
+context.DateList=DateList;
+branchId = parameters.branchId;
+branch = delegator.findOne("PartyGroup",[partyId : branchId] , false);
+branchName = branch.get("groupName");
+DateMap.put("branchName", branchName);
+
+branchList = [];
+
+condListb = [];
+
+condListb.add(EntityCondition.makeCondition("partyIdFrom", EntityOperator.EQUALS, branchId));
+condListb.add(EntityCondition.makeCondition("roleTypeIdFrom", EntityOperator.EQUALS, "PARENT_ORGANIZATION"));
+condListb = EntityCondition.makeCondition(condListb, EntityOperator.AND);
+
+PartyRelationship = delegator.findList("PartyRelationship", condListb,UtilMisc.toSet("partyIdTo"), null, null, false);
+
+branchList=EntityUtil.getFieldListFromEntityList(PartyRelationship, "partyIdTo", true);
+
+if(!branchList)
+branchList.add(branchId);
+
+
+
+//Debug.log("branchList=================="+branchList);
+
+  
+daystart = null;
+dayend = null;
+if(UtilValidate.isNotEmpty(parameters.partyfromDate)){
+  
+	try {
+		//daystart = UtilDateTime.toTimestamp(sdf.parse(parameters.partyfromDate));
+		
+		fromDate = new java.sql.Timestamp(sdf.parse(parameters.partyfromDate).getTime());
+		daystart = UtilDateTime.getDayStart(fromDate);
+		 } catch (ParseException e) {
+			 ////Debug.logError(e, "Cannot parse date string: " + parameters.partyfromDate, "");
+			 }
+   
+}
+if(UtilValidate.isNotEmpty(parameters.partythruDate)){
+   
+   try {
+	 //  dayend = UtilDateTime.toTimestamp(sdf.parse(parameters.partythruDate));
+	   
+	   thruDate = new java.sql.Timestamp(sdf.parse(parameters.partythruDate).getTime());
+	   
+	   dayend = UtilDateTime.getDayEnd(thruDate);
+	   
+   } catch (ParseException e) {
+	   ////Debug.logError(e, "Cannot parse date string: " + parameters.partythruDate, "");
+		}
+}
+  
+
+/*Debug.log("daystart================="+daystart);
+Debug.log("partyId================="+partyId);
+*/
+
+condList = [];
+//condList.add(EntityCondition.makeCondition("invoiceId", EntityOperator.EQUALS, "11821"));
+
+if(UtilValidate.isNotEmpty(daystart)){
+	condList.add(EntityCondition.makeCondition("invoiceDate", EntityOperator.GREATER_THAN_EQUAL_TO, daystart));
+	condList.add(EntityCondition.makeCondition("invoiceDate", EntityOperator.LESS_THAN_EQUAL_TO, dayend));
+}
+
+condList.add(EntityCondition.makeCondition("invoiceTypeId", EntityOperator.EQUALS, "SALES_INVOICE"));
+condList.add(EntityCondition.makeCondition("partyIdFrom", EntityOperator.IN, branchList));
+if(partyId)
+condList.add(EntityCondition.makeCondition("partyId", EntityOperator.EQUALS, partyId));
+condList.add(EntityCondition.makeCondition("statusId", EntityOperator.NOT_EQUAL, "INVOICE_CANCELLED"));
+condList.add(EntityCondition.makeCondition("purposeTypeId", EntityOperator.EQUALS, "YARN_SALE"));
+
+
+cond = EntityCondition.makeCondition(condList, EntityOperator.AND);
+
+//Debug.log("cond================="+cond);
+
+
+fieldsToSelect = ["invoiceId","invoiceDate","shipmentId","partyIdFrom","referenceNumber"] as Set;
+
+invoice = delegator.findList("Invoice", cond, fieldsToSelect, null, null, false);
+
+//////Debug.log("invoice========================="+invoice);
+
+
+invoiceIds=EntityUtil.getFieldListFromEntityList(invoice, "invoiceId", true);
+
+
+
+//Debug.log("invoiceIds================="+invoiceIds);
+
+finalList = [];
+for (eachInvoiceList in invoice) {
+	
+	
+	
+	tempMap=[:];
+	
+	tempMap.put("invoiceId", eachInvoiceList.invoiceId);
+	
+	
+	billOfSalesInvSeqs = delegator.findList("BillOfSaleInvoiceSequence",EntityCondition.makeCondition("invoiceId", EntityOperator.EQUALS , eachInvoiceList.invoiceId)  , UtilMisc.toSet("invoiceSequence"), null, null, false );
+	if(UtilValidate.isNotEmpty(billOfSalesInvSeqs)){
+		invoiceSeqDetails = EntityUtil.getFirst(billOfSalesInvSeqs);
+		invoiceSequence = invoiceSeqDetails.invoiceSequence;
+		tempMap.put("billno", invoiceSequence);
+	}else{
+		tempMap.put("billno", eachItem.invoiceId);
+	}
+	
+	 tempMap.put("invoiceDate",UtilDateTime.toDateString(eachInvoiceList.invoiceDate,"dd/MM/yyyy"));
+		   
+	 //Debug.log("eachInvoiceList.invoiceId================="+eachInvoiceList.invoiceId);
+   
+	condList.clear();
+	
+	condList.add(EntityCondition.makeCondition("invoiceId", EntityOperator.EQUALS, eachInvoiceList.invoiceId));
+	condList.add(EntityCondition.makeCondition("invoiceItemTypeId", EntityOperator.EQUALS, "INV_FPROD_ITEM"));
+	invoiceItemcond = EntityCondition.makeCondition(condList, EntityOperator.AND);
+	
+	InvoiceItem = delegator.findList("InvoiceItem", invoiceItemcond, null, null, null, false);
+	  
+	double invoiceAMT = 0;
+	for (eachInvoiceItem in InvoiceItem) {
+		
+		invoiceAMT = invoiceAMT+(eachInvoiceItem.amount*eachInvoiceItem.quantity);
+		
+	}
+	
+	tempMap.put("invoiceAmount", invoiceAMT);
+	
+	//Debug.log("invoiceAMT================="+invoiceAMT);
+	condList.clear();
+	condList.add(EntityCondition.makeCondition("invoiceId", EntityOperator.EQUALS, eachInvoiceList.invoiceId));
+	//conditionList.add(EntityCondition.makeCondition("invoiceItemSeqId", EntityOperator.EQUALS, eachItem.invoiceItemSeqId));
+	condList.add(EntityCondition.makeCondition("statusId", EntityOperator.NOT_EQUAL, "INVOICE_CANCELLED"));
+	cond = EntityCondition.makeCondition(condList, EntityOperator.AND);
+	OrderItemBilling = delegator.findList("OrderItemBillingAndInvoiceAndInvoiceItem", cond, null, null, null, false);
+   
+	//Debug.log("OrderItemBilling======================"+OrderItemBilling);
+		   
+	 itemOrderId  = OrderItemBilling[0].orderId;
+	 
+	 conditionList = [];
+	 conditionList.add(EntityCondition.makeCondition("orderId", EntityOperator.EQUALS, itemOrderId));
+	 conditionList.add(EntityCondition.makeCondition("roleTypeId", EntityOperator.IN, ["SUPPLIER","ON_BEHALF_OF","BILL_TO_CUSTOMER"]));
+	 expr = EntityCondition.makeCondition(conditionList, EntityOperator.AND);
+	 OrderRoleList = delegator.findList("OrderRole", expr, null, null, null, false);
+	 
+	 partyId = "";
+	 supplier = "";
+	 
+	 for (eachRole in OrderRoleList) {
+		 
+		 if(eachRole.roleTypeId == "SUPPLIER")
+		  supplier = eachRole.get("partyId");
+		 if(eachRole.roleTypeId == "BILL_TO_CUSTOMER")
+		  partyId = eachRole.get("partyId");
+		
+	 }
+	
+	 
+	 partyName = "";
+	 if(partyId)
+	  partyName = PartyHelper.getPartyName(delegator, partyId, false);
+	 
+	 
+	 supplierName = "";
+	 if(supplier)
+	 supplierName = PartyHelper.getPartyName(delegator, supplier, false);
+	 
+	 
+	 tempMap.put("supplierName", supplierName);
+	 
+	 tempMap.put("partyName", partyName);
+	 
+	 shipmentId = eachInvoiceList.shipmentId;
+	 
+	 if(shipmentId){
+		 shipmentList = delegator.findOne("Shipment",[shipmentId : shipmentId] , false);
+		 
+		 lrNumber = shipmentList.get("lrNumber");
+		 
+		 if(lrNumber)
+		 tempMap.put("lrNumber", lrNumber);
+		 else
+		 tempMap.put("lrNumber", "");
+		 
+		 deliveryChallanDate = shipmentList.get("deliveryChallanDate");
+		 
+		 if(deliveryChallanDate)
+		 tempMap.put("lrDate",UtilDateTime.toDateString(deliveryChallanDate,"dd-MM-yyyy") );
+		 else
+		 tempMap.put("lrDate", "");
+		 
+		 
+		 estimatedShipCost = shipmentList.get("estimatedShipCost");
+					 
+		 if(estimatedShipCost)
+		 tempMap.put("claim", estimatedShipCost);
+		 else
+		 tempMap.put("claim", "");
+		 
+		 estimatedShipCost = shipmentList.get("estimatedShipCost");
+		 
+		 if(estimatedShipCost)
+		 tempMap.put("eligibility", estimatedShipCost);
+		 else
+		 tempMap.put("eligibility", "");
+		 
+		 carrierName = shipmentList.get("carrierName");
+		 
+		 if(carrierName)
+		 tempMap.put("transporter", carrierName);
+		 else
+		 tempMap.put("transporter", "");
+		 
+		 supplierInvoiceId = ""
+		 supplierInvoiceId = shipmentList.get("supplierInvoiceId");
+		 
+		 if(supplierInvoiceId)
+		 tempMap.put("supplierInvoiceId", supplierInvoiceId);
+		 else
+		 tempMap.put("supplierInvoiceId", "");
+		 
+		 
+		 supplierInvoiceDate = "";
+		 supplierInvoiceDate = shipmentList.get("supplierInvoiceDate");
+		 
+		 if(supplierInvoiceDate)
+		 tempMap.put("supplierInvoiceDate", UtilDateTime.toDateString(supplierInvoiceDate,"dd-MM-yyyy"));
+		 else
+		 tempMap.put("supplierInvoiceDate", "");
+		 
+		 }
+	 
+	 finalList.add(tempMap);
+	 
+}
+
+//Debug.log("finalList================"+finalList);
+
+context.finalList = finalList;
