9ad9ea33f3b281af8385bd7a549b12af4bb3ac83
draviteja <raviteja@vasista.in>
2016-10-17 18:43:42 +0530

    added getSupplierPO API

--- hot-deploy/depotsales/src/in/vasista/vbiz/depotsales/SupplierApiServices.java
+++ hot-deploy/depotsales/src/in/vasista/vbiz/depotsales/SupplierApiServices.java
@@ -183,4 +183,155 @@ public class SupplierApiServices {
 		
 		return result;
     }
+    
+    public static Map<String, Object> getSupplierPO(DispatchContext dctx, Map<String, ? extends Object> context) {
+    	Delegator delegator = dctx.getDelegator();
+		LocalDispatcher dispatcher = dctx.getDispatcher();    	
+        GenericValue userLogin = (GenericValue) context.get("userLogin");
+        Map result = ServiceUtil.returnSuccess();
+        
+        String partyId = (String)context.get("partyId");
+        //OrderHeaderAndSequenceAndRoles
+        
+        List conditionList = FastList.newInstance();
+        conditionList.add(EntityCondition.makeCondition("partyId", EntityOperator.EQUALS, partyId));
+        //conditionList.add(EntityCondition.makeCondition("statusId", EntityOperator.EQUALS, "ORDER_APPROVED"));
+        conditionList.add(EntityCondition.makeCondition("statusId", EntityOperator.EQUALS, "ORDER_APPROVED"));
+        conditionList.add(EntityCondition.makeCondition("roleTypeId", EntityOperator.EQUALS, "BILL_FROM_VENDOR"));
+        conditionList.add(EntityCondition.makeCondition("orderTypeId", EntityOperator.EQUALS, "PURCHASE_ORDER"));
+        EntityListIterator orderHeaderList = null;
+        try{
+	        orderHeaderList = delegator.find("OrderHeaderAndSequenceAndRoles", EntityCondition.makeCondition(conditionList, EntityOperator.AND), null, null, null, null);
+	    } catch (GenericEntityException e) {
+			Debug.logError(e, module);
+		}
+        GenericValue eachHeader;
+        Map ordersMap = FastMap.newInstance();
+        while( orderHeaderList != null && (eachHeader = orderHeaderList.next()) != null) {
+        	String eachOrderId = (String) eachHeader.get("orderId");
+        	Map tempData = FastMap.newInstance();
+        	tempData.put("orderDate", String.valueOf(eachHeader.get("orderDate")).substring(0,10));
+        	tempData.put("orderId",eachOrderId);
+        	tempData.put("orderNo",eachHeader.get("orderNo"));
+        	tempData.put("statusId",eachHeader.get("statusId"));
+        	List<GenericValue> shipReceiptList = null;
+        	try{
+        		shipReceiptList = delegator.findList("ShipmentReceipt", EntityCondition.makeCondition("orderId", EntityOperator.EQUALS, eachOrderId), null, null, null, false);
+			} catch (GenericEntityException e) {
+				Debug.logError(e, module);
+			}
+        	
+        	
+        	List orderItems = FastList.newInstance();
+        	List itemCondList = FastList.newInstance();
+		    itemCondList.add(EntityCondition.makeCondition("orderId", EntityOperator.EQUALS, eachOrderId));
+		    itemCondList.add(EntityCondition.makeCondition("orderItemTypeId", EntityOperator.EQUALS, "PRODUCT_ORDER_ITEM"));
+		    try{
+		    	List<GenericValue> orderItemsList = delegator.findList("OrderItem", EntityCondition.makeCondition(itemCondList, EntityOperator.AND), null, null, null, false);
+		    	if(UtilValidate.isNotEmpty(orderItemsList)){
+					for(GenericValue eachItem:orderItemsList){
+						String productId = eachItem.getString("productId");
+						String itemName = eachItem.getString("itemDescription");
+						String spec = "";
+						try{
+							GenericValue orderItemAttribute = delegator.findOne("OrderItemAttribute", UtilMisc.toMap("orderId", eachOrderId,"orderItemSeqId",eachItem.get("orderItemSeqId"),"attrName","remarks"), false);
+							if(UtilValidate.isNotEmpty(orderItemAttribute)){
+								spec = orderItemAttribute.getString("attrValue");
+							}
+						}catch(GenericEntityException e){
+							Debug.logError(e, module);
+						}
+						BigDecimal unitPrice = eachItem.getBigDecimal("unitPrice");
+						BigDecimal quantity = eachItem.getBigDecimal("quantity");
+						BigDecimal shippedQuantity = BigDecimal.ZERO;
+						List shipCondList = FastList.newInstance();
+						shipCondList.add(EntityCondition.makeCondition("orderId", EntityOperator.EQUALS, eachOrderId));
+						shipCondList.add(EntityCondition.makeCondition("productId", EntityOperator.EQUALS, eachItem.get("productId")));
+						shipCondList.add(EntityCondition.makeCondition("orderItemSeqId", EntityOperator.EQUALS, eachItem.get("orderItemSeqId")));
+						//List<GenericValue> shipReceiptList = delegator.findList("ShipmentReceipt", EntityCondition.makeCondition(shipCondList, EntityOperator.AND), null, null, null, false);
+						List<GenericValue> shipList = EntityUtil.filterByCondition(shipReceiptList, EntityCondition.makeCondition(shipCondList, EntityOperator.AND));
+						for(GenericValue eachShip:shipList){
+							shippedQuantity = shippedQuantity.add(eachShip.getBigDecimal("quantityAccepted"));
+						}
+						
+						BigDecimal balQuantity = quantity.subtract(shippedQuantity);
+						Map itemMap = FastMap.newInstance();
+						itemMap.put("productId",productId);
+						itemMap.put("itemName",itemName);
+						itemMap.put("specification",spec);
+						itemMap.put("unitPrice",unitPrice);
+						itemMap.put("quantity",quantity);
+						itemMap.put("shippedQuantity",shippedQuantity);
+						itemMap.put("balQuantity",balQuantity);
+						orderItems.add(itemMap);
+					}
+				}
+		    } catch (GenericEntityException e) {
+				Debug.logError(e, module);
+			}
+        	tempData.put("orderItemsList", orderItems);
+        	
+        	Map shipmentHistory = FastMap.newInstance();
+			List<String> shipmentIdList = (List) EntityUtil.getFieldListFromEntityList(shipReceiptList, "shipmentId", true);
+			for(String shipmentId:shipmentIdList){
+				List custCondList = FastList.newInstance();
+				custCondList.add(EntityCondition.makeCondition("orderId", EntityOperator.EQUALS, eachOrderId));
+				custCondList.add(EntityCondition.makeCondition("shipmentId", EntityOperator.EQUALS, shipmentId));
+				List<GenericValue> shipmentDetails = EntityUtil.filterByCondition(shipReceiptList, EntityCondition.makeCondition(custCondList, EntityOperator.AND));
+				
+				List shipmentDetailList = FastList.newInstance();
+				for(GenericValue eachDetail:shipmentDetails){
+					Map detailMap = FastMap.newInstance();
+					GenericValue orderItemDetail = null;
+					try{
+						orderItemDetail = delegator.findOne("OrderItem", UtilMisc.toMap("orderId", eachOrderId,"orderItemSeqId",eachDetail.get("orderItemSeqId")), false);
+					}catch(GenericEntityException e){
+						Debug.logError(e, module);
+					}
+					BigDecimal quantity = eachDetail.getBigDecimal("quantityAccepted");
+					BigDecimal unitPrice = orderItemDetail.getBigDecimal("unitPrice");
+					
+					detailMap.put("itemName",orderItemDetail.get("itemDescription"));
+					detailMap.put("productId",eachDetail.get("productId"));
+					detailMap.put("orderItemSeqId",eachDetail.get("orderItemSeqId"));
+					detailMap.put("quantity",quantity);
+					detailMap.put("unitPrice",unitPrice);
+					
+					GenericValue Shipment = null;
+					try{
+						Shipment = delegator.findOne("Shipment", UtilMisc.toMap("shipmentId", shipmentId), false);
+					}catch(GenericEntityException e){
+						Debug.logError(e, module);
+					}
+					
+					List conditionlist = FastList.newInstance();
+					conditionlist.add(EntityCondition.makeCondition("orderId", EntityOperator.EQUALS, eachOrderId));
+					conditionlist.add(EntityCondition.makeCondition("orderItemSeqId", EntityOperator.EQUALS, eachDetail.get("orderItemSeqId")));
+					conditionlist.add(EntityCondition.makeCondition("changeTypeEnumId", EntityOperator.EQUALS, "ODR_ITM_AMEND"));
+					conditionlist.add(EntityCondition.makeCondition("changeDatetime", EntityOperator.LESS_THAN_EQUAL_TO, Shipment.get("createdDate")));
+					EntityCondition conditionMain1=EntityCondition.makeCondition(conditionlist,EntityOperator.AND);
+					
+					try{
+						FastList<GenericValue> OrderItemChangeDetails = FastList.newInstance();
+						OrderItemChangeDetails = (FastList)delegator.findList("OrderItemChange", conditionMain1 , null ,UtilMisc.toList("changeDatetime"), null, false );
+						if(UtilValidate.isNotEmpty(OrderItemChangeDetails)){
+							GenericValue lastItemChange = OrderItemChangeDetails.getLast();
+							unitPrice = lastItemChange.getBigDecimal("unitPrice");
+							detailMap.put("unitPrice",unitPrice);
+						}
+						
+					}catch(GenericEntityException e){
+						Debug.logError(e, module);
+					}
+					detailMap.put("itemAmount",quantity.multiply(unitPrice));
+					shipmentDetailList.add(detailMap);
+				}
+				shipmentHistory.put(shipmentId,shipmentDetailList);
+			}
+			tempData.put("shipmentHistory",shipmentHistory);
+        	ordersMap.put(eachOrderId,tempData);
+        }
+        result.put("supplierPOList",ordersMap);
+        return result;
+    }
 }
