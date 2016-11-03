b72058ef4d1c06db2dff6f2fab4ae6c23ad4b4d6
sujatha95 <gsujatha@vasista.in>
2016-11-03 18:36:37 +0530

    checkbox to copy present address into perment address field, unique PL number and benefit/ded screen change

--- applications/humanres/webapp/humanres/images/getAllEmployees.js
+++ applications/humanres/webapp/humanres/images/getAllEmployees.js
@@ -12,3 +12,19 @@ function addSpouseName(){
 		jQuery("#spouseName").parent().parent().hide();
 	}
 }
+
+function copyAddress(){
+	var address1 = $("input[name='address1']").val();
+	var address2 = $("input[name='address2']").val();
+	var city = $("input[name='city']").val();
+	var state = $("select[name='state']").val();
+	var postalCode = $("input[name='postalCode']").val();
+	var country = $("select[name='country']").val();
+	$("#prsAddress1").val(address1);
+	$("#prsAddress2").val(address2);
+	$("#prsCity").val(city);
+	$("#prsState").val(state);
+	$("#prsPostalCode").val(postalCode);
+	$("#prsCountry").val(country);
+}
+
