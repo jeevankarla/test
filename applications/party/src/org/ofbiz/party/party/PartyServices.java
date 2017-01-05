efb6b99485614017313fa3fdfda8d5b199247431
vedavyasmithra <vedavyas@vasista.in>
2017-01-05 12:34:46 +0530

    #3363 Datepicker

--- applications/party/src/org/ofbiz/party/party/PartyServices.java
+++ applications/party/src/org/ofbiz/party/party/PartyServices.java
@@ -1988,7 +1988,7 @@ public class PartyServices {
 		}
 	 	
 	 	if (UtilValidate.isNotEmpty(thruDateStr)) { //2011-12-25 18:09:45
-			SimpleDateFormat sdf = new SimpleDateFormat("mm/dd/yyyy");             
+			SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");             
 			try {
 				thruDate = new java.sql.Timestamp(sdf.parse(thruDateStr).getTime());
 				thruDate = UtilDateTime.getDayEnd(thruDate);
