b8ec0e01b771010e698274d08fbcaf07e146a0f4
draviteja <raviteja@vasista.in>
2017-02-20 01:46:05 +0530

    issue fix

---
+++ applications/accounting/webapp/accounting/WEB-INF/actions/reports/getFinAccounts.groovy
@@ -0,0 +1,25 @@
+import org.ofbiz.base.util.*;
+import org.ofbiz.entity.*;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONArray;
+import org.ofbiz.entity.util.EntityUtil;
+
+condList = [];
+condList.add(EntityCondition.makeCondition("organizationPartyId", EntityOperator.EQUALS, "Company"));
+condList.add(EntityCondition.makeCondition("finAccountTypeId", EntityOperator.EQUALS, "BANK_ACCOUNT"));
+if(UtilValidate.isNotEmpty(parameters.ownerPartyId)){
+	condList.add(EntityCondition.makeCondition("ownerPartyId", EntityOperator.EQUALS, parameters.ownerPartyId));
+}
+cond = EntityCondition.makeCondition(condList, EntityOperator.AND);
+
+finAccounts = delegator.findList("FinAccount", cond, null, null, null, false);
+
+finAccountList=[];
+finAccounts.each{ eachvalue ->
+	if(eachvalue.finAccountName){
+		finAccountList.addAll(eachvalue);
+	}
+}
+context.finAccountList = finAccountList;
