fa7a9e57759aa457fb77925fb3b990a228fa407c
p-vamsi <vamsikrishna@vasista.in>
2017-02-28 11:48:47 +0530

    Changes..

--- hot-deploy/depotsales/webapp/depotsales/WEB-INF/actions/depotsales/CustomerDetails.groovy
+++ hot-deploy/depotsales/webapp/depotsales/WEB-INF/actions/depotsales/CustomerDetails.groovy
@@ -50,6 +50,7 @@ if(UtilValidate.isEmpty(partyId)){
 		condListb = [];
 		condListb.add(EntityCondition.makeCondition("partyIdFrom", EntityOperator.EQUALS, branchId));
 		condListb.add(EntityCondition.makeCondition("roleTypeIdFrom", EntityOperator.EQUALS, "PARENT_ORGANIZATION"));
+		condListb.add(EntityCondition.makeCondition("roleTypeIdTo", EntityOperator.NOT_EQUAL, "BRANCH_EMPLOYEE"));
 		condListb = EntityCondition.makeCondition(condListb, EntityOperator.AND);
 		PartyRelationship = delegator.findList("PartyRelationship", condListb,UtilMisc.toSet("partyIdTo"), null, null, false);
 		branchList=EntityUtil.getFieldListFromEntityList(PartyRelationship, "partyIdTo", true);
