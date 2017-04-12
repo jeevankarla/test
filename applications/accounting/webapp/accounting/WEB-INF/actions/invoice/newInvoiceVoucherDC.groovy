94546117fb6ac212e3a3378ca711f8bc53bd574f
p-vamsi <vamsikrishna@vasista.in>
2017-04-12 15:26:14 +0530

    Invoice..

--- applications/accounting/webapp/accounting/WEB-INF/actions/invoice/newInvoiceVoucherDC.groovy
+++ applications/accounting/webapp/accounting/WEB-INF/actions/invoice/newInvoiceVoucherDC.groovy
@@ -299,7 +299,7 @@ if(roID){
 			  mgpsAmt = mgpsAmt+eachItem.itemValue;
 			  }
 			  
-			  if(eachItem.invoiceItemTypeId !="VAT_SALE" && eachItem.invoiceItemTypeId !="VAT_SURCHARGE" && eachItem.invoiceItemTypeId !="TEN_PERCENT_SUBSIDY" && eachItem.invoiceItemTypeId !="INV_FPROD_ITEM" && eachItem.invoiceItemTypeId !="PRICE_DISCOUNT"){
+			  if(eachItem.invoiceItemTypeId == "SERVICE_CHARGE"){
 				  unitPriceIncTax=unitPriceIncTax+(eachItem.amount/eachList.quantity);
 			  }
 			  if(eachItem.invoiceItemTypeId=="VAT_SALE" || eachItem.invoiceItemTypeId=="VAT_SURCHARGE"){
